cc148522873702196f76ecffdc2c527275a92f1c
tteck <tteckster@gmail.com>
2021-08-12 12:47:00 -0400

    Update pve6_ha_container.sh

--- pve6_ha_container.sh
+++ pve6_ha_container.sh
@@ -141,7 +141,7 @@ ARCH=$(dpkg --print-architecture)
 HOSTNAME=homeassistant
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
-  -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 \
+  -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 2 -memory 4096 \
   -ostype $OSTYPE -rootfs $ROOTFS,size=$DISK_SIZE -storage $STORAGE >/dev/null
 
 # Modify LXC permissions to support Docker
