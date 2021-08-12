297f07f7ab23074c523b40832acdd4314b991039
tteck <tteckster@gmail.com>
2021-08-12 15:40:27 -0400

    Update debian10_container.sh

--- debian10_container.sh
+++ debian10_container.sh
@@ -137,7 +137,7 @@ else
   mkfs.ext4 $(pvesm path $ROOTFS) &>/dev/null
 fi
 ARCH=$(dpkg --print-architecture)
-HOSTNAME=lxc
+HOSTNAME=debian10
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
   -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 1 -memory 512\
