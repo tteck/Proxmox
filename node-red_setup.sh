4dd471b0b02af76ccd4a2f6e34917e64ad5c6392
tteck <tteckster@gmail.com>
2021-08-16 19:06:51 -0400

    Update node-red_setup.sh

--- node-red_setup.sh
+++ node-red_setup.sh
@@ -32,7 +32,7 @@ apt-get autoremove >/dev/null
 
 # Update container OS
 msg "Updating container OS..."
-apt-get update >/dev/null
+apt-get --allow-releaseinfo-change update >/dev/null
 apt-get -qqy upgrade &>/dev/null
 
 # Install prerequisites
