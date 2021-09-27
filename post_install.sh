d6c34d72104b738464b7eeb481f4851439e9e3eb
tteck <tteckster@gmail.com>
2021-09-27 12:55:11 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "\e[1;33m This script will Disable the Enterprise Repo, Enable the No Subscription Repo,
-Enable the Test Repo (repo's can be enabled/disabled via the UI in Repositories) and attempt 
+Add (Disabled) Test Repo (repo's can be enabled/disabled via the UI in Repositories) and attempt 
 the No Nag fix *PVE7 ONLY*\e[0m"
 
 read -p "Press [Enter] to start the PVE7 Post Install Script"
@@ -13,7 +13,7 @@ deb http://ftp.us.debian.org/debian bullseye main contrib
 deb http://ftp.us.debian.org/debian bullseye-updates main contrib
 deb http://security.debian.org bullseye-security main contrib
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
-deb http://download.proxmox.com/debian/pve bullseye pvetest
+# deb http://download.proxmox.com/debian/pve bullseye pvetest
 EOF
 
 sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
