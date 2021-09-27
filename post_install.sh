ab8e9abc588a7870845ce0a10493b3b0eedc4789
tteck <tteckster@gmail.com>
2021-09-27 08:09:55 -0400

    Add files via upload

---
+++ post_install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo -e "\e[1;33m This script will Disable the Enterprise Repo and Enable the No Subscription Repo  *PVE7 ONLY*\e[0m"
+
+read -p "Press [Enter] to start the PVE7 Post Install Script"
+
+read -t 2 -p "Disabling Enterprise Repo ..."
+sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
+
+read -t 2 -p "Enabling No Subscription Repo ..."
+cat <<EOF > /etc/apt/sources.list
+deb http://ftp.us.debian.org/debian bullseye main contrib
+deb http://ftp.us.debian.org/debian bullseye-updates main contrib
+deb http://security.debian.org bullseye-security main contrib
+deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
+# deb http://download.proxmox.com/debian/pve bullseye pvetest
+EOF
+
+# read -t 2 -p "Enabling No Nag ..."
+# sed -i.backup -z "s/res === null || res === undefined || \!res || res\n\t\t\t.data.status.toLowerCase() \!== 'active'/false/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
+
+echo -e "\e[1;33m Finished....Please Update Proxmox \e[0m"
+
+# bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/post_install.sh)"
