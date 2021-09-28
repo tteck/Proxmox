30a6065626ab0f31df831a798cd18ac9a6445a77
tteck <tteckster@gmail.com>
2021-09-28 18:01:13 -0400

    Update esphome_container.sh

--- esphome_container.sh
+++ esphome_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-    read -p "This will create a New Proxmox ESPHome LXC Container. Proceed(y/n)?" yn
+    read -p "This will create a New ESPHome LXC Container. Proceed(y/n)?" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
