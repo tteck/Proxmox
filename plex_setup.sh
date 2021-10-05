fe71fa8fc87a7495ee1e99a706550659dfcceab2
tteck <tteckster@gmail.com>
2021-10-05 12:53:57 -0400

    Add files via upload

---
+++ plex_setup.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+set -o errexit  
+set -o errtrace 
+set -o nounset  
+set -o pipefail 
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+trap 'die "Script interrupted."' INT
+
+function error_exit() {
+  trap - ERR
+  local DEFAULT='Unknown failure occured.'
+  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
+  msg "$FLAG $REASON"
+  exit $EXIT
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+
+msg "Setting up LXC OS..."
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+locale-gen >/dev/null
+apt-get -y purge openssh-{client,server} >/dev/null
+apt-get autoremove >/dev/null
+
+msg "Updating LXC OS..."
+apt-get update >/dev/null
+apt-get -qqy upgrade &>/dev/null
+
+msg "Installing Prerequisites..."
+apt-get -qqy install \
+    curl \
+    sudo &>/dev/null
+
+msg "Installing Plex Server.."
+echo deb https://downloads.plex.tv/repo/deb public main | tee /etc/apt/sources.list.d/plexmediaserver.list &>/dev/null
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt-key add - &>/dev/null
+apt-get update &>/dev/null
+apt-get install plexmediaserver &>/dev/null
+apt-get upgrade &>/dev/null
+
+msg "Customizing LXC..."
+rm /etc/motd 
+rm /etc/update-motd.d/10-uname 
+touch ~/.hushlogin 
+GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
+mkdir -p $(dirname $GETTY_OVERRIDE)
+cat << EOF > $GETTY_OVERRIDE
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+EOF
+systemctl daemon-reload
+systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+
+msg "Cleanup..."
+rm -rf /plex_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
