69368b28a12c5906f293b2fa7fe73c3cced9ec9b
tteck <tteckster@gmail.com>
2021-10-07 09:12:55 -0400

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -16,9 +16,9 @@ done
 sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
 
 cat <<EOF > /etc/apt/sources.list
-deb http://ftp.us.debian.org/debian bullseye main contrib
-deb http://ftp.us.debian.org/debian bullseye-updates main contrib
-deb http://security.debian.org bullseye-security main contrib
+deb http://ftp.debian.org/debian bullseye main contrib
+deb http://ftp.debian.org/debian bullseye-updates main contrib
+deb http://security.debian.org/debian-security bullseye-security main contrib
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 # deb http://download.proxmox.com/debian/pve bullseye pvetest
 EOF
