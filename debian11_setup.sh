9f40c2dbe870b978a64640b2f09b88eafeb92119
tteckster <tteckster@gmail.com>
2021-10-23 17:30:14 -0400

    Update debian11_setup.sh

--- debian11_setup.sh
+++ debian11_setup.sh
@@ -32,7 +32,7 @@ apt-get autoremove >/dev/null
 
 # Update container OS
 msg "Updating container OS..."
-apt-get qqy update --allow-releaseinfo-change && apt-get qqy full-upgrade &>/dev/null
+apt-get update --allow-releaseinfo-change && apt-get qqy full-upgrade &>/dev/null
 #apt-get upgrade &>/dev/null
 
 # Install prerequisites
