d589387e36517ce6ae47a91b0fbe4684b4945605
tteckster <tteckster@gmail.com>
2021-11-28 01:32:12 -0500

    Update ha_container.sh

--- ha_container.sh
+++ ha_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 while true; do
-    read -p "This will create a New Home Assistant LXC Container. Proceed(y/n)?" yn
+    read -p "This will create a New Home Assistant Container LXC. Proceed(y/n)?" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
