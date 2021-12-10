44eb8ab1b27d230fd0a206f4648d9b8192ca0cf8
tteckster <tteckster@gmail.com>
2021-12-10 16:10:36 -0500

    Add files via upload

---
+++ webmin.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+while true; do
+    read -p "This will Install Webmin, Proceed(y/n)?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
+echo -e "Loading Script..."
+echo -e "${CHECKMARK} \e[1;92m Installing Prerequisites... \e[0m"
+apt install -y libnet-ssleay-perl libauthen-pam-perl libio-pty-perl unzip shared-mime-info >&/dev/null
+echo -e "${CHECKMARK} \e[1;92m Downloading Webmin... \e[0m"
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.983_all.deb >&/dev/null
+echo -e "${CHECKMARK} \e[1;92m Installing Webmin... \e[0m"
+dpkg --install webmin_1.983_all.deb >&/dev/null
+echo -e "${CHECKMARK} \e[1;92m Setting Default Webmin usermame & password to root... \e[0m"
+/usr/share/webmin/changepass.pl /etc/webmin root root >&/dev/null
+echo -e    "Installed Successfully! Go To IP:10000"
+
+
+# bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/webmin.sh)"
