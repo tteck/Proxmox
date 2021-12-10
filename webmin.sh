f05e74a93b6fd0fe3933e3ad2310252e513e591e
tteckster <tteckster@gmail.com>
2021-12-10 16:30:44 -0500

    Add files via upload

--- webmin.sh
+++ webmin.sh
@@ -10,14 +10,14 @@ done
 CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
 echo -e "Loading Script..."
 echo -e "${CHECKMARK} \e[1;92m Installing Prerequisites... \e[0m"
-apt update #>&/dev/null
-apt install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl unzip shared-mime-info #>&/dev/null
+apt update &>/dev/null
+apt-get -qqy install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl unzip shared-mime-info &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Downloading Webmin... \e[0m"
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.983_all.deb #>&/dev/null
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.983_all.deb &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Installing Webmin... \e[0m"
-dpkg --install webmin_1.983_all.deb #>&/dev/null
+dpkg --install webmin_1.983_all.deb &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Setting Default Webmin usermame & password to root... \e[0m"
-/usr/share/webmin/changepass.pl /etc/webmin root root #>&/dev/null
+/usr/share/webmin/changepass.pl /etc/webmin root root &>/dev/null
 echo -e    "Installed Successfully! Go To IP:10000"
 
 
