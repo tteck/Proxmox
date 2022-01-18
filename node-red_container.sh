f8ea7aba3a1415172b869b523a35a17676d49607
tteckster <tteckster@gmail.com>
2022-01-18 18:50:19 -0500

    Update node-red_container.sh

--- node-red_container.sh
+++ node-red_container.sh
@@ -74,7 +74,7 @@ TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
 # Download setup script
-wget -qL https://raw.githubusercontent.com/tteck/Proxmox/main/node-red_setup.sh
+wget -qL https://raw.githubusercontent.com/tteck/Proxmox/main/setup/node-red_setup.sh
 
 # Detect modules and automatically load at boot
 load_module overlay
