d294bc9d1c67f0c2fe745f850056047c2dc3df27
tteckster <tteckster@gmail.com>
2022-01-25 20:40:43 -0500

    Add files via upload

--- ct/jellyfin_container.sh
+++ ct/jellyfin_container.sh
@@ -138,7 +138,7 @@ else
   mkfs.ext4 $(pvesm path $ROOTFS) &>/dev/null
 fi
 ARCH=$(dpkg --print-architecture)
-HOSTNAME=plex
+HOSTNAME=jellyfin
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
 pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
   -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 2 -memory 2048\
