d098b175deb91c5ade69d9d75eebedb84610fae7
tteckster <tteckster@gmail.com>
2022-01-27 14:24:31 -0500

    Update pihole_setup.sh

--- setup/pihole_setup.sh
+++ setup/pihole_setup.sh
@@ -25,8 +25,6 @@ function msg() {
 msg "Setting up LXC OS..."
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
-apt-get -y purge openssh-{client,server} >/dev/null
-apt-get autoremove >/dev/null
 
 # Update container OS
 msg "Updating container OS..."
