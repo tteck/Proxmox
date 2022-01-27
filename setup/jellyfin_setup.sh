e692f53107612cee5ddd71f14674e006297e5ba0
tteckster <tteckster@gmail.com>
2022-01-27 16:00:18 -0500

    Update jellyfin_setup.sh

--- setup/jellyfin_setup.sh
+++ setup/jellyfin_setup.sh
@@ -68,6 +68,8 @@ ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,3840
 EOF
 systemctl daemon-reload
 systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-sudo systemctl enable jellyfin &>/dev/null
+usermod -a -G input jellyfin
+usermod -a -G render jellyfin
+systemctl enable jellyfin &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Cleanup... \e[0m"
 rm -rf /jellyfin_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
