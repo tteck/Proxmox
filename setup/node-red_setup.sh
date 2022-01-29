09c095a87ab1af6f55492d048940d8c72dd2a94b
tteckster <tteckster@gmail.com>
2022-01-29 17:51:08 -0500

    Update node-red_setup.sh

--- setup/node-red_setup.sh
+++ setup/node-red_setup.sh
@@ -6,6 +6,7 @@ set -o nounset
 set -o pipefail 
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
+CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
 trap die ERR
 trap 'die "Script interrupted."' INT
 
@@ -23,38 +24,26 @@ function msg() {
 }
 
 msg "Setting up container OS..."
+echo -e "${CHECKMARK} \e[1;92m Setting up Container OS... \e[0m"
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
 apt-get -y purge openssh-{client,server} >/dev/null
 apt-get autoremove >/dev/null
 
 msg "Updating container OS..."
+echo -e "${CHECKMARK} \e[1;92m Updating Container OS... \e[0m"
 apt update &>/dev/null
 apt-get -qqy upgrade &>/dev/null
 
-msg "Installing prerequisites..."
+echo -e "${CHECKMARK} \e[1;92m Installing Dependencie... \e[0m"
 apt-get -qqy install \
     curl \
     sudo &>/dev/null
 
-msg "Installing Node-Red..."
+echo -e "${CHECKMARK} \e[1;92m Installing Node-Red... \e[0m"
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi
 
-#msg "Installing Node..."
-#sudo apt -y install nodejs npm &>/dev/null
-
-#msg "Installing PM2..."
-#npm install -g pm2 &>/dev/null 
-
-#msg "Installing Node-RED..."
-#npm install -g --unsafe-perm node-red &>/dev/null
-
-#msg "Setting up PM2..."
-#/usr/local/bin/pm2 start /usr/local/bin/node-red  &>/dev/null
-#/usr/local/bin/pm2 save &>/dev/null
-#/usr/local/bin/pm2 startup &>/dev/null
-
-msg "Customizing container..."
+echo -e "${CHECKMARK} \e[1;92m Customizing Container... \e[0m"
 rm /etc/motd # Remove message of the day after login
 rm /etc/update-motd.d/10-uname # Remove kernel information after login
 touch ~/.hushlogin # Remove 'Last login: ' and mail notification after login
@@ -67,6 +56,7 @@ ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,3840
 EOF
 systemctl daemon-reload
 systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-
-msg "Cleanup..."
+node-red-start &>/dev/null
+systemctl enable nodered.service &>/dev/null
+echo -e "${CHECKMARK} \e[1;92m Cleanup... \e[0m"
 rm -rf /node-red_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
