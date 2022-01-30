98fd861560ce854c9d27c28fddcbf5a191b91463
tteckster <tteckster@gmail.com>
2022-01-30 16:52:19 -0500

    Update adguard_container.sh

--- ct/adguard_container.sh
+++ ct/adguard_container.sh
@@ -140,7 +140,7 @@ fi
 ARCH=$(dpkg --print-architecture)
 HOSTNAME=adguard
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
-pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
+pct create $CTID $TEMPLATE_STRING -arch $ARCH -features nesting=1,unprivileged 0 \
   -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 1 -memory 512 \
   -ostype $OSTYPE -rootfs $ROOTFS,size=$DISK_SIZE -storage $STORAGE >/dev/null
 
