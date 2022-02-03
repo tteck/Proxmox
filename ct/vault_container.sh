35f3913b9136e794670c483c734a6890060f6832
tteckster <tteckster@gmail.com>
2022-02-03 18:42:12 -0500

    Add files via upload

--- ct/vault_container.sh
+++ ct/vault_container.sh
@@ -73,7 +73,7 @@ function load_module() {
 TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
-wget -qL https://raw.githubusercontent.com/tteck/Proxmox/main/setup/debian11_setup.sh
+wget -qL https://raw.githubusercontent.com/tteck/Proxmox/main/setup/vault_setup.sh
 
 load_module overlay
 
