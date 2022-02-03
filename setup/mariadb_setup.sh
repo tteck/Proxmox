03d46eb1f6cb2e69cb90b097458e3d261340b764
tteckster <tteckster@gmail.com>
2022-02-03 07:34:57 -0500

    Update mariadb_setup.sh

--- setup/mariadb_setup.sh
+++ setup/mariadb_setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Setup script environment
-set -o errexit  #Exit immediately if a pipeline returns a non-zero status
-set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
-set -o nounset  #Treat unset variables as an error
-set -o pipefail #Pipe will exit with last non-zero status if applicable
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 CROSS='\033[1;31m\xE2\x9D\x8C\033[0m'
@@ -28,8 +27,7 @@ function msg() {
   echo -e "$TEXT"
 }
 
-# Prepare container OS
-msg "Setting up container OS..."
+echo -e "${CHECKMARK} \e[1;92m Setting up Container OS... \e[0m"
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
 while [ "$(hostname -I)" = "" ]; do
@@ -44,28 +42,25 @@ while [ "$(hostname -I)" = "" ]; do
 done
   echo -e "${CHECKMARK} \e[1;92m Network Connected: \e[0m $(hostname -I)"
 
-# Update container OS
-msg "Updating container OS..."
+echo -e "${CHECKMARK} \e[1;92m Updating Container OS... \e[0m"
 apt-get update &>/dev/null
 apt-get -qqy upgrade &>/dev/null
 
-# Install prerequisites
-msg "Installing Prerequisites..."
+echo -e "${CHECKMARK} \e[1;92m Installing Dependencies... \e[0m"
 apt-get update &>/dev/null
 apt-get -qqy install \
     curl \
     sudo &>/dev/null
 
-# Installing MariaDB
-msg "Installing MariaDB..."
+echo -e "${CHECKMARK} \e[1;92m Installing MariaDB... \e[0m"
 curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash &>/dev/null
 apt-get update >/dev/null
 apt-get install -y mariadb-server &>/dev/null
-# Customize container
-msg "Customizing container..."
-rm /etc/motd # Remove message of the day after login
-rm /etc/update-motd.d/10-uname # Remove kernel information after login
-touch ~/.hushlogin # Remove 'Last login: ' and mail notification after login
+
+echo -e "${CHECKMARK} \e[1;92m Customizing Container... \e[0m"
+rm /etc/motd
+rm /etc/update-motd.d/10-uname
+touch ~/.hushlogin
 GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
 mkdir -p $(dirname $GETTY_OVERRIDE)
 cat << EOF > $GETTY_OVERRIDE
@@ -76,6 +71,5 @@ EOF
 systemctl daemon-reload
 systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
 
-# Cleanup container
-msg "Cleanup..."
+echo -e "${CHECKMARK} \e[1;92m Cleanup... \e[0m"
 rm -rf /mariadb_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
