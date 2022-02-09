c665358d21ab0e4349db6bfd495cce514113a9f9
tteckster <tteckster@gmail.com>
2022-02-09 03:49:21 -0500

    Update jellyfin_container.sh

--- ct/jellyfin_container.sh
+++ ct/jellyfin_container.sh
@@ -108,8 +108,8 @@ info "Container ID is $CTID."
 echo -e "${CHECKMARK} \e[1;92m Updating LXC Template List... \e[0m"
 pveam update >/dev/null
 echo -e "${CHECKMARK} \e[1;92m Downloading LXC Template... \e[0m"
-OSTYPE=ubuntu
-OSVERSION=${OSTYPE}-21.10
+OSTYPE=debian
+OSVERSION=${OSTYPE}-11
 mapfile -t TEMPLATES < <(pveam available -section system | sed -n "s/.*\($OSVERSION.*\)/\1/p" | sort -t - -k 2 -V)
 TEMPLATE="${TEMPLATES[-1]}"
 pveam download local $TEMPLATE >/dev/null ||
