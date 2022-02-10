b51c9d7c98ce57c72ef36c5b95ead3412165204d
tteckster <tteckster@gmail.com>
2022-02-10 10:50:41 -0500

    Add files via upload

--- setup/motioneye_setup.sh
+++ setup/motioneye_setup.sh
@@ -66,8 +66,8 @@ echo -e "${CHECKMARK} \e[1;92m Installing Python... \e[0m"
  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py &>/dev/null
  python2 get-pip.py &>/dev/null
  #apt-get install python-dev-is-python2 python-setuptools libssl-dev libcurl4-openssl-dev libjpeg-dev zlib1g-dev libffi-dev libzbar-dev libzbar0 -y &>/dev/null
- apt-get install libffi-dev libzbar-dev libzbar0 -y
- apt-get install python2-dev libssl-dev libcurl4-openssl-dev libjpeg-dev -y
+ apt-get install libffi-dev libzbar-dev libzbar0 -y &>/dev/null
+ apt-get install python2-dev libssl-dev libcurl4-openssl-dev libjpeg-dev -y &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Installing MotionEye... \e[0m"
  apt-get update &>/dev/null
  sudo pip install motioneye
