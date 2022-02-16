890a74e09a848fd6dc301a6118a7266295552f44
tteckster <tteckster@gmail.com>
2022-02-16 17:31:32 -0500

    Update mariadb_setup.sh

--- setup/mariadb_setup.sh
+++ setup/mariadb_setup.sh
@@ -60,6 +60,9 @@ apt-get install -y mariadb-server &>/dev/null
 echo -e "${CHECKMARK} \e[1;92m Installing Adminer... \e[0m"
 sudo apt install adminer -y &>/dev/null
 sudo a2enconf adminer &>/dev/null
+mkdir /var/log/apache2
+chmod 750 /var/log/apache2
+chown root:adm /var/log/apache2
 sudo systemctl reload apache2 &>/dev/null
 
 echo -e "${CHECKMARK} \e[1;92m Customizing Container... \e[0m"
