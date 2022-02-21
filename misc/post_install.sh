02b4453d2d03d0400bec43517e88befe76ca439a
tteckster <tteckster@gmail.com>
2022-02-21 11:25:19 -0500

    Update post_install.sh

--- misc/post_install.sh
+++ misc/post_install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "\e[1;33m This script will Disable the Enterprise Repo, Add & Enable the No-Subscription Repo, 
-Add & Disable Test Repo (repo's can be enabled/disabled via the UI in Repositories) 
-and attempt the No-Nag fix. PVE7 ONLY \e[0m"
+echo -e "\e[1;33m This script will Setup Repositories and attempt the No-Nag fix. PVE7 ONLY \e[0m"
 while true; do
     read -p "Start the PVE7 Post Install Script (y/n)?" yn
     case $yn in
@@ -11,7 +9,12 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+clear
+echo -e "\e[1;33m Disable Enterprise Repository...  \e[0m"
+sleep 1
 sed -i "s/^deb/#deb/g" /etc/apt/sources.list.d/pve-enterprise.list
+echo -e "\e[1;33m Setup Repositories...  \e[0m"
+sleep 1
 cat <<EOF > /etc/apt/sources.list
 deb http://ftp.debian.org/debian bullseye main contrib
 deb http://ftp.debian.org/debian bullseye-updates main contrib
@@ -19,6 +22,8 @@ deb http://security.debian.org/debian-security bullseye-security main contrib
 deb http://download.proxmox.com/debian/pve bullseye pve-no-subscription
 # deb http://download.proxmox.com/debian/pve bullseye pvetest
 EOF
+echo -e "\e[1;33m Disable Subscription Nag...  \e[0m"
+sleep 1
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
 apt --reinstall install proxmox-widget-toolkit &>/dev/null
 echo -e "\e[1;33m Finished....Please Update Proxmox \e[0m"
