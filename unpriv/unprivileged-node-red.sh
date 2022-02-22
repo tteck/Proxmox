1312b256f28657f5993fad645c2fe518d983b35a
tteckster <tteckster@gmail.com>
2022-02-22 13:49:23 -0500

    Update unprivileged-node-red.sh

--- unpriv/unprivileged-node-red.sh
+++ unpriv/unprivileged-node-red.sh
@@ -1,63 +1,116 @@
 #!/usr/bin/env bash
-CROSS='\033[1;31m\xE2\x9D\x8C\033[0m'
-RD=`echo "\033[01;31m"`
+
 BL=`echo "\033[36m"`
+RD=`echo "\033[01;31m"`
 CM='\xE2\x9C\x94\033'
 GN=`echo "\033[1;92m"`
 CL=`echo "\033[m"`
-RETRY_NUM=5
-RETRY_EVERY=3
-NUM=$RETRY_NUM
-echo -en "${GN} Setting up Container OS... "
-sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
-locale-gen >/dev/null
-while [ "$(hostname -I)" = "" ]; do
-  1>&2 echo -en "${CROSS}${RD}  No Network! "
-  sleep $RETRY_EVERY
-  ((NUM--))
-  if [ $NUM -eq 0 ]
-  then
-    1>&2 echo -e "${CROSS}${RD}  No Network After $RETRY_NUM Tries${CL}"    
-    exit 1
-  fi
+while true; do
+    read -p "This will create a New Unprivileged Node-Red LXC. Proceed(y/n)?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 done
-echo -e "${CM}${CL} \r"
-echo -en "${GN} Network Connected: ${BL}$(hostname -I)${CL} "
+clear
+function header_info {
+echo -e "${RD}
+
+      _   _           _             _____          _ 
+     | \ | |         | |           |  __ \        | |
+     |  \| | ___   __| | ___ ______| |__) |___  __| |
+     |     |/ _ \ / _  |/ _ \______|  _  // _ \/ _  |
+     | |\  | (_) | (_| |  __/      | | \ \  __/ (_| |
+     |_| \_|\___/ \__,_|\___|      |_|  \_\___|\__,_|
+                                                 
+                                                 
+${CL}"
+}
+
+header_info
+
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+trap cleanup EXIT
+
+function error_exit() {
+  trap - ERR
+  local DEFAULT='Unknown failure occured.'
+  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR] \e[93m$EXIT@$LINE"
+  msg "$FLAG $REASON"
+  [ ! -z ${CTID-} ] && cleanup_ctid
+  exit $EXIT
+}
+function warn() {
+  local REASON="\e[97m$1\e[39m"
+  local FLAG="\e[93m[WARNING]\e[39m"
+  msg "$FLAG $REASON"
+}
+function info() {
+  local REASON="$1"
+  local FLAG="\e[36m[INFO]\e[39m"
+  msg "$FLAG $REASON"
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+function cleanup_ctid() {
+  if $(pct status $CTID &>/dev/null); then
+    if [ "$(pct status $CTID | awk '{print $2}')" == "running" ]; then
+      pct stop $CTID
+    fi
+    pct destroy $CTID
+  elif [ "$(pvesm list $STORAGE --vmid $CTID)" != "" ]; then
+    pvesm free $ROOTFS
+  fi
+}
+function cleanup() {
+  popd >/dev/null
+  rm -rf $TEMP_DIR
+}
+TEMP_DIR=$(mktemp -d)
+pushd $TEMP_DIR >/dev/null
+
+export CTID=$(pvesh get /cluster/nextid)
+export PCT_OSTYPE=debian
+export PCT_OSVERSION=11
+export PCT_DISK_SIZE=4
+export PCT_OPTIONS="
+  -features nesting=1
+  -hostname node-red
+  -net0 name=eth0,bridge=vmbr0,ip=dhcp
+  -onboot 1
+  -cores 1
+  -memory 1024
+  -unprivileged 1
+"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/unprev/create_lxc.sh)" || exit
+
+STORAGE_TYPE=$(pvesm status -storage $(pct config $CTID | grep rootfs | awk -F ":" '{print $2}') | awk 'NR>1 {print $2}')
+if [ "$STORAGE_TYPE" == "zfspool" ]; then
+  warn "Some addons may not work due to ZFS not supporting 'fallocate'."
+fi
+
+echo -en "${GN} Starting LXC Container... "
+pct start $CTID
 echo -e "${CM}${CL} \r"
 
-echo -en "${GN} Updating Container OS... "
-apt update &>/dev/null
-apt-get -qqy upgrade &>/dev/null
-echo -e "${CM}${CL} \r"
+alias lxc-cmd="lxc-attach -n $CTID --"
 
-echo -en "${GN} Installing Dependencies... "
-apt-get install -y curl &>/dev/null
-apt-get install -y sudo &>/dev/null
-echo -e "${CM}${CL} \r"
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/unprev/node-red-install.sh)" || exit
 
-echo -en "${GN} Installing Node-Red... "
-bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered) --confirm-root --confirm-install --skip-pi &>/dev/nul
-sudo systemctl enable nodered.service &>/dev/null
-sudo systemctl start nodered.service &>/dev/null
-echo -e "${CM}${CL} \r"
+IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
-echo -en "${GN} Customizing Container... "
-rm /etc/motd
-rm /etc/update-motd.d/10-uname
-touch ~/.hushlogin
-GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
-mkdir -p $(dirname $GETTY_OVERRIDE)
-cat << EOF > $GETTY_OVERRIDE
-[Service]
-ExecStart=
-ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
-EOF
-systemctl daemon-reload
-systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-echo -e "${CM}${CL} \r"
+echo -e "${GN}Successfully created Node-Red LXC to${CL} ${BL}$CTID${CL}."
+${RD}Node-Red${CL} is reachable by going to the following URL.
 
-echo -en "${GN} Cleanup... "
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
-rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
-echo -e "${CM}${CL} \n"
+         ${BL}http://${IP}:1880${CL} \n"
