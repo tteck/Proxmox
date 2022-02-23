e9a575dcb9d43eac5887d1f0894c5b7632afa4d3
tteckster <tteckster@gmail.com>
2022-02-22 23:11:56 -0500

    Update unprivileged-homeassistant.sh

--- unpriv/unprivileged-homeassistant.sh
+++ unpriv/unprivileged-homeassistant.sh
@@ -98,6 +98,7 @@ bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/unpr
 
 STORAGE_TYPE=$(pvesm status -storage $(pct config $CTID | grep rootfs | awk -F ":" '{print $2}') | awk 'NR>1 {print $2}')
 if [ "$STORAGE_TYPE" == "zfspool" ]; then
+  wget -qL -O fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/v1.8.2/fuse-overlayfs-x86_64
   warn "Some addons may not work due to ZFS not supporting 'fallocate'."
 fi
 LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
@@ -108,6 +109,10 @@ EOF
 
 echo -en "${GN} Starting LXC Container... "
 pct start $CTID
+if [ "$STORAGE_TYPE" == "zfspool" ]; then
+pct push $CTID fuse-overlayfs /usr/local/bin/fuse-overlayfs -perms 755
+info "${BL}Using fuse-overlayfs.${CL}"
+fi
 echo -e "${CM}${CL} \r"
 
 alias lxc-cmd="lxc-attach -n $CTID --"
