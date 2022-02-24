339c79a0f675b5679cdd698db324feb10a740b8b
tteckster <tteckster@gmail.com>
2022-02-24 17:06:14 -0500

    Update podman_ha_setup.sh

--- setup/podman_ha_setup.sh
+++ setup/podman_ha_setup.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o errtrace
-set -o nounset
-set -o pipefail
+set -o errexit 
+set -o errtrace 
+set -o nounset 
+set -o pipefail 
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
-CROSS='\033[1;31m\xE2\x9D\x8C\033[0m'
-CHECKMARK='\033[0;32m\xE2\x9C\x94\033[0m'
-RETRY_NUM=5
-RETRY_EVERY=3
-NUM=$RETRY_NUM
 trap die ERR
 trap 'die "Script interrupted."' INT
 
@@ -27,39 +22,55 @@ function msg() {
   echo -e "$TEXT"
 }
 
-echo -e "${CHECKMARK} \e[1;92m Setting up Container OS... \e[0m"
+CROSS='\033[1;31m\xE2\x9D\x8C\033[0m'
+RD=`echo "\033[01;31m"`
+BL=`echo "\033[36m"`
+CM='\xE2\x9C\x94\033'
+GN=`echo "\033[1;92m"`
+CL=`echo "\033[m"`
+RETRY_NUM=5
+RETRY_EVERY=3
+NUM=$RETRY_NUM
+
+echo -en "${GN} Setting up Container OS... "
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
 while [ "$(hostname -I)" = "" ]; do
-  1>&2 echo -e "${CROSS} \e[1;31m No Network: \e[0m $(date)"
+  1>&2 echo -en "${CROSS}${RD}  No Network! "
   sleep $RETRY_EVERY
   ((NUM--))
   if [ $NUM -eq 0 ]
   then
-    1>&2 echo -e "${CROSS} \e[1;31m No Network After $RETRY_NUM Tries \e[0m"
+    1>&2 echo -e "${CROSS}${RD}  No Network After $RETRY_NUM Tries${CL}"    
     exit 1
   fi
 done
-  echo -e "${CHECKMARK} \e[1;92m Network Connected: \e[0m $(hostname -I)"
+echo -e "${CM}${CL} \r"
+echo -en "${GN} Network Connected: ${BL}$(hostname -I)${CL} "
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Updating Container OS... \e[0m"
-apt-get update &>/dev/null
+echo -en "${GN} Updating Container OS... "
+apt update &>/dev/null
 apt-get -qqy upgrade &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Installing Dependencies... \e[0m"
+echo -en "${GN} Installing Dependencies... "
 apt-get update &>/dev/null
 apt-get -qqy install \
     curl \
     sudo \
     runc &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Installing Podman... \e[0m"
+echo -en "${GN} Installing Podman... "
 apt-get -y install podman &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Pulling Yacht Image...\e[0m"
+echo -en "${GN} Pulling Yacht Image... "
 podman pull docker.io/selfhostedpro/yacht:latest &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Installing Yacht... \e[0m"
+echo -en "${GN} Installing Yacht... "
 podman volume create yacht >/dev/null
 podman run -d \
   --name yacht \
@@ -70,11 +81,13 @@ podman run -d \
   -v /etc/timezone:/etc/timezone:ro \
   -p 8000:8000 \
   selfhostedpro/yacht:latest &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Pulling Home Assistant Image...\e[0m"
+echo -en "${GN} Pulling Home Assistant Image... "
 podman pull docker.io/homeassistant/home-assistant:stable &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Installing Home Assistant... \e[0m"
+echo -en "${GN} Installing Home Assistant... "
 podman volume create hass_config >/dev/null
 podman run -d \
   --name homeassistant \
@@ -85,8 +98,9 @@ podman run -d \
   -v /etc/timezone:/etc/timezone:ro \
   --net=host \
   homeassistant/home-assistant:stable &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Creating Update Script... \e[0m"
+echo -en "${GN} Creating Update Script... "
 file_path="/root/update.sh"
 echo "#!/bin/bash
 echo -e '\e[1;33m Pulling New Stable Version... \e[0m'
@@ -109,8 +123,9 @@ echo -e '\e[1;33m Removing Old Image... \e[0m'
 podman image prune -f
 echo -e '\e[1;33m Finished Update! \e[0m'" > $file_path
 sudo chmod +x /root/update.sh
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Customizing LXC... \e[0m"
+echo -en "${GN} Customizing LXC... "
 rm /etc/motd
 rm /etc/update-motd.d/10-uname
 touch ~/.hushlogin
@@ -133,6 +148,8 @@ podman generate systemd \
     --new --name yacht \
     > /etc/systemd/system/yacht.service 
 systemctl enable yacht &>/dev/null
+echo -e "${CM}${CL} \r"
 
-echo -e "${CHECKMARK} \e[1;92m Cleanup... \e[0m"
+echo -en "${GN} Cleanup... "
 rm -rf /podman_ha_setup.sh /var/{cache,log}/* /var/lib/apt/lists/*
+echo -e "${CM}${CL} \n"
