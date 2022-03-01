37d89837cd2b566a09a4bbacde74bc63b5a22230
tteckster <tteckster@gmail.com>
2022-03-01 16:51:27 -0500

    Update daemonsync.sh

--- ct/daemonsync.sh
+++ ct/daemonsync.sh
@@ -153,7 +153,7 @@ echo -e "${CM}${CL} \r"
 
 alias lxc-cmd="lxc-attach -n $CTID --"
 
-lxc-cmd bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/dev/setup/daemonsync-install.sh)" || exit
+lxc-cmd bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/daemonsync-install.sh)" || exit
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
