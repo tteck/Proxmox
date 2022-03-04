8a36b55982355c7bb73de1c342e3d3883b2f15c8
tteckster <tteckster@gmail.com>
2022-03-04 07:50:51 -0500

    Update webmin.sh

--- misc/webmin.sh
+++ misc/webmin.sh
@@ -22,16 +22,16 @@ apt-get -y install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl unzip sh
 echo -e "${CM}${CL} \r"
 
 echo -en "${GN} Downloading Webmin... "
-wget http://prdownloads.sourceforge.net/webadmin/webmin_1.984_all.deb &>/dev/null
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.990_all.deb &>/dev/null
 echo -e "${CM}${CL} \r"
 
 echo -en "${GN} Installing Webmin... "
-dpkg --install webmin_1.984_all.deb &>/dev/null
+dpkg --install webmin_1.990_all.deb &>/dev/null
 echo -e "${CM}${CL} \r"
 
 echo -en "${GN} Setting Default Webmin usermame & password to root... "
 /usr/share/webmin/changepass.pl /etc/webmin root root &>/dev/null
-rm -rf /root/webmin_1.984_all.deb
+rm -rf /root/webmin_1.990_all.deb
 echo -e "${CM}${CL} \r"
 IP=$(hostname -I | cut -f1 -d ' ')
 echo -e    "${BL} Successfully Installed Webmin, Now Go To https://${IP}:10000 ${CL}"
