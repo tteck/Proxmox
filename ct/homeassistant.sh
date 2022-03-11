18e6170b040a9b1a8445d7ab438622318116e3b2
tteckster <tteckster@gmail.com>
2022-03-10 21:19:50 -0500

    Update homeassistant.sh

--- ct/homeassistant.sh
+++ ct/homeassistant.sh
@@ -173,7 +173,7 @@ pushd $TEMP_DIR >/dev/null
 export CTID=$(pvesh get /cluster/nextid)
 export PCT_OSTYPE=debian
 export PCT_OSVERSION=11
-export PCT_DISK_SIZE=8
+export PCT_DISK_SIZE=16
 export PCT_OPTIONS="
   -features $FEATURES
   -hostname homeassistant
