1f4a5ecd5fd4b1f39c443bc87203107f4e4be58e
tteckster <tteckster@gmail.com>
2022-03-16 17:42:54 -0400

    Update dashy-update.sh

--- misc/dashy-update.sh
+++ misc/dashy-update.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
-set -e
-RD=`echo "\033[01;31m"`
+clear
+YW=`echo "\033[33m"`
 BL=`echo "\033[36m"`
+RD=`echo "\033[01;31m"`
 CM='\xE2\x9C\x94\033'
 GN=`echo "\033[1;92m"`
 CL=`echo "\033[m"`
+while true; do
+    read -p "This will Update Dashy LXC. Proceed(y/n)?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 clear
+function header_info {
+echo -e "${RD}
+  _____            _           
+ |  __ \          | |          
+ | |  | | __ _ ___| |__  _   _ 
+ | |  | |/ _  / __|  _ \| | | |
+ | |__| | (_| \__ \ | | | |_| |
+ |_____/ \__,_|___/_| |_|\__, |
+          UPDATE          __/ |
+                         |___/ 
+${CL}"
+}
+
+header_info
 echo -en "${GN} Stopping Dashy... "
 systemctl stop dashy
 sleep 1
