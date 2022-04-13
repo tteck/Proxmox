2cd089fbbf1c7838fb40f8528a23f625a9c55e36
tteckster <tteckster@gmail.com>
2022-04-13 12:11:06 -0400

    Create uptimekuma-install.sh

---
+++ setup/uptimekuma-install.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+set -o errexit 
+set -o errtrace 
+set -o nounset 
+set -o pipefail 
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+trap 'die "Script interrupted."' INT
+
+function error_exit() {
+  trap - ERR
+  local DEFAULT='Unknown failure occured.'
+  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
+  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
+  msg "$FLAG $REASON"
+  exit $EXIT
+}
+function msg() {
+  local TEXT="$1"
+  echo -e "$TEXT"
+}
+
+CROSS='\033[1;31m\xE2\x9D\x8C\033[0m'
+RD=`echo "\033[01;31m"`
+BL=`echo "\033[36m"`
+CM='\xE2\x9C\x94\033'
+GN=`echo "\033[1;92m"`
+CL=`echo "\033[m"`
+RETRY_NUM=10
+RETRY_EVERY=3
+NUM=$RETRY_NUM
+
+echo -en "${GN} Setting up Container OS... "
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+locale-gen >/dev/null
+while [ "$(hostname -I)" = "" ]; do
+  1>&2 echo -en "${CROSS}${RD}  No Network! "
+  sleep $RETRY_EVERY
+  ((NUM--))
+  if [ $NUM -eq 0 ]
+  then
+    1>&2 echo -e "${CROSS}${RD}  No Network After $RETRY_NUM Tries${CL}"    
+    exit 1
+  fi
+done
+echo -e "${CM}${CL} \r"
+echo -en "${GN} Network Connected: ${BL}$(hostname -I)${CL} "
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Updating Container OS... "
+apt update &>/dev/null
+apt-get -qqy upgrade &>/dev/null
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Installing Dependencies... "
+apt-get install -y curl &>/dev/null
+apt-get install -y sudo &>/dev/null
+apt-get install -y git &>/dev/null
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Setting up Node.js Repository... "
+sudo curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - &>/dev/null
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Installing Node.js... "
+sudo apt-get install -y nodejs &>/dev/null
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Installing Uptime Kuma... "
+git clone https://github.com/louislam/uptime-kuma.git &>/dev/null
+mv uptime-kuma /opt/uptime-kuma
+cd /opt/uptime-kuma
+npm run setup &>/dev/null
+echo -e "${CM}${CL} \r"
+
+echo -en "${GN} Creating Service file uptime-kuma.service... "
+service_path="/etc/systemd/system/uptime-kuma.service"
+echo "[Unit]
+Description=uptime-kuma
+
+[Service]
+Type=simple
+Restart=always
+User=root
+WorkingDirectory=/opt/uptime-kuma
+ExecStart=/usr/bin/npm start
+
+[Install]
+WantedBy=multi-user.target" > $service_path
+systemctl enable --now uptime-kuma.service &>/dev/null
+echo -e "${CM}${CL} \r"
+
+PASS=$(grep -w "root" /etc/shadow | cut -b6);
+  if [[ $PASS != $ ]]; then
+echo -en "${GN} Customizing Container... "
+rm /etc/motd
+rm /etc/update-motd.d/10-uname
+touch ~/.hushlogin
+GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
+mkdir -p $(dirname $GETTY_OVERRIDE)
+cat << EOF > $GETTY_OVERRIDE
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+EOF
+systemctl daemon-reload
+systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+echo -e "${CM}${CL} \r"
+  fi
+  
+echo -en "${GN} Cleanup... "
+apt-get autoremove >/dev/null
+apt-get autoclean >/dev/null
+rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
+echo -e "${CM}${CL} \n"
