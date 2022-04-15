55a211c0e599f67edcd2e7e863e479ca924b2dc9
tteckster <tteckster@gmail.com>
2022-04-15 08:11:50 -0400

    Update zigbee2mqtt-v3.sh

--- ct/zigbee2mqtt-v3.sh
+++ ct/zigbee2mqtt-v3.sh
@@ -47,8 +47,8 @@ function default_settings() {
                 clear
                 header_info
                 echo -e "${BL}Using Default Settings${CL}"
-                echo -e "${GN}Using CT Type ${BGN}Unprivileged${CL} ${RD}NO DEVICE PASSTHROUGH${CL}"
-                CT_TYPE="1"
+                echo -e "${GN}Using CT Type ${BGN}Privileged${CL}"
+                CT_TYPE="0"
 		echo -e "${GN}Using CT Password ${BGN}Automatic Login${CL}"
 		PW=" "
 		echo -e "${GN}Using ID ${BGN}$NEXTID${CL}"
@@ -73,14 +73,14 @@ function advanced_settings() {
                 clear
                 header_info
                 echo -e "${RD}Using Advanced Settings${CL}"
-                echo -e "${YW}Type Privileged, or Press [ENTER] for Default: Unprivileged (${RD}NO DEVICE PASSTHROUGH${CL}${YW})"
+                echo -e "${YW}Type Privileged, or Press [ENTER] for Default: Privileged "
                 read CT_TYPE1
-                if [ -z $CT_TYPE1 ]; then CT_TYPE1="Unprivileged" CT_TYPE="1"; 
+                if [ -z $CT_TYPE1 ]; then CT_TYPE1="Privileged" CT_TYPE="0"; 
                 echo -en "${GN}Set CT Type ${BL}$CT_TYPE1${CL}"
                 else
-                CT_TYPE1="Privileged"
-                CT_TYPE="0"
-                echo -en "${GN}Set CT Type ${BL}Privileged${CL}"  
+                CT_TYPE1="Unprivileged"
+                CT_TYPE="1"
+                echo -en "${GN}Set CT Type ${BL}Unprivileged${CL}"  
                 fi;
 echo -e " ${CM}${CL} \r"
 sleep 1
