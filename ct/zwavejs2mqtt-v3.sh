9e96acc7ca66beabf57a305271cee50352a94384
tteckster <tteckster@gmail.com>
2022-04-17 14:31:36 -0400

    Update zwavejs2mqtt-v3.sh

--- ct/zwavejs2mqtt-v3.sh
+++ ct/zwavejs2mqtt-v3.sh
@@ -297,7 +297,7 @@ echo -en "${GN} Starting LXC Container... "
 pct start $CTID
 echo -e "${CM}${CL} \r"
 
-lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/v3/setup/zwavejs2mqtt-install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/zwavejs2mqtt-install.sh)" || exit
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
