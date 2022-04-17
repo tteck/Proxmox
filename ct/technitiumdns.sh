4838a96dfbdc7b1c06f44afa095860e97bf9e26c
tteckster <tteckster@gmail.com>
2022-04-17 15:25:05 -0400

    Update technitiumdns.sh

--- ct/technitiumdns.sh
+++ ct/technitiumdns.sh
@@ -1,12 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -ex
+set -euo pipefail
+shopt -s inherit_errexit nullglob
+
+NEXTID=$(pvesh get /cluster/nextid)
+INTEGER='^[0-9]+$'
 YW=`echo "\033[33m"`
 BL=`echo "\033[36m"`
 RD=`echo "\033[01;31m"`
-CM='\xE2\x9C\x94\033'
+BGN=`echo "\033[4;92m"`
 GN=`echo "\033[1;92m"`
+DGN=`echo "\033[32m"`
 CL=`echo "\033[m"`
-APP="Technitium DNS"
-HN=$(echo ${APP,,} | tr -d ' ')
+BFR="\\r\\033[K"
+HOLD="-"
+CM="${GN}✓${CL}"
+APP="Debian"
+NSAPP=$(echo ${APP,,} | tr -d ' ')
 while true; do
     read -p "This will create a New ${APP} LXC. Proceed(y/n)?" yn
     case $yn in
@@ -18,10 +27,9 @@ done
 clear
 function header_info {
 echo -e "${RD}
-
   _______        _           _ _   _                 
  |__   __|      | |         (_) | (_)                
-    | | ___  ___| |__  _ __  _| |_ _ _   _ _ __ ___  
+    | | ___v3___| |__  _ __  _| |_ _ _   _ _ __ ___  
     | |/ _ \/ __|  _ \|  _ \| | __| | | | |  _   _ \ 
     | |  __/ (__| | | | | | | | |_| | |_| | | | | | |
     |_|\___|\___|_| |_|_| |_|_|\__|_|\__,_|_| |_| |_|
@@ -29,174 +37,221 @@ ${CL}"
 }
 
 header_info
-show_menu(){
-    printf "    ${YW} 1)${YW} Privileged ${CL}\n"
-    printf "    ${YW} 2)${GN} Unprivileged ${CL}\n"
 
-    printf "Please choose a Install Method and hit enter or ${RD}x${CL} to exit."
-    read opt
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
 }
 
-option_picked(){
-    message1=${@:-"${CL}Error: No message passed"}
-    printf " ${YW}${message1}${CL}\n"
-}
-show_menu
-while [ "$opt" != " " ]
-    do
-      case $opt in
-        1) clear;
-            header_info;
-            option_picked "Using Privileged Install";
-            IM=0
-            break;
-        ;;
-        2) clear;
-            header_info;
-            option_picked "Using Unprivileged Install";
-            IM=1
-            break;
-        ;;
-
-        x)exit;
-        ;;
-        \n)exit;
-        ;;
-        *)clear;
-            option_picked "Please choose a Install Method from the menu";
-            show_menu;
-        ;;
-      esac
-  done
-show_menu2(){
-    printf "    ${YW} 1)${GN} Use Automatic Login ${CL}\n"
-    printf "    ${YW} 2)${GN} Use Password (changeme) ${CL}\n"
-
-    printf "Please choose a Password Type and hit enter or ${RD}x${CL} to exit."
-    read opt
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
 }
 
-option_picked(){
-    message2=${@:-"${CL}Error: No message passed"}
-    printf " ${YW}${message1}${CL}\n"
-    printf " ${YW}${message2}${CL}\n"
-}
-show_menu2
-while [ "$opt" != " " ]
-    do
-      case $opt in
-        1) clear;
-            header_info;
-            option_picked "Using Automatic Login";
-            PW=" "
-            break;
-        ;;
-        2) clear;
-            header_info;
-            option_picked "Using Password (changeme)";
-            PW="-password changeme"
-            break;
-        ;;
+function PVE_CHECK() {
+    PVE=$(pveversion | grep "pve-manager/7" | wc -l)
 
-        x)exit;
-        ;;
-        \n)exit;
-        ;;
-        *)clear;
-            option_picked "Please choose a Password Type from the menu";
-            show_menu2;
-        ;;
-      esac
-  done
-show_menu3(){
-    printf "    ${YW} 1)${GN} Automatic DHCP ${CL}\n"
-    printf "    ${YW} 2)${GN} Manual DHCP ${CL}\n"
-
-    printf "Please choose a DHCP Type and hit enter or ${RD}x${CL} to exit."
-    read opt
-}
-
-option_picked(){
-    message3=${@:-"${CL}Error: No message passed"}
-    printf " ${YW}${message1}${CL}\n"
-    printf " ${YW}${message2}${CL}\n"
-    printf " ${YW}${message3}${CL}\n"
-}
-show_menu3
-while [ "$opt" != " " ]
-    do
-      case $opt in
-        1) clear;
-            header_info;
-            option_picked "Using Automatic DHCP";
-            DHCP=" "
-            break;
-        ;;
-        2) clear;
-            header_info;
-            option_picked "Using Manual DHCP";
-            DHCP="1"
-            break;
-        ;;
-
-        x)exit;
-        ;;
-        \n)exit;
-        ;;
-        *)clear;
-            option_picked "Please choose a DHCP Type from the menu";
-            show_menu3;
-        ;;
-      esac
-  done
-
-set -o errexit
-set -o errtrace
-set -o nounset
-set -o pipefail
-shopt -s expand_aliases
-alias die='EXIT=$? LINE=$LINENO error_exit'
-trap die ERR
-trap cleanup EXIT
-
-function error_exit() {
-  trap - ERR
-  local DEFAULT='Unknown failure occured.'
-  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
-  local FLAG="\e[91m[ERROR] \e[93m$EXIT@$LINE"
-  msg "$FLAG $REASON"
-  [ ! -z ${CTID-} ] && cleanup_ctid
-  exit $EXIT
-}
-function warn() {
-  local REASON="\e[97m$1\e[39m"
-  local FLAG="\e[93m[WARNING]\e[39m"
-  msg "$FLAG $REASON"
-}
-function info() {
-  local REASON="$1"
-  local FLAG="\e[36m[INFO]\e[39m"
-  msg "$FLAG $REASON"
-}
-function msg() {
-  local TEXT="$1"
-  echo -e "$TEXT"
-}
-function cleanup_ctid() {
-  if $(pct status $CTID &>/dev/null); then
-    if [ "$(pct status $CTID | awk '{print $2}')" == "running" ]; then
-      pct stop $CTID
+    if [[ $PVE != 1 ]]; then
+        echo -e "${RD}This script requires Proxmox Virtual Environment 7.0 or greater${CL}"
+        echo -e "Exiting..."
+        sleep 2
+        exit
     fi
-    pct destroy $CTID
-  elif [ "$(pvesm list $STORAGE --vmid $CTID)" != "" ]; then
-    pvesm free $ROOTFS
-  fi
 }
-function cleanup() {
-  popd >/dev/null
-  rm -rf $TEMP_DIR
+
+function default_settings() {
+        clear
+        header_info
+        echo -e "${BL}Using Default Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}Unprivileged${CL} ${RD}NO DEVICE PASSTHROUGH${CL}"
+        CT_TYPE="1"
+	    echo -e "${DGN}Using CT Password ${BGN}Automatic Login${CL}"
+		PW=" "
+		echo -e "${DGN}Using ID ${BGN}$NEXTID${CL}"
+		CT_ID=$NEXTID
+		echo -e "${DGN}Using CT Name ${BGN}$NSAPP${CL}"
+		HN=$NSAPP
+		echo -e "${DGN}Using Disk Size ${BGN}2GB${CL}"
+		DISK_SIZE="2"
+		echo -e "${DGN}Using ${BGN}1vCPU${CL}"
+		CORE_COUNT="1"
+		echo -e "${DGN}Using ${BGN}512MiB${CL}${GN} RAM${CL}"
+		RAM_SIZE="512"
+		echo -e "${DGN}Using IP Address ${BGN}DHCP${CL}"
+		NET=dhcp
+		echo -e "${DGN}Using VLAN Tag ${BGN}NONE${CL}"
+        VLAN=" "
 }
- if [ "$IM" == "1" ]; then 
+
+function advanced_settings() {
+        clear
+        header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${YW}Type Privileged, or Press [ENTER] for Default: Unprivileged (${RD}NO DEVICE PASSTHROUGH${CL}${YW})"
+        read CT_TYPE1
+        if [ -z $CT_TYPE1 ]; then CT_TYPE1="Unprivileged" CT_TYPE="1"; 
+        echo -en "${DGN}Set CT Type ${BL}$CT_TYPE1${CL}"
+        else
+        CT_TYPE1="Privileged"
+        CT_TYPE="0"
+        echo -en "${DGN}Set CT Type ${BL}Privileged${CL}"  
+        fi;
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${YW}Set Password, or Press [ENTER] for Default: Automatic Login "
+        read PW1
+        if [ -z $PW1 ]; then PW1="Automatic Login" PW=" "; 
+        echo -en "${DGN}Set CT ${BL}$PW1${CL}"
+        else
+          PW="-password $PW1"
+        echo -en "${DGN}Set CT Password ${BL}$PW1${CL}"
+        fi;
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${YW}Enter the CT ID, or Press [ENTER] to automatically generate (${NEXTID}) "
+        read CT_ID
+        if [ -z $CT_ID ]; then CT_ID=$NEXTID; fi;
+        echo -en "${DGN}Set CT ID To ${BL}$CT_ID${CL}"
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${YW}Enter CT Name (no-spaces), or Press [ENTER] for Default: $NSAPP "
+        read CT_NAME
+        if [ -z $CT_NAME ]; then
+           HN=$NSAPP
+        else
+           HN=$(echo ${CT_NAME,,} | tr -d ' ')
+        fi
+        echo -en "${DGN}Set CT Name To ${BL}$HN${CL}"
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${YW}Enter a Disk Size, or Press [ENTER] for Default: 2Gb "
+        read DISK_SIZE
+        if [ -z $DISK_SIZE ]; then DISK_SIZE="2"; fi;
+        if ! [[ $DISK_SIZE =~ $INTEGER ]] ; then echo "ERROR! DISK SIZE MUST HAVE INTEGER NUMBER!"; exit; fi;
+        echo -en "${DGN}Set Disk Size To ${BL}$DISK_SIZE${CL}"
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${DGN}Using Disk Size ${BGN}$DISK_SIZE${CL}"
+        echo -e "${YW}Allocate CPU cores, or Press [ENTER] for Default: 1 "
+        read CORE_COUNT
+        if [ -z $CORE_COUNT ]; then CORE_COUNT="1"; fi;
+        echo -en "${DGN}Set Cores To ${BL}$CORE_COUNT${CL}"
+echo -e " ${CM}${CL} \r"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${DGN}Using Disk Size ${BGN}$DISK_SIZE${CL}"
+        echo -e "${DGN}Using ${BGN}${CORE_COUNT}vCPU${CL}"
+        echo -e "${YW}Allocate RAM in MiB, or Press [ENTER] for Default: 512 "
+        read RAM_SIZE
+        if [ -z $RAM_SIZE ]; then RAM_SIZE="512"; fi;
+        echo -en "${DGN}Set RAM To ${BL}$RAM_SIZE${CL}"
+echo -e " ${CM}${CL} \n"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${DGN}Using Disk Size ${BGN}$DISK_SIZE${CL}"
+        echo -e "${DGN}Using ${BGN}${CORE_COUNT}vCPU${CL}"
+        echo -e "${DGN}Using ${BGN}${RAM_SIZE}MiB${CL}${GN} RAM${CL}"
+        echo -e "${YW}Enter a IP Address, or Press [ENTER] for Default: DHCP "
+        read NET
+        if [ -z $NET ]; then NET="dhcp"; fi;
+        echo -en "${DGN}Set IP Address To ${BL}$NET${CL}"
+echo -e " ${CM}${CL} \n"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${DGN}Using Disk Size ${BGN}$DISK_SIZE${CL}"
+        echo -e "${DGN}Using ${BGN}${CORE_COUNT}vCPU${CL}"
+        echo -e "${DGN}Using ${BGN}${RAM_SIZE}MiB${CL}${GN} RAM${CL}"
+        echo -e "${DGN}Using IP Address ${BGN}$NET${CL}"
+        echo -e "${YW}Enter a VLAN Tag, or Press [ENTER] for Default: NONE "
+        read VLAN1
+        if [ -z $VLAN1 ]; then VLAN1="NONE" VLAN=" "; 
+        echo -en "${DGN}Set VLAN Tag To ${BL}$VLAN1${CL}"
+        else
+          VLAN="-tag $VLAN1"
+        echo -en "${DGN}Set VLAN Tag To ${BL}$VLAN1${CL}"
+        fi;
+echo -e " ${CM}${CL} \n"
+sleep 1
+clear
+header_info
+        echo -e "${RD}Using Advanced Settings${CL}"
+        echo -e "${DGN}Using CT Type ${BGN}$CT_TYPE1${CL}"
+        echo -e "${DGN}Using CT Password ${BGN}$PW1${CL}"
+        echo -e "${DGN}Using ID ${BGN}$CT_ID${CL}"
+        echo -e "${DGN}Using CT Name ${BGN}$HN${CL}"
+        echo -e "${DGN}Using Disk Size ${BGN}$DISK_SIZE${CL}"
+        echo -e "${DGN}Using ${BGN}${CORE_COUNT}vCPU${CL}"
+        echo -e "${DGN}Using ${BGN}${RAM_SIZE}MiB${CL}${GN} RAM${CL}"
+        echo -e "${DGN}Using IP Address ${BGN}$NET${CL}"
+        echo -e "${DGN}Using VLAN Tag ${BGN}$VLAN1${CL}"
+
+read -p "Are these settings correct(y/n)? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    advanced_settings
+fi
+}
+
+function start_script() {
+		echo -e "${YW}Type Advanced, or Press [ENTER] for Default Settings "
+		read SETTINGS
+		if [ -z $SETTINGS ]; then default_settings; 
+		else
+		advanced_settings 
+		fi;
+}
+
+start_script
+
+if [ "$CT_TYPE" == "1" ]; then 
  FEATURES="nesting=1,keyctl=1"
  else
  FEATURES="nesting=1"
@@ -205,19 +260,20 @@ function cleanup() {
 TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
-export CTID=$(pvesh get /cluster/nextid)
+export CTID=$CT_ID
 export PCT_OSTYPE=debian
 export PCT_OSVERSION=11
-export PCT_DISK_SIZE=2
+export PCT_DISK_SIZE=$DISK_SIZE
 export PCT_OPTIONS="
   -features $FEATURES
   -hostname $HN
-  -net0 name=eth0,bridge=vmbr0,ip=dhcp
+  -net0 name=eth0,bridge=vmbr0,ip=$NET
+  $VLAN
   -onboot 1
-  -cores 1
-  -memory 512
-  -unprivileged ${IM}
-  ${PW}
+  -cores $CORE_COUNT
+  -memory $RAM_SIZE
+  -unprivileged $CT_TYPE
+  $PW
 "
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/ct/create_lxc.sh)" || exit
 
@@ -225,35 +281,15 @@ STORAGE_TYPE=$(pvesm status -storage $(pct config $CTID | grep rootfs | awk -F "
 if [ "$STORAGE_TYPE" == "zfspool" ]; then
   warn "Some addons may not work due to ZFS not supporting 'fallocate'."
 fi
-LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
-cat <<EOF >> $LXC_CONFIG
-lxc.cgroup2.devices.allow: a
-lxc.cap.drop:
-EOF
-if [ "$DHCP" == "1" ]; then
-MAC=$(pct config $CTID \
-| grep -i hwaddr \
-| awk '{print substr($2, 31, length($3) 17 ) }') \
 
-echo -e "MAC Address ${BL}$MAC${CL}"
-
-dhcp_reservation(){
-    printf "Please set DHCP reservation and press Enter."
-    read
-}
-dhcp_reservation
-fi
-
-echo -en "${GN} Starting LXC Container... "
+msg_info "Starting LXC Container"
 pct start $CTID
-echo -e "${CM}${CL} \r"
+msg_ok "Started LXC Container"
 
-alias lxc-cmd="lxc-attach -n $CTID --"
-
-lxc-cmd bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/$HN-install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/technitiumdns-install.sh)" || exit
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
-echo -e "${GN}Successfully created ${APP} LXC to${CL} ${BL}$CTID${CL}. 
-${BL}${APP}${CL} should be reachable by going to the following URL.
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
          ${BL}http://${IP}:5380${CL} \n"
