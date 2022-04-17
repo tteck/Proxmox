777479aedc74f14dca82ab60a0100f20720cb44f
tteckster <tteckster@gmail.com>
2022-04-17 14:32:14 -0400

    Update zigbee2mqtt-v3.sh

--- ct/zigbee2mqtt-v3.sh
+++ ct/zigbee2mqtt-v3.sh
@@ -298,7 +298,7 @@ msg_info "Starting LXC Container"
 pct start $CTID
 msg_ok "Started LXC Container"
 
-lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/v3/setup/zigbee2mqtt-install.sh)" || exit
+lxc-attach -n $CTID -- bash -c "$(wget -qLO - https://raw.githubusercontent.com/tteck/Proxmox/main/setup/zigbee2mqtt-install.sh)" || exit
 
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
 
