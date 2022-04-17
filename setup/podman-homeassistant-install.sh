b8353e346cdebb85cfde40cfff54622926040123
tteckster <tteckster@gmail.com>
2022-04-17 18:00:51 -0400

    Create podman-homeassistant-install.sh

---
+++ setup/podman-homeassistant-install.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash -ex
+set -euo pipefail
+shopt -s inherit_errexit nullglob
+
+YW=`echo "\033[33m"`
+RD=`echo "\033[01;31m"`
+BL=`echo "\033[36m"`
+GN=`echo "\033[1;92m"`
+CL=`echo "\033[m"`
+RETRY_NUM=10
+RETRY_EVERY=3
+NUM=$RETRY_NUM
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+BFR="\\r\\033[K"
+HOLD="-"
+
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+msg_info "Setting up Container OS "
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+locale-gen >/dev/null
+while [ "$(hostname -I)" = "" ]; do
+  1>&2 echo -en "${CROSS}${RD}  No Network! "
+  sleep $RETRY_EVERY
+  ((NUM--))
+  if [ $NUM -eq 0 ]
+  then
+    1>&2 echo -e "${CROSS}${RD}  No Network After $RETRY_NUM Tries${CL}"    
+    exit 1
+  fi
+done
+msg_ok "Set up Container OS"
+msg_ok "Network Connected: ${BL}$(hostname -I)"
+
+msg_info "Updating Container OS"
+apt update &>/dev/null
+apt-get -qqy upgrade &>/dev/null
+msg_ok "Updated Container OS"
+
+msg_info "Installing Dependencies"
+apt-get install -y curl &>/dev/null
+apt-get install -y sudo &>/dev/null
+apt-get install -y runc &>/dev/null
+msg_ok "Installed Dependencies"
+
+get_latest_release() {
+   curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+CORE_LATEST_VERSION=$(get_latest_release "home-assistant/core")
+YACHT_LATEST_VERSION=$(get_latest_release "selfhostedpro/yacht")
+
+msg_info "Installing Podman"
+apt-get -y install podman &>/dev/null
+msg_ok "Installed Podman"
+
+msg_info "Pulling Yacht $YACHT_LATEST_VERSION Image"
+podman pull docker.io/selfhostedpro/yacht:latest &>/dev/null
+msg_ok "Pulled Yacht $YACHT_LATEST_VERSION Image"
+
+msg_info "Installing Yacht $YACHT_LATEST_VERSION"
+podman volume create yacht >/dev/null
+podman run -d \
+  --privileged \
+  --name yacht \
+  --restart always \
+  -v /var/run/podman/podman.sock:/var/run/docker.sock \
+  -v yacht:/config \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/timezone:/etc/timezone:ro \
+  -p 8000:8000 \
+  selfhostedpro/yacht:latest &>/dev/null
+msg_ok "Installed Yacht $YACHT_LATEST_VERSION"
+
+msg_info "Pulling Home Assistant $CORE_LATEST_VERSION Image"
+podman pull docker.io/homeassistant/home-assistant:stable &>/dev/null
+msg_ok "Pulled Home Assistant $CORE_LATEST_VERSION Image"
+
+msg_info "Installing Home Assistant $CORE_LATEST_VERSION"
+podman volume create hass_config >/dev/null
+podman run -d \
+  --privileged \
+  --name homeassistant \
+  --restart unless-stopped \
+  -v /dev:/dev \
+  -v hass_config:/config \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /etc/timezone:/etc/timezone:ro \
+  --net=host \
+  homeassistant/home-assistant:stable &>/dev/null
+msg_ok "Installed Home Assistant $CORE_LATEST_VERSION"
+
+PASS=$(grep -w "root" /etc/shadow | cut -b6);
+  if [[ $PASS != $ ]]; then
+msg_info "Customizing Container"
+rm /etc/motd
+rm /etc/update-motd.d/10-uname
+touch ~/.hushlogin
+GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
+mkdir -p $(dirname $GETTY_OVERRIDE)
+cat << EOF > $GETTY_OVERRIDE
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+EOF
+systemctl daemon-reload
+systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+msg_ok "Customized Container"
+  fi
+
+msg_info "Cleaning up"
+apt-get autoremove >/dev/null
+apt-get autoclean >/dev/null
+rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
+msg_ok "Cleaned"
