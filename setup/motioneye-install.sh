064d3d54bfa47c16822caf5116078f7e3279574e
tteckster <tteckster@gmail.com>
2022-04-18 07:33:08 -0400

    Update motioneye-install.sh

--- setup/motioneye-install.sh
+++ setup/motioneye-install.sh
@@ -1,38 +1,30 @@
-#!/usr/bin/env bash
-
-set -o errexit 
-set -o errtrace 
-set -o nounset 
-set -o pipefail 
-shopt -s expand_aliases
-alias die='EXIT=$? LINE=$LINENO error_exit'
-trap die ERR
-trap 'die "Script interrupted."' INT
-
-function error_exit() {
-  trap - ERR
-  local DEFAULT='Unknown failure occured.'
-  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
-  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
-  msg "$FLAG $REASON"
-  exit $EXIT
-}
-function msg() {
-  local TEXT="$1"
-  echo -e "$TEXT"
-}
-
+#!/usr/bin/env bash -ex
+set -euo pipefail
+shopt -s inherit_errexit nullglob
+YW=`echo "\033[33m"`
 RD=`echo "\033[01;31m"`
 BL=`echo "\033[36m"`
 GN=`echo "\033[1;92m"`
 CL=`echo "\033[m"`
-CM="${GN}✓${CL}"
-CROSS="${RD}✗${CL}"
 RETRY_NUM=10
 RETRY_EVERY=3
 NUM=$RETRY_NUM
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+BFR="\\r\\033[K"
+HOLD="-"
 
-echo -en "${GN} Setting up Container OS... "
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+msg_info "Setting up Container OS "
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
 while [ "$(hostname -I)" = "" ]; do
@@ -45,16 +37,18 @@ while [ "$(hostname -I)" = "" ]; do
     exit 1
   fi
 done
-echo -e "${CM}${CL} \r"
-echo -en "${GN} Network Connected: ${BL}$(hostname -I)${CL} "
-echo -e "${CM}${CL} \r"
+msg_ok "Set up Container OS"
+msg_ok "Network Connected: ${BL}$(hostname -I)"
 
-echo -en "${GN} Installing Dependencies... "
-apt-get update &>/dev/null
-apt-get -qqy install \
-    curl \
-    sudo &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_info "Updating Container OS"
+apt update &>/dev/null
+apt-get -qqy upgrade &>/dev/null
+msg_ok "Updated Container OS"
+
+msg_info "Installing Dependencies"
+apt-get install -y curl &>/dev/null
+apt-get install -y sudo &>/dev/null
+msg_ok "Installed Dependencies"
 
 echo -en "${GN} Installing Motion... "
  apt-get install motion -y &>/dev/null
@@ -62,36 +56,36 @@ echo -en "${GN} Installing Motion... "
  systemctl disable motion &>/dev/null
 echo -e "${CM}${CL} \r"
 
-echo -en "${GN} Installing FFmpeg... "
+msg_info "Installing FFmpeg"
  apt-get install ffmpeg v4l-utils -y &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Installed FFmpeg"
 
-echo -en "${GN} Installing Python... "
+msg_info "Installing Python"
  apt-get update &>/dev/null
  apt-get install python2 -y &>/dev/null
  curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py &>/dev/null
  python2 get-pip.py &>/dev/null
  apt-get install libffi-dev libzbar-dev libzbar0 -y &>/dev/null
  apt-get install python2-dev libssl-dev libcurl4-openssl-dev libjpeg-dev -y &>/dev/null
- echo -e "${CM}${CL} \r"
+ msg_ok "Installed Python"
  
-echo -en "${GN} Installing MotionEye... "
+msg_info "Installing MotionEye"
  apt-get update &>/dev/null
  sudo pip install motioneye &>/dev/null
  mkdir -p /etc/motioneye
  cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
  mkdir -p /var/lib/motioneye
-echo -e "${CM}${CL} \r"
+msg_ok "Installed MotionEye"
 
-echo -en "${GN} Creating Service file motioneye.service... " 
+msg_info "Creating Service" 
  cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service &>/dev/null
  systemctl enable motioneye &>/dev/null
  systemctl start motioneye 
-echo -e "${CM}${CL} \r"
+msg_ok "Created Service" 
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
   if [[ $PASS != $ ]]; then
-echo -en "${GN} Customizing Container... "
+msg_info "Customizing Container"
 rm /etc/motd
 rm /etc/update-motd.d/10-uname
 touch ~/.hushlogin
@@ -104,11 +98,11 @@ ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,3840
 EOF
 systemctl daemon-reload
 systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-echo -e "${CM}${CL} \r"
+msg_ok "Customized Container"
   fi
   
-echo -en "${GN} Cleanup... "
+msg_info "Cleaning up"
 apt-get autoremove >/dev/null
 apt-get autoclean >/dev/null
 rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
-echo -e "${CM}${CL} \n"
+msg_ok "Cleaned"
