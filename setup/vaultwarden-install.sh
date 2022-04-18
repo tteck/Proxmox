8cb2ec9433b40e3dc4ec7a5b7809b072dd994d7d
tteckster <tteckster@gmail.com>
2022-04-18 07:16:55 -0400

    Update vaultwarden-install.sh

--- setup/vaultwarden-install.sh
+++ setup/vaultwarden-install.sh
@@ -1,38 +1,30 @@
-#!/usr/bin/env bash
-
-set -o errexit 
-set -o errtrace 
-set -o nounset 
-set -o pipefail 
-shopt -s expand_aliases
-alias die='EXIT=$? LINE=$LINENO error_exit'
-trap die ERR
-trap 'die "Script interrupted."' INT
-
-function error_exit() {
-  trap - ERR
-  local DEFAULT='Unknown failure occured.'
-  local REASON="\e[97m${1:-$DEFAULT}\e[39m"
-  local FLAG="\e[91m[ERROR:LXC] \e[93m$EXIT@$LINE"
-  msg "$FLAG $REASON"
-  exit $EXIT
-}
-function msg() {
-  local TEXT="$1"
-  echo -e "$TEXT"
-}
-
+#!/usr/bin/env bash -ex
+set -euo pipefail
+shopt -s inherit_errexit nullglob
+YW=`echo "\033[33m"`
 RD=`echo "\033[01;31m"`
 BL=`echo "\033[36m"`
 GN=`echo "\033[1;92m"`
 CL=`echo "\033[m"`
-CM="${GN}✓${CL}"
-CROSS="${RD}✗${CL}"
 RETRY_NUM=10
 RETRY_EVERY=3
 NUM=$RETRY_NUM
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+BFR="\\r\\033[K"
+HOLD="-"
 
-echo -en "${GN} Setting up Container OS... "
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+msg_info "Setting up Container OS "
 sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
 locale-gen >/dev/null
 while [ "$(hostname -I)" = "" ]; do
@@ -45,16 +37,15 @@ while [ "$(hostname -I)" = "" ]; do
     exit 1
   fi
 done
-echo -e "${CM}${CL} \r"
-echo -en "${GN} Network Connected: ${BL}$(hostname -I)${CL} "
-echo -e "${CM}${CL} \r"
+msg_ok "Set up Container OS"
+msg_ok "Network Connected: ${BL}$(hostname -I)"
 
-echo -en "${GN} Updating Container OS... "
-apt-get update &>/dev/null
+msg_info "Updating Container OS"
+apt update &>/dev/null
 apt-get -qqy upgrade &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Updated Container OS"
 
-echo -en "${GN} Installing Dependencies... "
+msg_info "Installing Dependencies"
 apt-get update &>/dev/null
 apt-get -qqy install \
     git \
@@ -67,36 +58,36 @@ apt-get -qqy install \
     libssl-dev \
     curl \
     sudo &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Installed Dependencies"
 
-echo -en "${GN} Installing Build Essentials... "
+msg_info "Installing Build Essentials"
 apt-get install -y build-essential &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Installed Build Essentials"
 
-echo -en "${GN} Installing Rust... "
+msg_info "Installing Rust"
 curl https://sh.rustup.rs -sSf | sh -s -- -y &>/dev/null
 echo 'export PATH=~/.cargo/bin:$PATH' >> ~/.bashrc &>/dev/null
 export PATH=~/.cargo/bin:$PATH &>/dev/null
 which rustc &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Installed Rust"
 
-echo -en "${GN} Installing Node.js... "
+msg_info "Installing Node.js"
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &>/dev/null
 apt-get install -y nodejs &>/dev/null
 npm -g install npm@7 &>/dev/null
 which npm &>/dev/null
 npm i npm@latest -g &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Installed Node.js"
 
-echo -en "${GN} Building Vaultwarden (Patience)... "
+msg_info "Building Vaultwarden (Patience)"
 git clone https://github.com/dani-garcia/vaultwarden &>/dev/null
 pushd vaultwarden &>/dev/null
 cargo clean &>/dev/null 
 cargo build --features sqlite --release &>/dev/null
 file target/release/vaultwarden &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Built Vaultwarden"
 
-echo -en "${GN} Building Web-Vault... "
+msg_info "Building Web-Vault"
 pushd target/release/ &>/dev/null
 git clone --recurse-submodules https://github.com/bitwarden/web.git web-vault.git &>/dev/null
 cd web-vault.git &>/dev/null
@@ -110,9 +101,9 @@ npm run --silent dist:oss:selfhost &>/dev/null
 cp -a build ../web-vault &>/dev/null
 cd ..
 mkdir data 
-echo -e "${CM}${CL} \r"
+msg_ok "Built Web-Vault"
 
-echo -en "${GN} Create Systemd Service... "
+msg_info "Creating Service"
 cp ../../.env.template /etc/vaultwarden.env &>/dev/null
 cp vaultwarden /usr/bin/vaultwarden &>/dev/null
 chmod +x /usr/bin/vaultwarden &>/dev/null
@@ -145,11 +136,11 @@ WantedBy=multi-user.target" > $service_path
 systemctl daemon-reload
 systemctl enable vaultwarden.service &>/dev/null
 systemctl start vaultwarden.service &>/dev/null
-echo -e "${CM}${CL} \r"
+msg_ok "Created Service"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
   if [[ $PASS != $ ]]; then
-echo -en "${GN} Customizing Container... "
+msg_info "Customizing Container"
 rm /etc/motd
 rm /etc/update-motd.d/10-uname
 touch ~/.hushlogin
@@ -162,11 +153,11 @@ ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,3840
 EOF
 systemctl daemon-reload
 systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
-echo -e "${CM}${CL} \r"
+msg_ok "Customized Container"
   fi
   
-echo -en "${GN} Cleanup... "
+msg_info "Cleaning up"
 apt-get autoremove >/dev/null
 apt-get autoclean >/dev/null
 rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
-echo -e "${CM}${CL} \n"
+msg_ok "Cleaned"
