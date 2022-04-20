45ba48dcb64249bd3fdb6d7fc112348d57e6a7ab
tteckster <tteckster@gmail.com>
2022-04-20 16:08:49 -0400

    Update zigbee2mqtt-v3.sh

--- ct/zigbee2mqtt-v3.sh
+++ ct/zigbee2mqtt-v3.sh
@@ -79,6 +79,8 @@ function default_settings() {
 	CORE_COUNT="2"
 	echo -e "${DGN}Using ${BGN}1024MiB${CL}${DGN} RAM${CL}"
 	RAM_SIZE="1024"
+	echo -e "${DGN}Using Bridge ${BGN}vmbr0${CL}"
+	BRG="vmbr0"
 	echo -e "${DGN}Using Static IP Address ${BGN}DHCP${CL}"
 	NET=dhcp
 	echo -e "${DGN}Using Gateway Address ${BGN}NONE${CL}"
