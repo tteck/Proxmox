428e3534843b0b99e957184a5bfd838381bbf240
tteckster <tteckster@gmail.com>
2022-04-21 11:13:16 -0400

    Update podman-homeassistant-v3.sh

--- ct/podman-homeassistant-v3.sh
+++ ct/podman-homeassistant-v3.sh
@@ -96,7 +96,7 @@ function advanced_settings() {
         clear
         header_info
         echo -e "${RD}Using Advanced Settings${CL}"
-        echo -e "${YW}Type ${CROSS}Unprivileged, or Press [ENTER] for Default: Privileged"
+        echo -e "${YW}Type ${CROSS}${YW}Unprivileged, or Press [ENTER] for Default: Privileged"
         read CT_TYPE1
         if [ -z $CT_TYPE1 ]; then CT_TYPE1="Unprivileged" CT_TYPE="0"; 
         echo -en "${DGN}Set CT Type ${BL}$CT_TYPE1${CL}"
