1f969e914cb3b316e2ef7428bacaf3a32c7fe318
tteckster <tteckster@gmail.com>
2022-04-24 04:31:37 -0400

    Update internet-check.sh

--- misc/internet-check.sh
+++ misc/internet-check.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash -ex
+RD=`echo "\033[01;31m"`
+CL=`echo "\033[m"`
+GN=`echo "\033[1;92m"`
 CM="${GN}✓${CL}"
 CROSS="${RD}✗${CL}"
 wget -q --tries=10 --timeout=5 --spider http://google.com
