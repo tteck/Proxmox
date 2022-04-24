85beb59b11943a26cd7410700dd9b2868003e060
tteckster <tteckster@gmail.com>
2022-04-24 04:34:12 -0400

    Update internet-check.sh

--- misc/internet-check.sh
+++ misc/internet-check.sh
@@ -6,8 +6,8 @@ CM="${GN}✓${CL}"
 CROSS="${RD}✗${CL}"
 wget -q --tries=10 --timeout=5 --spider http://google.com
 if [[ $? -eq 0 ]]; then
-        echo -e "${CM} Internet Online"
+        echo -e " ${CM} Internet Online"
 else
-        echo -e "${CROSS} Internet Offline"
+        echo -e " ${CROSS} Internet Offline"
 fi
 # bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/internet-check.sh)"
