6701c30d8236eb49208e77320a72697ad5cdacc0
tteckster <tteckster@gmail.com>
2022-04-28 21:04:37 -0400

    Update wireguard-install.sh

--- setup/wireguard-install.sh
+++ setup/wireguard-install.sh
@@ -54,15 +54,6 @@ done
 msg_ok "Set up Container OS"
 msg_ok "Network Connected: ${BL}$(hostname -I)"
 
-msg_info "Checking Internet Status"
-INTER=$(wget -q --tries=10 --timeout=5 --spider https://google.com)
-sleep 2
-if [[ "$INTER" != "0" ]]; then
-        msg_ok "Internet Online"
-else
-        echo -e "${BFR} ${CROSS}${RD} Internet Offline${CL}"
-fi
-
 OPTIONS_PATH='/options.conf'
 cat >$OPTIONS_PATH <<'EOF'
 IPv4dev=eth0
