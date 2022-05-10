ed6a25180b532f7c09a158005560385c00790dea
tteckster <tteckster@gmail.com>
2022-05-10 08:22:06 -0400

    Update deconz-v3.sh

--- ct/deconz-v3.sh
+++ ct/deconz-v3.sh
@@ -111,7 +111,7 @@ function advanced_settings() {
         clear
         header_info
         echo -e "${RD}Using Advanced Settings${CL}"
-        echo -e "${YW}Type Privileged, or Press [ENTER] for Default: Privileged (${RD}NO DEVICE PASSTHROUGH${CL}${YW})"
+        echo -e "${YW}Type Unprivileged, or Press [ENTER] for Default: Privileged"
         read CT_TYPE1
         if [ -z $CT_TYPE1 ]; then CT_TYPE1="Privileged" CT_TYPE="0"; 
         echo -en "${DGN}Set CT Type ${BL}$CT_TYPE1${CL}"
