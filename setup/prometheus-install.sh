fb4781cbb7159a6d16190520259663d3bca58ec4
tteckster <tteckster@gmail.com>
2022-06-30 06:53:09 -0400

    Update prometheus-install.sh

--- setup/prometheus-install.sh
+++ setup/prometheus-install.sh
@@ -118,5 +118,5 @@ msg_ok "Customized Container"
 msg_info "Cleaning up"
 apt-get autoremove >/dev/null
 apt-get autoclean >/dev/null
-rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
+rm -rf /var/{cache,log}/* /var/lib/apt/lists/* /root/prometheus-2.36.2.linux-amd64  /root/prometheus-2.36.2.linux-amd64.tar.gz
 msg_ok "Cleaned"
