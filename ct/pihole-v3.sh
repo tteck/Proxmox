12c79ae15edf21a36eb5e970b5d9cbaea383bc10
tteckster <tteckster@gmail.com>
2022-07-11 07:23:26 -0400

    Update pihole-v3.sh

--- ct/pihole-v3.sh
+++ ct/pihole-v3.sh
@@ -77,8 +77,8 @@ function default_settings() {
         clear
         header_info
         echo -e "${BL}Using Default Settings${CL}"
-        echo -e "${DGN}Using CT Type ${BGN}Unprivileged${CL} ${RD}NO DEVICE PASSTHROUGH${CL}"
-        CT_TYPE="1"
+        echo -e "${DGN}Using CT Type ${BGN}Privileged${CL}"
+        CT_TYPE="0"
         echo -e "${DGN}Using CT Password ${BGN}Automatic Login${CL}"
         PW=" "
         echo -e "${DGN}Using CT ID ${BGN}$NEXTID${CL}"
@@ -105,14 +105,14 @@ function advanced_settings() {
         clear
         header_info
         echo -e "${RD}Using Advanced Settings${CL}"
-        echo -e "${YW}Type Privileged, or Press [ENTER] for Default: Unprivileged (${RD}NO DEVICE PASSTHROUGH${CL}${YW})"
+        echo -e "${YW}Type Unprivileged, or Press [ENTER] for Default: Privileged"
         read CT_TYPE1
-        if [ -z $CT_TYPE1 ]; then CT_TYPE1="Unprivileged" CT_TYPE="1"; 
+        if [ -z $CT_TYPE1 ]; then CT_TYPE1="Privileged" CT_TYPE="0"; 
         echo -en "${DGN}Set CT Type ${BL}$CT_TYPE1${CL}"
         else
-        CT_TYPE1="Privileged"
-        CT_TYPE="0"
-        echo -en "${DGN}Set CT Type ${BL}Privileged${CL}"  
+        CT_TYPE1="Unprivileged"
+        CT_TYPE="1"
+        echo -en "${DGN}Set CT Type ${BL}Unprivileged${CL}"  
         fi;
 echo -e " ${CM}${CL} \r"
 sleep 1
@@ -348,4 +348,4 @@ IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP}${CL} should be reachable by going to the following URL.
-         ${BL}http://${IP}${CL} \n"
+         ${BL}http://${IP}/admin${CL} \n"
