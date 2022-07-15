9fa467647a696dc36e04b5e9fafe051617c5a72d
tteckster <tteckster@gmail.com>
2022-07-15 12:55:17 -0400

    Update haos-vm-v3.sh

--- vm/haos-vm-v3.sh
+++ vm/haos-vm-v3.sh
@@ -337,7 +337,8 @@ qm set $VMID \
   -scsi0 ${DISK1_REF},size=32G >/dev/null
 qm set $VMID \
   -boot order=scsi0 >/dev/null
-
+qm set $VMID -description "# Home Assistant OS
+### https://github.com/tteck/Proxmox" >/dev/null
 msg_ok "Created HAOS VM ${CL}${BL}${VM_NAME}"
 
 if [ "$START_VM" == "yes" ]; then
