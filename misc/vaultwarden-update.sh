471c5759430f8c5b0e4cb5866d697dba664f586f
tteckster <tteckster@gmail.com>
2022-07-17 15:24:14 -0400

    Update vaultwarden-update.sh

--- misc/vaultwarden-update.sh
+++ misc/vaultwarden-update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 VAULT=$(curl -s https://api.github.com/repos/dani-garcia/vaultwarden/releases/latest \
 | grep "tag_name" \
 | awk '{print substr($2, 2, length($2)-3) }') \
