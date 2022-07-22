35fdc52d335f0bff960da601f2b52f0a3124e5da
Kickbut101 <bansheedriver400@hotmail.com>
2022-07-22 13:47:56 -0500

    Update ombi-install.sh

--- setup/ombi-install.sh
+++ setup/ombi-install.sh
@@ -55,7 +55,7 @@ msg_ok "Set up Container OS"
 msg_ok "Network Connected: ${BL}$(hostname -I)"
 
 msg_info "Setting up Ombi Repository and Key"
-echo "deb https://apt.ombi.app/master jessie main" | sudo tee /etc/apt/sources.list.d/ombi.list &>/dev/null
+echo "deb https://apt.ombi.app/master $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/ombi.list &>/dev/null
 curl -sSL https://apt.ombi.app/pub.key | sudo apt-key add - &>/dev/null
 msg_ok "Set up Ombi Repository and Key"
 
