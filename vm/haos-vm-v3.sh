ba2e96b7baa5f9be6096edf1e25539b0b3a91157
tteckster <tteckster@gmail.com>
2022-07-24 09:31:40 -0400

    Update haos-vm-v3.sh

--- vm/haos-vm-v3.sh
+++ vm/haos-vm-v3.sh
@@ -336,7 +336,7 @@ pvesm alloc $STORAGE $VMID $DISK0 4M 1>&/dev/null
 qm importdisk $VMID ${FILE%.*} $STORAGE ${DISK_IMPORT:-} 1>&/dev/null
 qm set $VMID \
   -efidisk0 ${DISK0_REF},efitype=4m,size=4M \
-  -scsi0 ${DISK1_REF},size=32G >/dev/null
+  -scsi0 ${DISK1_REF},discard=on,size=32G,ssd=1 >/dev/null
 qm set $VMID \
   -boot order=scsi0 >/dev/null
 qm set $VMID -description "# Home Assistant OS
