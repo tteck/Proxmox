d93c146b87e4fde47d86b11d32984817cfada295
tteckster <tteckster@gmail.com>
2022-08-23 08:36:08 -0400

    v5.5.6

--- setup/omada-install.sh
+++ setup/omada-install.sh
@@ -69,9 +69,9 @@ wget -qL https://repo.mongodb.org/apt/ubuntu/dists/bionic/mongodb-org/3.6/multiv
 sudo dpkg -i mongodb-org-server_3.6.23_amd64.deb &>/dev/null
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Omada Controller v5.4.6"
-wget -qL https://static.tp-link.com/upload/software/2022/202207/20220729/Omada_SDN_Controller_v5.4.6_Linux_x64.deb
-sudo dpkg -i Omada_SDN_Controller_v5.4.6_Linux_x64.deb &>/dev/null
+msg_info "Installing Omada Controller v5.5.6"
+wget -qL https://static.tp-link.com/upload/software/2022/202208/20220822/Omada_SDN_Controller_v5.5.6_Linux_x64.deb
+sudo dpkg -i Omada_SDN_Controller_v5.5.6_Linux_x64.deb &>/dev/null
 msg_ok "Installed Omada Controller"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
