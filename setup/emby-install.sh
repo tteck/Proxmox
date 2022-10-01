5f75108ff3cdab459688c756c49a0fea21a8c0ba
tteckster <tteckster@gmail.com>
2022-09-30 21:52:08 -0400

    Update emby-install.sh

--- setup/emby-install.sh
+++ setup/emby-install.sh
@@ -87,8 +87,8 @@ msg_ok "Set Up Hardware Acceleration"
 LATEST=$(curl -sL https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
 
 msg_info "Installing Emby"
-wget https://github.com/MediaBrowser/Emby.Releases/releases/download/${LATEST}/emby-server-deb_${LATEST}_amd64.deb &>/dev/null
-dpkg -i emby-server-deb_${LATEST}_amd64.deb &>/dev/null
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.7.6.0/emby-server-deb_4.7.6.0_amd64.deb &>/dev/null
+dpkg -i emby-server-deb_4.7.6.0_amd64.deb &>/dev/null
 msg_ok "Installed Emby"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6);
