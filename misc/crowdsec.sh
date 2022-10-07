606d825c5761ccab3207ee6b13e7bbb6776a968f
tteckster <tteckster@gmail.com>
2022-10-07 15:26:16 -0400

    Update crowdsec.sh
    add curl install

--- misc/crowdsec.sh
+++ misc/crowdsec.sh
@@ -62,6 +62,7 @@ function msg_ok() {
 
 msg_info "Setting up ${APP} Repository"
 apt-get update &>/dev/null
+apt-get install -y curl &>/dev/null
 apt-get install -y gnupg &>/dev/null
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash &>/dev/null
 msg_ok "Setup ${APP} Repository"
