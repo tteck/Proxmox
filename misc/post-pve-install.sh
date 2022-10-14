c97a5d57258fe320d5b2903adbec29d3e892d091
tteckster <tteckster@gmail.com>
2022-10-14 11:51:45 -0400

    Update post-pve-install.sh
    mention to delete browser cache

--- misc/post-pve-install.sh
+++ misc/post-pve-install.sh
@@ -107,7 +107,7 @@ then
 msg_info "Disabling Subscription Nag"
 echo "DPkg::Post-Invoke { \"dpkg -V proxmox-widget-toolkit | grep -q '/proxmoxlib\.js$'; if [ \$? -eq 1 ]; then { echo 'Removing subscription nag from UI...'; sed -i '/data.status/{s/\!//;s/Active/NoMoreNagging/}' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js; }; fi\"; };" > /etc/apt/apt.conf.d/no-nag-script
 apt --reinstall install proxmox-widget-toolkit &>/dev/null
-msg_ok "Disabled Subscription Nag"
+msg_ok "Disabled Subscription Nag (Delete browser cache)"
 fi
 
 read -r -p "Update Proxmox VE 7 now? <y/N> " prompt
