0beaa20971301c255a7a4ba2661afcb1af49c6c8
tteckster <tteckster@gmail.com>
2022-10-22 05:52:33 -0400

    Maintenance (#662)

--- setup/wireguard-install.sh
+++ setup/wireguard-install.sh
@@ -59,9 +59,13 @@ done
 msg_ok "Set up Container OS"
 msg_ok "Network Connected: ${BL}$(hostname -I)"
 
+set +e
+alias die=''
 if nc -zw1 8.8.8.8 443; then  msg_ok "Internet Connected"; else  msg_error "Internet NOT Connected"; exit 1; fi;
 RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure";  else msg_ok "DNS Resolved github.com to $RESOLVEDIP";  fi;
+alias die='EXIT=$? LINE=$LINENO error_exit'
+set -e
 
 OPTIONS_PATH='/options.conf'
 cat >$OPTIONS_PATH <<'EOF'
