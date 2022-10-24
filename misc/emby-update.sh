35ea3211078016a347083109ab883a4038832aad
tteckster <tteckster@gmail.com>
2022-10-24 17:24:31 -0400

    Create emby-update.sh

---
+++ misc/emby-update.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+LATEST=$(curl -sL https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+YW=`echo "\033[33m"`
+BL=`echo "\033[36m"`
+RD=`echo "\033[01;31m"`
+BGN=`echo "\033[4;92m"`
+GN=`echo "\033[1;92m"`
+DGN=`echo "\033[32m"`
+CL=`echo "\033[m"`
+BFR="\\r\\033[K"
+HOLD="-"
+CM="${GN}✓${CL}"
+APP="Emby"
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+
+function error_exit() {
+  trap - ERR
+  local reason="Unknown failure occured."
+  local msg="${1:-$reason}"
+  local flag="${RD}‼ ERROR ${CL}$EXIT@$LINE"
+  echo -e "$flag $msg" 1>&2
+  exit $EXIT
+}
+
+while true; do
+    read -p "This will Update ${APP} to ${LATEST}. Proceed(y/n)?" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+clear
+function header_info {
+cat << "EOF"
+    ______          __         
+   / ____/___ ___  / /_  __  __
+  / __/ / __  __ \/ __ \/ / / /
+ / /___/ / / / / / /_/ / /_/ / 
+/_____/_/ /_/ /_/_.___/\__, /  
+         UPDATE       /____/   
+EOF
+}
+
+header_info
+
+function msg_info() {
+    local msg="$1"
+    echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+    local msg="$1"
+    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+msg_info "Updating ${APP}"
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/${LATEST}/emby-server-deb_${LATEST}_amd64.deb &>/dev/null
+systemctl stop emby-server
+dpkg -i emby-server-deb_${LATEST}_amd64.deb &>/dev/null
+systemctl start emby-server
+rm emby-server-deb_${LATEST}_amd64.deb
+msg_ok "Updated ${APP}"
