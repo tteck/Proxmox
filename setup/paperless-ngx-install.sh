b0f5898f699c987aaf18a380fad698ac70eb1f4b
tteckster <tteckster@gmail.com>
2022-12-22 08:48:29 -0500

    Update to allow continue despite network issues (#855) (#856)
    Change to allow install to continue like it does for Postgres
    
    Co-authored-by: ThellraAK <github.com@absurdlybored.com>

--- setup/paperless-ngx-install.sh
+++ setup/paperless-ngx-install.sh
@@ -61,8 +61,14 @@ msg_ok "Network Connected: ${BL}$(hostname -I)"
 set +e
 alias die=''
 if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
-	msg_error "Internet NOT Connected"
-	exit 1
+  msg_error "Internet NOT Connected"
+    read -r -p "Would you like to continue anyway? <y/N> " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+      echo -e " ⚠️  ${RD}Expect Issues Without Internet${CL}"
+    else
+      echo -e " 🖧  Check Network Settings"
+      exit 1
+    fi
 fi
 RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure"; else msg_ok "DNS Resolved github.com to $RESOLVEDIP"; fi
