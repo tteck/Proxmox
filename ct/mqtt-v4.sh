5bc7e2fc7c90950e90ed65c2c7fe29ee2e3ab221
tteckster <tteckster@gmail.com>
2022-12-23 03:06:24 -0500

    Update mqtt-v4.sh

--- ct/mqtt-v4.sh
+++ ct/mqtt-v4.sh
@@ -255,6 +255,11 @@ fi
 }
 clear
 start_script
+if [ "$CT_TYPE" == "1" ]; then 
+ FEATURES="nesting=1,keyctl=1"
+ else
+ FEATURES="nesting=1"
+ fi
 if [ "$VERB" == "yes" ]; then set -x; fi
 if [ "$CT_TYPE" == "1" ]; then
   FEATURES="nesting=1,keyctl=1"
