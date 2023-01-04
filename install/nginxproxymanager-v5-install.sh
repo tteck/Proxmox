2fc71ff407c520854496caeeff19771d4154e168
tteckster <tteckster@gmail.com>
2023-01-04 16:38:41 -0500

    Create nginxproxymanager-v5-install.sh

---
+++ install/nginxproxymanager-v5-install.sh
@@ -0,0 +1,279 @@
+#!/usr/bin/env bash
+if [ "$VERBOSE" == "yes" ]; then set -x; fi
+YW=$(echo "\033[33m")
+RD=$(echo "\033[01;31m")
+BL=$(echo "\033[36m")
+GN=$(echo "\033[1;92m")
+CL=$(echo "\033[m")
+RETRY_NUM=10
+RETRY_EVERY=3
+NUM=$RETRY_NUM
+CM="${GN}✓${CL}"
+CROSS="${RD}✗${CL}"
+BFR="\\r\\033[K"
+HOLD="-"
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+shopt -s expand_aliases
+alias die='EXIT=$? LINE=$LINENO error_exit'
+trap die ERR
+silent() { "$@" > /dev/null 2>&1; }
+function error_exit() {
+  trap - ERR
+  local reason="Unknown failure occurred."
+  local msg="${1:-$reason}"
+  local flag="${RD}‼ ERROR ${CL}$EXIT@$LINE"
+  echo -e "$flag $msg" 1>&2
+  exit $EXIT
+}
+
+function msg_info() {
+  local msg="$1"
+  echo -ne " ${HOLD} ${YW}${msg}..."
+}
+
+function msg_ok() {
+  local msg="$1"
+  echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
+}
+
+function msg_error() {
+  local msg="$1"
+  echo -e "${BFR} ${CROSS} ${RD}${msg}${CL}"
+}
+
+msg_info "Setting up Container OS "
+sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+locale-gen >/dev/null
+while [ "$(hostname -I)" = "" ]; do
+  echo 1>&2 -en "${CROSS}${RD} No Network! "
+  sleep $RETRY_EVERY
+  ((NUM--))
+  if [ $NUM -eq 0 ]; then
+    echo 1>&2 -e "${CROSS}${RD} No Network After $RETRY_NUM Tries${CL}"
+    exit 1
+  fi
+done
+msg_ok "Set up Container OS"
+msg_ok "Network Connected: ${BL}$(hostname -I)"
+
+set +e
+alias die=''
+if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
+  msg_error "Internet NOT Connected"
+    read -r -p "Would you like to continue anyway? <y/N> " prompt
+    if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
+      echo -e " ⚠️  ${RD}Expect Issues Without Internet${CL}"
+    else
+      echo -e " 🖧  Check Network Settings"
+      exit 1
+    fi
+fi
+RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
+if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure"; else msg_ok "DNS Resolved github.com to $RESOLVEDIP"; fi
+alias die='EXIT=$? LINE=$LINENO error_exit'
+set -e
+
+msg_info "Updating Container OS"
+$STD apt-get update
+$STD apt-get -y upgrade
+msg_ok "Updated Container OS"
+
+msg_info "Installing Dependencies"
+$STD apt-get update
+$STD apt-get -y install \
+  sudo \
+  curl \
+  gnupg \
+  make \
+  g++ \
+  gcc \
+  ca-certificates \
+  apache2-utils \
+  logrotate \
+  build-essential \
+  python3-dev \
+  git \
+  lsb-release
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Python"
+$STD apt-get install -y -q --no-install-recommends python3 python3-pip python3-venv
+$STD pip3 install --upgrade setuptools
+$STD pip3 install --upgrade pip
+$STD python3 -m venv /opt/certbot/
+if [ "$(getconf LONG_BIT)" = "32" ]; then
+  $STD python3 -m pip install --no-cache-dir -U cryptography==3.3.2
+fi
+$STD python3 -m pip install --no-cache-dir cffi certbot
+msg_ok "Installed Python"
+
+msg_info "Installing Openresty"
+$STD apt-key add <(curl -fsSL https://openresty.org/package/pubkey.gpg)
+cat <<EOF >/etc/apt/sources.list.d/openresty.list
+deb http://openresty.org/package/debian bullseye openresty
+EOF
+$STD apt-get -y update
+$STD apt-get -y install --no-install-recommends openresty
+msg_ok "Installed Openresty"
+
+msg_info "Setting up Node.js Repository"
+$STD bash <(curl -fsSL https://deb.nodesource.com/setup_16.x)
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get install -y nodejs
+msg_ok "Installed Node.js"
+
+msg_info "Installing Yarn"
+$STD npm install --global yarn
+msg_ok "Installed Yarn"
+
+RELEASE=$(curl -s https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest |
+  grep "tag_name" |
+  awk '{print substr($2, 3, length($2)-4) }')
+
+msg_info "Downloading Nginx Proxy Manager v${RELEASE}"
+wget -q https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v${RELEASE} -O - | tar -xz
+cd ./nginx-proxy-manager-${RELEASE}
+msg_ok "Downloaded Nginx Proxy Manager v${RELEASE}"
+
+msg_info "Setting up Enviroment"
+ln -sf /usr/bin/python3 /usr/bin/python
+ln -sf /usr/bin/certbot /opt/certbot/bin/certbot
+ln -sf /usr/local/openresty/nginx/sbin/nginx /usr/sbin/nginx
+ln -sf /usr/local/openresty/nginx/ /etc/nginx
+
+sed -i "s+0.0.0+${RELEASE}+g" backend/package.json
+sed -i "s+0.0.0+${RELEASE}+g" frontend/package.json
+
+sed -i 's+^daemon+#daemon+g' docker/rootfs/etc/nginx/nginx.conf
+NGINX_CONFS=$(find "$(pwd)" -type f -name "*.conf")
+for NGINX_CONF in $NGINX_CONFS; do
+  sed -i 's+include conf.d+include /etc/nginx/conf.d+g' "$NGINX_CONF"
+done
+
+mkdir -p /var/www/html /etc/nginx/logs
+cp -r docker/rootfs/var/www/html/* /var/www/html/
+cp -r docker/rootfs/etc/nginx/* /etc/nginx/
+cp docker/rootfs/etc/letsencrypt.ini /etc/letsencrypt.ini
+cp docker/rootfs/etc/logrotate.d/nginx-proxy-manager /etc/logrotate.d/nginx-proxy-manager
+ln -sf /etc/nginx/nginx.conf /etc/nginx/conf/nginx.conf
+rm -f /etc/nginx/conf.d/dev.conf
+
+mkdir -p /tmp/nginx/body \
+  /run/nginx \
+  /data/nginx \
+  /data/custom_ssl \
+  /data/logs \
+  /data/access \
+  /data/nginx/default_host \
+  /data/nginx/default_www \
+  /data/nginx/proxy_host \
+  /data/nginx/redirection_host \
+  /data/nginx/stream \
+  /data/nginx/dead_host \
+  /data/nginx/temp \
+  /var/lib/nginx/cache/public \
+  /var/lib/nginx/cache/private \
+  /var/cache/nginx/proxy_temp
+
+chmod -R 777 /var/cache/nginx
+chown root /tmp/nginx
+
+echo resolver "$(awk 'BEGIN{ORS=" "} $1=="nameserver" {print ($2 ~ ":")? "["$2"]": $2}' /etc/resolv.conf);" >/etc/nginx/conf.d/include/resolvers.conf
+
+if [ ! -f /data/nginx/dummycert.pem ] || [ ! -f /data/nginx/dummykey.pem ]; then
+  echo -en "${GN} Generating dummy SSL Certificate... "
+  openssl req -new -newkey rsa:2048 -days 3650 -nodes -x509 -subj "/O=Nginx Proxy Manager/OU=Dummy Certificate/CN=localhost" -keyout /data/nginx/dummykey.pem -out /data/nginx/dummycert.pem &>/dev/null
+fi
+
+mkdir -p /app/global /app/frontend/images
+cp -r backend/* /app
+cp -r global/* /app/global
+msg_ok "Set up Enviroment"
+
+msg_info "Building Frontend"
+cd ./frontend
+export NODE_ENV=development
+$STD yarn install --network-timeout=30000
+$STD yarn build
+cp -r dist/* /app/frontend
+cp -r app-images/* /app/frontend/images
+msg_ok "Built Frontend"
+
+msg_info "Initializing Backend"
+rm -rf /app/config/default.json
+if [ ! -f /app/config/production.json ]; then
+  cat <<'EOF' >/app/config/production.json
+{
+  "database": {
+    "engine": "knex-native",
+    "knex": {
+      "client": "sqlite3",
+      "connection": {
+        "filename": "/data/database.sqlite"
+      }
+    }
+  }
+}
+EOF
+fi
+cd /app
+export NODE_ENV=development
+$STD yarn install --network-timeout=30000
+msg_ok "Initialized Backend"
+
+msg_info "Creating Service"
+cat <<'EOF' >/lib/systemd/system/npm.service
+[Unit]
+Description=Nginx Proxy Manager
+After=network.target
+Wants=openresty.service
+
+[Service]
+Type=simple
+Environment=NODE_ENV=production
+ExecStartPre=-mkdir -p /tmp/nginx/body /data/letsencrypt-acme-challenge
+ExecStart=/usr/bin/node index.js --abort_on_uncaught_exception --max_old_space_size=250
+WorkingDirectory=/app
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+msg_ok "Created Service"
+
+PASS=$(grep -w "root" /etc/shadow | cut -b6)
+if [[ $PASS != $ ]]; then
+  msg_info "Customizing Container"
+  rm /etc/motd
+  rm /etc/update-motd.d/10-uname
+  touch ~/.hushlogin
+  GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
+  mkdir -p $(dirname $GETTY_OVERRIDE)
+  cat <<EOF >$GETTY_OVERRIDE
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+EOF
+  systemctl daemon-reload
+  systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+  msg_ok "Customized Container"
+fi
+if [[ "${SSH_ROOT}" == "yes" ]]; then
+  sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+  systemctl restart sshd
+fi
+
+msg_info "Starting Services"
+$STD systemctl enable --now openresty
+$STD systemctl enable --now npm
+msg_ok "Started Services"
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
