2e697748882c3242c554ff8bdcc966d545b1b0f8
tteckster <tteckster@gmail.com>
2023-01-06 13:01:12 -0500

    Update yunohost-v5-install.sh

--- install/yunohost-v5-install.sh
+++ install/yunohost-v5-install.sh
@@ -91,7 +91,7 @@ msg_ok "Installed Dependencies"
 
 msg_info "Installing YunoHost (Patience)"
 $STD bash <(curl -fsSL https://install.yunohost.org) -a
-wget -s -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+wget -q -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 msg_ok "Installed YunoHost"
 
 PASS=$(grep -w "root" /etc/shadow | cut -b6)
