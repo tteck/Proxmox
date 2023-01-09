b4fe32dcd7e26624754dda8047bc42bc666458d1
tteckster <tteckster@gmail.com>
2023-01-09 10:11:16 -0500

    Update umbrel-v5-install.sh
    adjust daemon.json

--- install/umbrel-v5-install.sh
+++ install/umbrel-v5-install.sh
@@ -86,21 +86,18 @@ $STD apt-get install -y curl
 $STD apt-get install -y sudo
 msg_ok "Installed Dependencies"
 
+msg_info "Installing Umbrel (Patience)"
 DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
 mkdir -p $(dirname $DOCKER_CONFIG_PATH)
-cat >$DOCKER_CONFIG_PATH <<'EOF'
-{
-  "log-driver": "journald"
-}
-EOF
-
-msg_info "Installing Umbrel (Patience)"
 if [ "$ST" == "yes" ]; then
 VER=$(curl -s https://api.github.com/repos/containers/fuse-overlayfs/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
 cd /usr/local/bin
 curl -sSL -o fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/$VER/fuse-overlayfs-x86_64
 chmod 755 /usr/local/bin/fuse-overlayfs
 cd ~
+echo -e '{\n  "storage-driver": "fuse-overlayfs",\n  "log-driver": "journald"\n}' > /etc/docker/daemon.json
+else
+echo -e '{\n  "log-driver": "journald"\n}' > /etc/docker/daemon.json
 fi
 $STD bash <(curl -fsSL https://umbrel.sh)
 systemctl daemon-reload
