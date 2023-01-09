75e422132405d8a8b9f146275dd7118720f2952a
tteckster <tteckster@gmail.com>
2023-01-09 16:40:50 -0500

    Update wikijs-v5-install.sh
    change node.js v16

--- install/wikijs-v5-install.sh
+++ install/wikijs-v5-install.sh
@@ -88,7 +88,7 @@ $STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
-$STD bash <(curl -fsSL https://deb.nodesource.com/setup_18.x)
+$STD bash <(curl -fsSL https://deb.nodesource.com/setup_16.x)
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
