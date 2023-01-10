a7d3f03be5d61bbddaa2ba5f62a8fdcb4104372c
tteckster <tteckster@gmail.com>
2023-01-10 14:55:44 -0500

    Update add-tailscale-lxc.sh
    tweak

--- misc/add-tailscale-lxc.sh
+++ misc/add-tailscale-lxc.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+function header_info {
+cat <<"EOF"
+  ______      _ __                __        
+ /_  __/___ _(_) /_____________ _/ /__      
+  / / / __ `/ / / ___/ ___/ __ `/ / _ \     
+ / / / /_/ / / (__  ) /__/ /_/ / /  __/     
+/_/  \__,_/_/_/____/\___/\__,_/_/\___/      
+                                            
+EOF
+}
+clear
+header_info
 echo -e "\e[1;33mThis script will add Tailscale to an existing LXC Container ONLY\e[0m"
 while true; do
   read -p "Did you replace 106 with your LXC ID? Proceed(y/n)?" yn
@@ -37,9 +49,9 @@ lxc.cgroup2.devices.allow: c 10:200 rwm
 lxc.mount.entry: /dev/net/tun dev/net/tun none bind,create=file
 EOF
 
-msg "⏳ Installing Tailscale..."
+msg "Installing Tailscale..."
 lxc-attach -n $CTID -- bash -c "$(curl -fsSL https://tailscale.com/install.sh)" &>/dev/null || exit
-msg "⌛ Installed Tailscale"
+msg "Installed Tailscale"
 sleep 2
 msg "\e[1;32m ✔ Completed Successfully!\e[0m"
 msg "\e[1;31m Reboot ${CTID} LXC to apply the changes, then run tailscale up in the LXC console\e[0m"
