aba51cc393b4844bd1a9340d142cf3f924e9dfb4
tteckster <tteckster@gmail.com>
2023-01-11 19:52:54 -0500

    Update cronicle-v5.sh

--- ct/cronicle-v5.sh
+++ ct/cronicle-v5.sh
@@ -405,5 +405,5 @@ pct set $CTID -description "# ${APP} LXC
 ### https://tteck.github.io/Proxmox/
 <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} Master should be reachable by going to the following URL.
+echo -e "${APP} Primary should be reachable by going to the following URL.
          ${BL}http://${IP}:3012${CL}  admin|admin \n"
