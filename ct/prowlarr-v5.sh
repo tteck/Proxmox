1122bca77eadfd53f78b0ad767f25d515a64d1ce
tteckster <tteckster@gmail.com>
2023-01-13 13:06:08 -0500

    Update prowlarr-v5.sh

--- ct/prowlarr-v5.sh
+++ ct/prowlarr-v5.sh
@@ -366,3 +366,5 @@ pct set $CTID -description "# ${APP} LXC
 ### https://tteck.github.io/Proxmox/
 <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/☕-Buy me a coffee-red' /></a>"
 msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:9696${CL} \n"
