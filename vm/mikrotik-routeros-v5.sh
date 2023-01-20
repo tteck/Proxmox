0a2896d81fb8bb543c4d71c947f006f928af65c6
tteckster <tteckster@gmail.com>
2023-01-20 11:18:32 -0500

    Update mikrotik-routeros-v5.sh
    update to 7.7

--- vm/mikrotik-routeros-v5.sh
+++ vm/mikrotik-routeros-v5.sh
@@ -227,7 +227,7 @@ msg_ok "Using ${CL}${BL}$STORAGE${CL} ${GN}for Storage Location."
 msg_ok "Virtual Machine ID is ${CL}${BL}$VMID${CL}."
 msg_info "Getting URL for Mikrotik RouterOS Disk Image"
 
-URL=https://download.mikrotik.com/routeros/7.4.1/install-image-7.4.1.zip
+URL=https://download.mikrotik.com/routeros/7.7/install-image-7.7.zip
 
 sleep 2
 msg_ok "${CL}${BL}${URL}${CL}"
