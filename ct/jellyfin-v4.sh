e392cf9811db00509bfe872d1808b9d373f91334
tteckster <tteckster@gmail.com>
2023-01-25 08:14:42 -0500

    add arch check (#981)

--- ct/jellyfin-v4.sh
+++ ct/jellyfin-v4.sh
@@ -68,6 +68,15 @@ function PVE_CHECK() {
     exit
   fi
 }
+function ARCH_CHECK() {
+  ARCH=$(dpkg --print-architecture)
+  if [[ "$ARCH" != "amd64" ]]; then
+    echo -e "\n ❌  This script will not work with PiMox! \n"
+    echo -e "Exiting..."
+    sleep 2
+    exit
+  fi
+}
 function default_settings() {
   echo -e "${DGN}Using ${var_os} Version: ${BGN}${var_version}${CL}"
 
