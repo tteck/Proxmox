32c38611ae72224957454842e2ad4a892bd4a5a6
tteckster <tteckster@gmail.com>
2023-01-27 22:10:14 -0500

    Update technitiumdns-v5-install.sh
    free port 53

--- install/technitiumdns-v5-install.sh
+++ install/technitiumdns-v5-install.sh
@@ -90,6 +90,9 @@ $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Technitium DNS"
+systemctl stop systemd-resolved
+echo "DNSStubListener=no" >>/etc/systemd/resolved.conf
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 $STD bash <(curl -fsSL https://download.technitium.com/dns/install.sh)
 msg_ok "Installed Technitium DNS"
 
