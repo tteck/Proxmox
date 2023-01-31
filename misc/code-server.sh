616dd24616b2b66277e93a979c81cda0bbb95f0b
tteckster <tteckster@gmail.com>
2023-01-31 03:40:43 -0500

    Update code-server.sh
    pve check

--- misc/code-server.sh
+++ misc/code-server.sh
@@ -40,6 +40,7 @@ function error_exit() {
 }
 clear
 header_info
+if command -v pveversion >/dev/null 2>&1; then echo -e "⚠️  Can't Install on Proxmox "; exit; fi
 while true; do
     read -p "This will Install ${APP} on $hostname. Proceed(y/n)?" yn
     case $yn in
