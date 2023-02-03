811ff7fd968af472341fcbf35b3e4cde9a416ea0
tteckster <tteckster@gmail.com>
2023-02-03 08:40:46 -0500

    remove residual error_exit (#1012)

--- install/iobroker-v5-install.sh
+++ install/iobroker-v5-install.sh
@@ -58,7 +58,6 @@ msg_ok "Set up Container OS"
 msg_ok "Network Connected: ${BL}$(hostname -I)"
 
 set +e
-alias die=''
 if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
   msg_error "Internet NOT Connected"
     read -r -p "Would you like to continue anyway? <y/N> " prompt
@@ -71,7 +70,6 @@ if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
 fi
 RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
 if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure"; else msg_ok "DNS Resolved github.com to ${BL}$RESOLVEDIP${CL}"; fi
-alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
