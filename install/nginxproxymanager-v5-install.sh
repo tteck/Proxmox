2d47b876b24acbc3bdfb02543ea0f8bd46e1cf76
tteckster <tteckster@gmail.com>
2023-03-15 21:31:40 -0400

    Update nginxproxymanager-v5-install.sh
    hold v2.9.19

--- install/nginxproxymanager-v5-install.sh
+++ install/nginxproxymanager-v5-install.sh
@@ -139,10 +139,10 @@ RELEASE=$(curl -s https://api.github.com/repos/NginxProxyManager/nginx-proxy-man
   grep "tag_name" |
   awk '{print substr($2, 3, length($2)-4) }')
 
-msg_info "Downloading Nginx Proxy Manager v${RELEASE}"
-wget -q https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v${RELEASE} -O - | tar -xz
-cd ./nginx-proxy-manager-${RELEASE}
-msg_ok "Downloaded Nginx Proxy Manager v${RELEASE}"
+msg_info "Downloading Nginx Proxy Manager v2.9.19"
+wget -q https://codeload.github.com/NginxProxyManager/nginx-proxy-manager/tar.gz/v2.9.19 -O - | tar -xz
+cd ./nginx-proxy-manager-2.9.19
+msg_ok "Downloaded Nginx Proxy Manager v2.9.19"
 
 msg_info "Setting up Enviroment"
 ln -sf /usr/bin/python3 /usr/bin/python
