766adccf7a222cfd445f5385eef1ff4e2f9f253e
tteckster <tteckster@gmail.com>
2023-03-19 09:46:12 -0400

    Update alpine-zigbee2mqtt-v5.sh

--- ct/alpine-zigbee2mqtt-v5.sh
+++ ct/alpine-zigbee2mqtt-v5.sh
@@ -27,7 +27,6 @@ var_os="alpine"
 var_version="3.17"
 NSAPP=$(echo ${APP,,} | tr -d ' ')
 var_install="${NSAPP}-v5-install"
-timezone=$(cat /etc/timezone)
 INTEGER='^[0-9]+([.][0-9]+)?$'
 YW=$(echo "\033[33m")
 BL=$(echo "\033[36m")
@@ -352,6 +351,7 @@ header_info
 }
 
 function update_script() {
+    header_info
     normal=$(echo "\033[m")
     menu=$(echo "\033[36m")
     number=$(echo "\033[33m")
