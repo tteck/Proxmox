46407df46eec712c8dc3cd3bd3ab838d686100a4
nicedevil007 <17103076+nicedevil007@users.noreply.github.com>
2023-04-09 17:37:43 +0200

    empty AdminToken for working vaultwarden

--- install/alpine-vaultwarden-install.sh
+++ install/alpine-vaultwarden-install.sh
@@ -19,9 +19,19 @@ $STD apk add openssl
 $STD apk add argon2
 msg_ok "Installed Dependencies"
 
+msg_info "Installing Alpine-Vaultwarden"
+$STD apk add vaultwarden
+ADMINTOKEN=''
 if NEWTOKEN=$(whiptail --passwordbox "Setup your ADMIN_TOKEN (make it strong)" 10 58 3>&1 1>&2 2>&3); then
   if [[ ! -z "$NEWTOKEN" ]]; then
     ADMINTOKEN=$(echo -n ${NEWTOKEN} | argon2 "$(openssl rand -base64 32)" -e -id -k 19456 -t 2 -p 1)
+cat <<EOF >/etc/conf.d/vaultwarden
+export DATA_FOLDER=/var/lib/vaultwarden
+export WEB_VAULT_FOLDER=/var/lib/vaultwarden/web-vault
+export WEB_VAULT_ENABLED=true
+export ADMIN_TOKEN='$ADMINTOKEN'
+export ROCKET_ADDRESS=0.0.0.0
+EOF
   else
     clear
     echo -e "⚠  User didn't setup ADMIN_TOKEN, admin panel is disabled! \n"
@@ -30,16 +40,6 @@ else
   clear
   echo -e "⚠  User didn't setup ADMIN_TOKEN, admin panel is disabled! \n"
 fi
-
-msg_info "Installing Alpine-Vaultwarden"
-$STD apk add vaultwarden
-cat <<EOF >/etc/conf.d/vaultwarden
-export DATA_FOLDER=/var/lib/vaultwarden
-export WEB_VAULT_FOLDER=/var/lib/vaultwarden/web-vault
-export WEB_VAULT_ENABLED=true
-export ADMIN_TOKEN='$ADMINTOKEN'
-export ROCKET_ADDRESS=0.0.0.0
-EOF
 $STD rc-service vaultwarden start
 $STD rc-update add vaultwarden default
 msg_ok "Installed Alpine-Vaultwarden"
