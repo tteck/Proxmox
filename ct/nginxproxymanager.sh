b39bb439bf674bfb75ec6e44dd5b540f09beaa60
tteckster <tteckster@gmail.com>
2023-04-09 03:46:42 -0400

    Update nginxproxymanager.sh
    fix branch

--- ct/nginxproxymanager.sh
+++ ct/nginxproxymanager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/dev/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2023 tteck
 # Author: tteck (tteckster)
 # License: MIT
