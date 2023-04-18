bd7c1ee7dbdefc7c4edff8f3c6932a6bcb532c2e
tteckster <tteckster@gmail.com>
2023-04-18 10:53:30 -0400

    Update nextcloudpi-install.sh
    set `memory_limit = 512M`

--- install/nextcloudpi-install.sh
+++ install/nextcloudpi-install.sh
@@ -23,6 +23,7 @@ msg_info "Installing NextCloudPi (Patience)"
 $STD bash <(curl -fsSL https://raw.githubusercontent.com/nextcloud/nextcloudpi/master/install.sh)
 sed -i "s/3 => 'nextcloudpi.lan',/3 => '0.0.0.0',/g" /var/www/nextcloud/config/config.php
 sed -i '{s|root:/usr/sbin/nologin|root:/bin/bash|g}' /etc/passwd
+sed -i 's/memory_limit = .*/memory_limit = 512M/' /etc/php/8.1/fpm/php.ini /etc/php/8.1/cli/php.ini
 service apache2 restart
 msg_ok "Installed NextCloudPi"
 
