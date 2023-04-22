52ce746cbeb2f8f8662de85ec6ded9ae38bc5000
tteckster <tteckster@gmail.com>
2023-04-22 05:31:32 -0400

    Update plex-install.sh
    - The command apt-key is now considered deprecated, so migrate keyring to the new location with the new format.
    - Remove the unnecessary dependency gnupg

--- install/plex-install.sh
+++ install/plex-install.sh
@@ -17,7 +17,6 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
-$STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
@@ -37,9 +36,8 @@ if [[ -z "$(grep -w "100000" /proc/self/uid_map)" ]]; then
 fi
 
 msg_info "Setting Up Plex Media Server Repository"
-$STD apt-key add <(curl -fsSL https://downloads.plex.tv/plex-keys/PlexSign.key)
-sh -c 'echo "deb [arch=$(dpkg --print-architecture)] https://downloads.plex.tv/repo/deb/ public main" > /etc/apt/sources.list.d/plexmediaserver.list'
-mv -f /etc/apt/trusted.gpg /etc/apt/trusted.gpg.d/ &>/dev/null
+wget -qO- https://downloads.plex.tv/plex-keys/PlexSign.key >/etc/apt/trusted.gpg.d/PlexSign.asc
+echo "deb [arch=$(dpkg --print-architecture)] https://downloads.plex.tv/repo/deb/ public main" >/etc/apt/sources.list.d/plexmediaserver.list
 msg_ok "Set Up Plex Media Server Repository"
 
 msg_info "Installing Plex Media Server"
