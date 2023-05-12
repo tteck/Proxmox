cb5f031635531832918f7b596924e9bc111095b6
MrTylerjet <MrTylerjet@gmail.com>
2023-05-12 09:24:50 -0600

    Replaced with concise code from tteck

--- install/bazarr-install.sh
+++ install/bazarr-install.sh
@@ -5,30 +5,20 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
-color
-verb_ip6
-catch_errors
-setting_up_container
-network_check
-update_os
-
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
+$STD apt-get install -y mc
 $STD apt-get install -y unzip
 $STD apt-get install -y python3-pip
-$STD apt-get install -y python3-distutils
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Bazarr"
 mkdir -p /var/lib/bazarr/
-chmod 775 /var/lib/bazarr/
-$STD wget --content-disposition 'https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip'
-$STD unzip bazarr -d Bazarr 
-mv Bazarr /opt
-chmod 775 /opt/Bazarr
-$STD python3 -m pip install -r requirements.txt
+wget -q https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip
+unzip -qq bazarr -d /opt/bazarr
+chmod 775 /opt/bazarr /var/lib/bazarr/
+python3 -m pip install -q -r /opt/bazarr/requirements.txt
 msg_ok "Installed Bazarr"
 
 msg_info "Creating Service"
@@ -47,20 +37,15 @@ ExecStart=/usr/bin/python3 /opt/bazarr/bazarr.py
 KillSignal=SIGINT
 TimeoutStopSec=20
 SyslogIdentifier=bazarr
-ExecStartPre=/bin/sleep 30
 
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl -q daemon-reload
-systemctl enable --now -q bazarr
+systemctl enable -q --now bazarr
 msg_ok "Created Service"
 
-motd_ssh
-root
-
 msg_info "Cleaning up"
-rm -rf Bazarr.zip
+rm -rf bazarr.zip
 $STD apt-get autoremove
 $STD apt-get autoclean
 msg_ok "Cleaned"
