e1f561ae748a261e458a6f23596c6987be1fdac5
MrTylerjet <mrtylerjet@gmail.com>
2023-05-12 10:50:39 -0600

    add Bazarr LXC script (#1418)
    * add bazzar LXC script (https://www.bazarr.media/)

---
+++ install/bazarr-install.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y unzip
+$STD apt-get install -y python3-pip
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Bazarr"
+mkdir -p /var/lib/bazarr/
+wget -q https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip
+unzip -qq bazarr -d /opt/bazarr
+chmod 775 /opt/bazarr /var/lib/bazarr/
+python3 -m pip install -q -r /opt/bazarr/requirements.txt
+msg_ok "Installed Bazarr"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/bazarr.service
+[Unit]
+Description=Bazarr Daemon
+After=syslog.target network.target
+
+[Service]
+WorkingDirectory=/opt/bazarr/
+UMask=0002
+Restart=on-failure
+RestartSec=5
+Type=simple
+ExecStart=/usr/bin/python3 /opt/bazarr/bazarr.py
+KillSignal=SIGINT
+TimeoutStopSec=20
+SyslogIdentifier=bazarr
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now bazarr
+msg_ok "Created Service"
+
+motd_ssh
+root
+
+msg_info "Cleaning up"
+rm -rf bazarr.zip
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
