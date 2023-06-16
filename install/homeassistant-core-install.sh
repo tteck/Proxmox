e80dbb4aade8467995a108dc8c80e8359deca584
tteckster <tteckster@gmail.com>
2023-06-16 16:42:46 -0400

    Update homeassistant-core-install.sh
    add bookworm

--- install/homeassistant-core-install.sh
+++ install/homeassistant-core-install.sh
@@ -5,7 +5,7 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
@@ -13,53 +13,82 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies (Patience)"
-$STD apt-get install -y \
-  git \
-  curl \
-  sudo \
-  mc \
-  build-essential \
-  libssl-dev \
-  zlib1g-dev \
-  libbz2-dev \
-  libpcap-dev \
-  libreadline-dev \
-  libsqlite3-dev \
-  libncursesw5-dev \
-  xz-utils \
-  tk-dev \
-  llvm \
-  libxml2-dev \
-  libxmlsec1-dev \
-  libffi-dev \
-  liblzma-dev \
-  dbus-broker \
-  bluez \
-  libmariadb-dev-compat \
-  libjpeg-dev \
-  autoconf \
-  libopenjp2-7 \
-  libtiff5 \
-  libturbojpeg0-dev \
-  liblapack3 \
-  liblapack-dev \
-  libatlas-base-dev
-msg_ok "Installed Dependencies"
+if [[ "$PCT_OSVERSION" == "12" ]]; then
+  msg_info "Installing Dependencies, Bookworm (Patience)"
+  $STD apt-get install -y \
+    git \
+    curl \
+    sudo \
+    mc \
+    python3 \
+    python3-dev \
+    python3-venv \
+    python3-pip \
+    bluez \
+    libffi-dev \
+    libssl-dev \
+    libjpeg-dev \
+    zlib1g-dev \
+    autoconf \
+    build-essential \
+    libopenjp2-7 \
+    libturbojpeg0-dev \
+    tzdata \
+    ffmpeg \
+    liblapack3 \
+    liblapack-dev \
+    libatlas-base-dev
+  $STD systemctl disable systemd-networkd-wait-online.service
+  msg_ok "Installed Dependencies"
+else
+  msg_info "Installing Dependencies, Bullseye (Patience)"
+  $STD apt-get install -y \
+    git \
+    curl \
+    sudo \
+    mc \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libpcap-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libncursesw5-dev \
+    xz-utils \
+    tk-dev \
+    llvm \
+    libxml2-dev \
+    libxmlsec1-dev \
+    libffi-dev \
+    liblzma-dev \
+    dbus-broker \
+    bluez \
+    libmariadb-dev-compat \
+    libjpeg-dev \
+    autoconf \
+    libopenjp2-7 \
+    libtiff5 \
+    libturbojpeg0-dev \
+    liblapack3 \
+    liblapack-dev \
+    libatlas-base-dev
+  msg_ok "Installed Dependencies"
 
-msg_info "Installing pyenv"
-$STD git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-set +e
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init --path)"\nfi' >> ~/.bashrc  
-msg_ok "Installed pyenv"
-. ~/.bashrc
-set -e
-msg_info "Installing Python 3.11.3 (Patience)"
-$STD pyenv install 3.11.3
-pyenv global 3.11.3
-msg_ok "Installed Python 3.11.3"
+  msg_info "Installing pyenv"
+  $STD git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+  set +e
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc
+  echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init --path)"\nfi' >>~/.bashrc
+  msg_ok "Installed pyenv"
+  . ~/.bashrc
+  set -e
+  msg_info "Installing Python 3.11.3 (Patience)"
+  $STD pyenv install 3.11.3
+  pyenv global 3.11.3
+  msg_ok "Installed Python 3.11.3"
+fi
 
 msg_info "Installing Home Assistant-Core"
 mkdir /srv/homeassistant
@@ -68,8 +97,6 @@ python3 -m venv .
 source bin/activate
 $STD pip install --upgrade pip
 $STD python3 -m pip install wheel
-$STD pip install mysqlclient
-$STD pip install psycopg2-binary
 $STD pip install homeassistant
 mkdir -p /root/.homeassistant
 msg_ok "Installed Home Assistant-Core"
