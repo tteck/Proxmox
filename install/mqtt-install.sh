2101583d1a07f3a22eced4b41d95b2b0139e68dd
tteckster <tteckster@gmail.com>
2023-06-17 10:42:01 -0400

    Update mqtt-install.sh
    use standard Debian packages

--- install/mqtt-install.sh
+++ install/mqtt-install.sh
@@ -17,15 +17,9 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
-$STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Mosquitto MQTT Broker"
-$STD wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-$STD apt-key add mosquitto-repo.gpg.key
-cd /etc/apt/sources.list.d/
-$STD wget http://repo.mosquitto.org/debian/mosquitto-bullseye.list
-$STD apt-get update
 $STD apt-get -y install mosquitto
 $STD apt-get -y install mosquitto-clients
 msg_ok "Installed Mosquitto MQTT Broker"
