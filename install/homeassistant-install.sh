c853c24a96a68ae6a59e703d26b9c392333565dd
tteckster <tteckster@gmail.com>
2023-06-20 10:07:02 -0400

    Update homeassistant-install.sh
    shfmt

--- install/homeassistant-install.sh
+++ install/homeassistant-install.sh
@@ -5,7 +5,7 @@
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
-source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
 color
 verb_ip6
 catch_errors
@@ -21,10 +21,10 @@ msg_ok "Installed Dependencies"
 
 msg_info "Updating Python3"
 $STD apt-get install -y \
-python3 \
-python3-dev \
-python3-pip \
-python3-venv
+  python3 \
+  python3-dev \
+  python3-pip \
+  python3-venv
 rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 msg_ok "Updated Python3"
 
@@ -44,14 +44,14 @@ msg_info "Installing Docker $DOCKER_LATEST_VERSION"
 DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
 mkdir -p $(dirname $DOCKER_CONFIG_PATH)
 if [ "$ST" == "yes" ]; then
-VER=$(curl -s https://api.github.com/repos/containers/fuse-overlayfs/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-cd /usr/local/bin
-curl -sSL -o fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/$VER/fuse-overlayfs-x86_64
-chmod 755 /usr/local/bin/fuse-overlayfs
-cd ~
-echo -e '{\n  "storage-driver": "fuse-overlayfs",\n  "log-driver": "journald"\n}' > /etc/docker/daemon.json
+  VER=$(curl -s https://api.github.com/repos/containers/fuse-overlayfs/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+  cd /usr/local/bin
+  curl -sSL -o fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/$VER/fuse-overlayfs-x86_64
+  chmod 755 /usr/local/bin/fuse-overlayfs
+  cd ~
+  echo -e '{\n  "storage-driver": "fuse-overlayfs",\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
 else
-echo -e '{\n  "log-driver": "journald"\n}' > /etc/docker/daemon.json
+  echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
 fi
 $STD sh <(curl -sSL https://get.docker.com)
 msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
@@ -88,7 +88,7 @@ $STD docker run -d \
   -v /etc/localtime:/etc/localtime:ro \
   --net=host \
   homeassistant/home-assistant:stable
-  mkdir /root/hass_config
+mkdir /root/hass_config
 msg_ok "Installed Home Assistant $CORE_LATEST_VERSION"
 
 motd_ssh
