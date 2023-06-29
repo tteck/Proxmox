6b02a5ec0c3e0576b15d390d4169b2a654446657
tteckster <tteckster@gmail.com>
2023-06-29 11:23:39 -0400

    Update dashy-install.sh
    fixed install
    closes https://github.com/tteck/Proxmox/issues/1564

--- install/dashy-install.sh
+++ install/dashy-install.sh
@@ -18,22 +18,20 @@ $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
 $STD apt-get install -y git
-$STD apt-get install -y make
-$STD apt-get install -y g++
-$STD apt-get install -y gcc
 msg_ok "Installed Dependencies"
 
-msg_info "Setting up Node.js Repository"
-$STD bash <(curl -fsSL https://deb.nodesource.com/setup_16.x)
-msg_ok "Set up Node.js Repository"
-
-msg_info "Installing Node.js"
-$STD apt-get install -y nodejs=16.20.1
+msg_info "Installing Node.js (Patience)"
+$STD apt-get install -y npm
+$STD npm cache clean -f
+$STD npm install -g n
+$STD n 16.20.1
 $STD npm install -g pnpm
+ln -sf /usr/local/bin/node /usr/bin/node
 msg_ok "Installed Node.js"
 
 msg_info "Installing Yarn"
 $STD npm install --global yarn
+ln -sf /usr/local/bin/yarn /usr/bin/yarn
 msg_ok "Installed Yarn"
 
 msg_info "Installing Dashy (Patience)"
