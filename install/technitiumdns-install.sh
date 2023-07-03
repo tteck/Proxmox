6acacbf8c875ee2c765d30c9d7b2ae93a8ae35f3
tteckster <tteckster@gmail.com>
2023-07-03 13:12:11 -0400

    Update technitiumdns-install.sh
    Debian 12 compatible

--- install/technitiumdns-install.sh
+++ install/technitiumdns-install.sh
@@ -28,9 +28,6 @@ $STD apt-get install -y aspnetcore-runtime-7.0
 msg_ok "Installed ASP.NET Core Runtime"
 
 msg_info "Installing Technitium DNS"
-systemctl stop systemd-resolved
-echo "DNSStubListener=no" >>/etc/systemd/resolved.conf
-ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 $STD bash <(curl -fsSL https://download.technitium.com/dns/install.sh)
 msg_ok "Installed Technitium DNS"
 
