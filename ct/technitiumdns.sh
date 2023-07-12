cf20993b8601904bf625ca851385f4f16e575af6
tteckster <tteckster@gmail.com>
2023-07-12 13:28:53 -0400

    Update technitiumdns.sh
    fix unbound variable 
    fixes https://github.com/tteck/Proxmox/issues/1608

--- ct/technitiumdns.sh
+++ ct/technitiumdns.sh
@@ -56,13 +56,13 @@ if [[ ! -d /etc/dns ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Updating ${APP}"
 
 if ! dpkg -s aspnetcore-runtime-7.0 > /dev/null 2>&1; then
-    wget -q https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-    dpkg -i packages-microsoft-prod.deb
-    apt-get update
-    apt-get install -y aspnetcore-runtime-7.0
+    wget -q https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb
+    dpkg -i packages-microsoft-prod.deb &>/dev/null
+    apt-get update &>/dev/null
+    apt-get install -y aspnetcore-runtime-7.0 &>/dev/null
     rm packages-microsoft-prod.deb
 fi
-$STD bash <(curl -fsSL https://download.technitium.com/dns/install.sh)
+bash <(curl -fsSL https://download.technitium.com/dns/install.sh) &>/dev/null
 msg_ok "Updated Successfully"
 exit
 }
