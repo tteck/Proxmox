73bf8379be9210bfa477de00025eaf8c89c17e58
tteckster <tteckster@gmail.com>
2023-07-24 14:13:36 -0400

    Create ombi-install.sh

---
+++ install/ombi-install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Ombi"
+LATEST=$(curl -sL https://api.github.com/repos/Ombi-app/Ombi/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
+# wget -q https://github.com/Ombi-app/Ombi/releases/download/${LATEST}/linux-x64.tar.gz
+wget -q https://github.com/Ombi-app/Ombi/releases/download/v4.43.2/linux-x64.tar.gz
+mkdir -p /opt/ombi
+tar -xzf linux-x64.tar.gz -C /opt/ombi
+msg_ok "Installed Ombi"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/ombi.service
+[Unit]
+Description=Ombi
+After=syslog.target network-online.target
+
+[Service]
+ExecStart=/opt/ombi/./Ombi
+WorkingDirectory=/opt/ombi
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now ombi.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
