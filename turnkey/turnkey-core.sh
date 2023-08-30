dd2202695fa7c9f8ae713082694a352f1e07580a
tteckster <tteckster@gmail.com>
2023-08-30 05:33:03 -0400

    tweak turnkey scripts

--- turnkey/turnkey-core.sh
+++ turnkey/turnkey-core.sh
@@ -37,6 +37,7 @@ cat <<"EOF"
 EOF
 }
 header_info
+read -p "Press ENTER to continue..."
 set -o errexit  #Exit immediately if a pipeline returns a non-zero status
 set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
 set -o nounset  #Treat unset variables as an error
