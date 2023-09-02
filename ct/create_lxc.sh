b42e0d85e415130ad7951c9f4008b602afaa645a
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-09-02 08:03:20 -0400

    Update create_lxc.sh

--- ct/create_lxc.sh
+++ ct/create_lxc.sh
@@ -146,11 +146,11 @@ msg_ok "Updated LXC Template List"
 
 if [ "$arm64ct" = "yes" ]; then
   msg_info "Arm64 Detected"
-  msg_info "Downloading LXC Template"
+  msg_info "Downloading LXC Template ${PCT_OSTYPE} ${PCT_OSVERSION:-}"
   pvesm add dir ctgrabtmp -content vztmpl -path /tmp/ctgrabtmp
   curl -s https://raw.githubusercontent.com/ArchemedIan/Proxmox-Arm64-Container-Fetcher/main/pimox_image_fetcher.sh > /tmp/ctgrabtmp/pimox_image_fetcher.sh
   chmod a+x /tmp/ctgrabtmp/pimox_image_fetcher.sh
-  /tmp/ctgrabtmp/pimox_image_fetcher.sh ${PCT_OSTYPE} ${PCT_OSVERSION:-} default /tmp/ctgrabtmp/template/cache 1 >/dev/null 
+  /tmp/ctgrabtmp/pimox_image_fetcher.sh "${PCT_OSTYPE}" "${PCT_OSVERSION:-}" default /tmp/ctgrabtmp/template/cache 1 >/dev/null 
   TEMPLATE_SEARCH=${PCT_OSTYPE}-${PCT_OSVERSION:-}
   mapfile -t TEMPLATES < <(ls -l /tmp/ctgrabtmp/template/cache | sed -n "s/.*\($TEMPLATE_SEARCH.*\)/\1/p" | sort -t - -k 2 -V)
   [ ${#TEMPLATES[@]} -gt 0 ] || exit "Unable to find a template when searching for '$TEMPLATE_SEARCH'."
