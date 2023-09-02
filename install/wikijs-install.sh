faf3bb8dd74b63fd7253d5ee5a8a18a6c29bcb31
tteckster <tteckster@gmail.com>
2023-09-02 08:28:55 -0400

    Update wikijs-install.sh
    node v18
    fixes https://github.com/tteck/Proxmox/issues/1752

--- install/wikijs-install.sh
+++ install/wikijs-install.sh
@@ -24,7 +24,7 @@ msg_ok "Installed Dependencies"
 
 msg_info "Setting up Node.js Repository"
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
