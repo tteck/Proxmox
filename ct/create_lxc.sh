81b0beb7e14abc211a49606cdc7d3f95073ecd03
ArchemedIan <105951210+ArchemedIan@users.noreply.github.com>
2023-09-03 08:43:31 -0400

    Update create_lxc.sh

--- ct/create_lxc.sh
+++ ct/create_lxc.sh
@@ -145,7 +145,7 @@ if [ "$arm64ct" = "yes" ]; then
   msg_info "Downloading And Building LXC Template for Arm64 $PCT_OSTYPE $PCT_OSVERSION"
   sudo pvesm status | grep ctgrabtmp && pvesm remove ctgrabtmp
   pvesm add dir ctgrabtmp -content vztmpl -path /tmp/ctgrabtmp
-  bash <(curl -s https://raw.githubusercontent.com/ArchemedIan/Proxmox-Arm64-Container-Fetcher/main/pimox_image_fetcher.sh) ${PCT_OSTYPE} ${PCT_OSVERSION:-} default /tmp/ctgrabtmp/template/cache 1 > /dev/null
+  bash <(curl -s https://raw.githubusercontent.com/ArchemedIan/Proxmox-Arm64-Container-Fetcher/main/pimox_image_fetcher.sh) ${PCT_OSTYPE} ${PCT_OSVERSION:-} default /tmp/ctgrabtmp/template/cache 5
   msg_ok "Downloaded And Built LXC Template for Arm64 $PCT_OSTYPE $PCT_OSVERSION"
   TEMPLATE_SEARCH=${PCT_OSTYPE}-${PCT_OSVERSION:-}
   mapfile -t TEMPLATES < <(ls -l /tmp/ctgrabtmp/template/cache | sed -n "s/.*\($TEMPLATE_SEARCH.*\)/\1/p" | sort -t - -k 2 -V)
