4280da6c42f814a1730ff0127bd96fc005ca5c95
tteckster <tteckster@gmail.com>
2023-09-30 07:03:00 -0400

    Update alpine-nextcloud.sh
    tweak

--- ct/alpine-nextcloud.sh
+++ ct/alpine-nextcloud.sh
@@ -96,5 +96,5 @@ build_container
 description
 
 msg_ok "Completed Successfully!\n"
-echo -e "${APP} Setup should be reachable by going to the following URL.
+echo -e "${APP} should be reachable by going to the following URL.
          ${BL}https://${IP}${CL} \n"
