737240916ee8daab0e54ad2f45642ef3f89f46a7
tteckster <tteckster@gmail.com>
2023-10-13 06:32:31 -0400

    Update alpine-docker-install.sh
    Change Portainer port

--- install/alpine-docker-install.sh
+++ install/alpine-docker-install.sh
@@ -39,7 +39,7 @@ if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
   docker volume create portainer_data >/dev/null
  $STD docker run -d \
     -p 8000:8000 \
-    -p 9000:9000 \
+    -p 9443:9443 \
     --name=portainer \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
