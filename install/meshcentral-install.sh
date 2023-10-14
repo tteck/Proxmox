34414e6266feea6488ebc91422dccd0583133888
tteckster <tteckster@gmail.com>
2023-10-14 19:57:25 -0400

    Update meshcentral-install.sh
    fixes https://github.com/tteck/Proxmox/issues/1923

--- install/meshcentral-install.sh
+++ install/meshcentral-install.sh
@@ -17,13 +17,19 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
+$STD apt-get install -y ca-certificates
+$STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
 msg_info "Installing Node.js"
-$STD bash <(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh)
-. ~/.bashrc
-$STD nvm install 16.20.1
-ln -sf /root/.nvm/versions/node/v16.20.1/bin/node /usr/bin/node
+$STD apt-get update
+$STD apt-get install -y nodejs
 msg_ok "Installed Node.js"
 
 msg_info "Installing MeshCentral"
