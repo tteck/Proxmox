89c3dfd206642242d658eb5e6bc4365ced0e2a30
tteckster <tteckster@gmail.com>
2023-10-14 20:18:46 -0400

    Update sonarr-install.sh
    - add an option to install v4 (experimental)

--- install/sonarr-install.sh
+++ install/sonarr-install.sh
@@ -21,11 +21,22 @@ $STD apt-get install -y gnupg
 $STD apt-get install -y ca-certificates
 msg_ok "Installed Dependencies"
 
+read -r -p "Would you like to install v4 (experimental)? <y/N> " prompt
 msg_info "Installing Sonarr"
 wget -qO /etc/apt/trusted.gpg.d/sonarr-repo.asc "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2009837cbffd68f45bc180471f4f90de2a9b4bf8"
 echo "deb https://apt.sonarr.tv/debian testing-main main" >/etc/apt/sources.list.d/sonarr.list
 $STD apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confold" install -qqy sonarr &>/dev/null
+if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+  systemctl stop sonarr.service
+  wget -q https://download.sonarr.tv/v4/develop/4.0.0.697/Sonarr.develop.4.0.0.697.linux-x64.tar.gz
+  tar -xzf Sonarr.develop.4.0.0.697.linux-x64.tar.gz
+  cp -r Sonarr/* /usr/lib/sonarr/bin
+  rm -rf Sonarr Sonarr.develop.4.0.0.697.linux-x64.tar.gz
+  sed -i 's|ExecStart=/usr/bin/mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/var/lib/sonarr|ExecStart=/usr/lib/sonarr/bin/Sonarr -nobrowser -data=/var/lib/sonarr|' /lib/systemd/system/sonarr.service
+  systemctl daemon-reload
+  systemctl start sonarr.service
+fi
 msg_ok "Installed Sonarr"
 
 motd_ssh
