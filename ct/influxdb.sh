8e5760af2865b7ff0338bb101414c709fc75f57a
tteckster <tteckster@gmail.com>
2023-10-20 13:17:43 -0400

    Update influxdb.sh
    - default Debian 12
    - set new file path check

--- ct/influxdb.sh
+++ ct/influxdb.sh
@@ -23,7 +23,7 @@ var_disk="8"
 var_cpu="2"
 var_ram="2048"
 var_os="debian"
-var_version="11"
+var_version="12"
 variables
 color
 catch_errors
@@ -52,12 +52,8 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[ ! -f /etc/apt/sources.list.d/influxdb.list || ! -f /etc/apt/sources.list.d/influxdata.list ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+if [[ ! -f /etc/apt/sources.list.d/influxdata.list ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 msg_info "Updating ${APP}"
-wget -q https://repos.influxdata.com/influxdata-archive_compat.key
-cat influxdata-archive_compat.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
-rm -f /etc/apt/trusted.gpg.d/influxdb.gpg
 apt-get update &>/dev/null
 apt-get -y upgrade &>/dev/null
 msg_ok "Updated Successfully"
