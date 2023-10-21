f3c8d3c774f91336290dacb18e1328cf4ee6985b
tteckster <tteckster@gmail.com>
2023-10-21 09:52:23 -0400

    Update nocodb-install.sh
    - full code overhaul

--- install/nocodb-install.sh
+++ install/nocodb-install.sh
@@ -17,25 +17,13 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
-$STD apt-get install -y git
-$STD apt-get install -y make
-$STD apt-get install -y g++
-$STD apt-get install -y gcc
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Node.js"
-$STD bash <(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh)
-. ~/.bashrc
-$STD nvm install 16.20.1
-ln -sf /root/.nvm/versions/node/v16.20.1/bin/node /usr/bin/node
-ln -sf /root/.nvm/versions/node/v16.20.1/bin/npm /usr/bin/npm
-msg_ok "Installed Node.js"
-
 msg_info "Installing NocoDB"
-$STD git clone https://github.com/nocodb/nocodb-seed
-mv nocodb-seed /opt/nocodb
+mkdir -p /opt/nocodb
 cd /opt/nocodb
-$STD npm install
+curl -s http://get.nocodb.com/linux-x64 -o nocodb -L
+chmod +x nocodb
 msg_ok "Installed NocoDB"
 
 msg_info "Creating Service"
@@ -48,11 +36,11 @@ Type=simple
 Restart=always
 User=root
 WorkingDirectory=/opt/nocodb
-ExecStart=/usr/bin/npm start
+ExecStart=/opt/nocodb/./nocodb
 
 [Install]
 WantedBy=multi-user.target" >$service_path
-systemctl enable --now nocodb.service &>/dev/null
+systemctl enable -q --now nocodb.service &>/dev/null
 msg_ok "Created Service"
 
 motd_ssh
