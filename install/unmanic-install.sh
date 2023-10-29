d4b0512eaf107ba42a244e80c828372cb2408fe3
tteckster <tteckster@gmail.com>
2023-10-29 17:53:59 -0400

    Create unmanic-install.sh

---
+++ install/unmanic-install.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y ffmpeg
+$STD apt-get install -y python3-pip
+msg_ok "Installed Dependencies"
+
+if [[ "$CTTYPE" == "0" ]]; then
+  msg_info "Setting Up Hardware Acceleration"
+  $STD apt-get -y install \
+    va-driver-all \
+    ocl-icd-libopencl1 \
+    intel-opencl-icd
+    
+  /bin/chgrp video /dev/dri
+  /bin/chmod 755 /dev/dri
+  /bin/chmod 660 /dev/dri/*
+  msg_ok "Set Up Hardware Acceleration"
+fi
+
+msg_info "Installing Unmanic"
+$STD pip3 install unmanic
+msg_ok "Installed Unmanic"
+
+msg_info "Creating Service"
+cat << EOF >/etc/systemd/system/unmanic.service
+[Unit]
+Description=Unmanic - Library Optimiser
+After=network-online.target
+StartLimitInterval=200
+StartLimitBurst=3
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/unmanic
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable --now -q unmanic.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
