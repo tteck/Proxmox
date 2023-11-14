8f8f5e27feb36913e756d650e4da705074a812f5
tteckster <tteckster@gmail.com>
2023-11-14 08:09:13 -0500

    Update homarr-install.sh

--- install/homarr-install.sh
+++ install/homarr-install.sh
@@ -38,7 +38,8 @@ msg_ok "Installed Node.js/Yarn"
 msg_info "Installing Homarr (Patience)"
 mkdir -p /opt/homarr
 RELEASE=$(curl -s https://api.github.com/repos/ajnart/homarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-wget -O- https://github.com/ajnart/homarr/archive/refs/tags/v${RELEASE}.tar.gz | $STD tar -xz -C /opt && mv /opt/homarr-${RELEASE}/* /opt/homarr
+wget -q -O- https://github.com/ajnart/homarr/archive/refs/tags/v${RELEASE}.tar.gz | $STD tar -xz -C /opt && mv /opt/homarr-${RELEASE}/* /opt/homarr
+rm -rf /opt/homarr-0.14.0
 cd /opt/homarr
 wget -q -O /opt/homarr/.env https://raw.githubusercontent.com/ajnart/homarr/dev/.env.example
 sed -i 's|NEXTAUTH_SECRET="[^"]*"|NEXTAUTH_SECRET="'"$(openssl rand -base64 32)"'"|' /opt/homarr/.env
