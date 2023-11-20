cd4faadeb6108fc2ea668fc5eaef9126a96a5844
tteckster <tteckster@gmail.com>
2023-11-20 09:06:43 -0500

    Update dockge.sh
    create update path

--- ct/dockge.sh
+++ ct/dockge.sh
@@ -53,11 +53,12 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[ ! -d /var ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-msg_info "Updating ${APP} LXC"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
-msg_ok "Updated ${APP} LXC"
+if [[ ! -d /opt/dockge ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating ${APP}"
+cd /opt/dockge
+docker compose pull
+docker compose up -d
+msg_ok "Updated ${APP}"
 exit
 }
 
