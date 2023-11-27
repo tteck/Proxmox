f76bfa30378f452c0e3760d36713cce36f41b34b
tteckster <tteckster@gmail.com>
2023-11-27 09:48:44 -0500

    Update pimox-haos-vm.sh
    tweak description

--- vm/pimox-haos-vm.sh
+++ vm/pimox-haos-vm.sh
@@ -314,10 +314,13 @@ qm set $VMID \
   -efidisk0 ${DISK0_REF},efitype=4m,size=64M \
   -scsi0 ${DISK1_REF},size=32G >/dev/null
 qm set $VMID \
-  -boot order=scsi0 >/dev/null
-qm set $VMID -description "# PiMox HAOS
-### https://github.com/tteck/Proxmox
-[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/D1D7EP4GF)" >/dev/null
+  -boot order=scsi0 \
+  -description "<div align='center'><a href='https://Helper-Scripts.com'><img src='https://raw.githubusercontent.com/tteck/Proxmox/main/misc/images/logo-81x112.png'/></a>
+
+  # Home Assistant OS
+
+  <a href='https://ko-fi.com/D1D7EP4GF'><img src='https://img.shields.io/badge/&#x2615;-Buy me a coffee-blue' /></a>
+  </div>" >/dev/null
 msg_ok "Created HAOS VM ${CL}${BL}(${HN})"
 if [ "$START_VM" == "yes" ]; then
   msg_info "Starting Home Assistant OS VM"
