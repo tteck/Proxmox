883a16637ef34cb32742a16a0020de437df7cc00
tteckster <tteckster@gmail.com>
2023-11-29 23:14:56 -0500

    Update casaos-install.sh
    - pin v0.4.1
    - update after install

--- install/casaos-install.sh
+++ install/casaos-install.sh
@@ -32,7 +32,10 @@ echo -e '{\n  "storage-driver": "fuse-overlayfs",\n  "log-driver": "journald"\n}
 else
 echo -e '{\n  "log-driver": "journald"\n}' > /etc/docker/daemon.json
 fi
-$STD bash <(curl -fsSL https://get.casaos.io)
+wget -q https://get.casaos.io/v0.4.1
+chmod +x v0.4.1
+$STD ./v0.4.1
+rm v0.4.1
 msg_ok "Installed CasaOS"
 
 motd_ssh
