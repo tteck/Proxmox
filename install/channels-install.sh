03b23c04564f0086716f05f68ac51cb7cf69c4ea
tteckster <tteckster@gmail.com>
2023-12-01 09:44:18 -0500

    Update channels-install.sh
    tweak

--- install/channels-install.sh
+++ install/channels-install.sh
@@ -17,7 +17,7 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
-$STD apt-get install -y chromium-browser
+$STD apt-get install -y chromium
 $STD apt-get install -y xvfb
 msg_ok "Installed Dependencies"
 
