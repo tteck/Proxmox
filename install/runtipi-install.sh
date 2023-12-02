f3c70a02225023b59c8408d661bc6bd9c6ce5c80
tteckster <tteckster@gmail.com>
2023-12-02 06:36:12 -0500

    Create runtipi-install.sh

---
+++ install/runtipi-install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2023 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Runtipi (Patience)"
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+mkdir -p "$(dirname "$DOCKER_CONFIG_PATH")"
+echo -e '{\n  "log-driver": "journald"\n}' > "$DOCKER_CONFIG_PATH"
+cd /opt
+wget -q https://raw.githubusercontent.com/runtipi/runtipi/master/scripts/install.sh
+sed -i -e '/newgrp docker/d' -e '/exit 0/d' install.sh
+chmod +x install.sh
+$STD ./install.sh
+msg_ok "Installed Runtipi"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm /opt/install.sh
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
