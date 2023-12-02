9728687e53f1743c0340ac357de40b19cfc13d82
Tanner Davis <tannerdavisr@gmail.com>
2023-12-02 14:10:22 -0700

    remove fuse overlayfs

--- install/rhasspy-install.sh
+++ install/rhasspy-install.sh
@@ -37,16 +37,6 @@ RHASSPY_LATEST_VERSION=$(get_latest_release "rhasspy/rhasspy")
 msg_info "Installing Docker $DOCKER_LATEST_VERSION"
 DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
 mkdir -p $(dirname $DOCKER_CONFIG_PATH)
-if [ "$ST" == "yes" ]; then
-  VER=$(curl -s https://api.github.com/repos/containers/fuse-overlayfs/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-  cd /usr/local/bin
-  curl -sSL -o fuse-overlayfs https://github.com/containers/fuse-overlayfs/releases/download/$VER/fuse-overlayfs-x86_64
-  chmod 755 /usr/local/bin/fuse-overlayfs
-  cd ~
-  echo -e '{\n  "storage-driver": "fuse-overlayfs",\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
-else
-  echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
-fi
 $STD sh <(curl -sSL https://get.docker.com)
 msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
 
