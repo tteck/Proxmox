d4363eb49f7530620c4d7019a7bfdaf9f2838653
tteckster <tteckster@gmail.com>
2023-12-04 05:19:05 -0500

    Update kavita.sh
    fixes https://github.com/tteck/Proxmox/issues/2151

--- ct/kavita.sh
+++ ct/kavita.sh
@@ -56,7 +56,7 @@ if [[ ! -d /opt/Kavita ]]; then msg_error "No ${APP} Installation Found!"; exit;
 msg_info "Updating $APP LXC"
 systemctl stop kavita
 RELEASE=$(curl -s https://api.github.com/repos/Kareadita/Kavita/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-tar -xvzf <(curl -fsSL https://github.com/Kareadita/Kavita/releases/download/$RELEASE/kavita-linux-x64.tar.gz) &>/dev/null
+tar -xvzf <(curl -fsSL https://github.com/Kareadita/Kavita/releases/download/$RELEASE/kavita-linux-x64.tar.gz) --no-same-owner &>/dev/null
 rm -rf Kavita/config
 cp -r Kavita/* /opt/Kavita
 rm -rf Kavita
