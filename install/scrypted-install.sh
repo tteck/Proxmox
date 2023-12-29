7beb2105ebeace2f298fb7d06afb28b4cf0c3f53
tteckster <tteckster@gmail.com>
2023-12-29 17:21:00 -0500

    Update scrypted-install.sh
    revert back to node 18.x

--- install/scrypted-install.sh
+++ install/scrypted-install.sh
@@ -68,7 +68,7 @@ msg_ok "Installed GStreamer"
 msg_info "Setting up Node.js Repository"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
