8abf4d979cb8f4d22d3bde27dc18ddfb9fce5e8f
Ryan Winter <ryanwinter@outlook.com>
2024-01-02 14:45:11 -0800

    portainer not available on port 9000

--- ct/homeassistant.sh
+++ ct/homeassistant.sh
@@ -136,4 +136,4 @@ msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
          ${BL}http://${IP}:8123${CL}
 Portainer should be reachable by going to the following URL.
-         ${BL}http://${IP}:9000${CL}\n"
+         ${BL}https://${IP}:9443${CL}\n"
