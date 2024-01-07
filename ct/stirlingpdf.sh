d11f09050711b1356ab60b7cda46831cd4fdd1cb
tteckster <tteckster@gmail.com>
2024-01-07 16:08:46 -0500

    Create stirlingpdf.sh

---
+++ ct/stirlingpdf.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+   _____ __  _      ___                   ____  ____  ______
+  / ___// /_(_)____/ (_)___  ____ _      / __ \/ __ \/ ____/
+  \__ \/ __/ / ___/ / / __ \/ __ `/_____/ /_/ / / / / /_
+ ___/ / /_/ / /  / / / / / / /_/ /_____/ ____/ /_/ / __/
+/____/\__/_/_/  /_/_/_/ /_/\__, /     /_/   /_____/_/
+                          /____/
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Stirling-PDF"
+var_disk="8"
+var_cpu="2"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /opt/Stirling-PDF ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_error "There is currently no update path available."
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:8080 ${CL} \n"
