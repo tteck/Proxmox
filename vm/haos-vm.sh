e71f2bf40d02de1d09ca2e5b696ac18350972c6e
tteckster <tteckster@gmail.com>
2024-01-16 03:41:46 -0500

    Update haos-vm.sh
    tweak

--- vm/haos-vm.sh
+++ vm/haos-vm.sh
@@ -449,7 +449,7 @@ qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
   -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=32G \
   -boot order=scsi0 \
-  -description "<div align='center'><a href='https://Helper-Scripts.com'><img src='https://raw.githubusercontent.com/tteck/Proxmox/main/misc/images/logo-81x112.png'/></a>
+  -description "<div align='center'><a href='https://Helper-Scripts.com' target='_blank' rel='noopener noreferrer'><img src='https://raw.githubusercontent.com/tteck/Proxmox/main/misc/images/logo-81x112.png'/></a>
 
   # Home Assistant OS
 
