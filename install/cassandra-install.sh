25cd94a4c8e9940aaba93d1b578b4c098b7a4931
Madhur <madhur.ahuja@games24x7.com>
2024-01-16 21:52:21 +0530

    feat: add cassandra lxc

---
+++ install/cassandra-install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y openjdk-8-jdk
+msg_ok "Installed Dependencies"
+
+
+msg_info "Installing Cassandra"
+mkdir /opt/cassandra && cd /opt/cassandra
+wget -q https://dlcdn.apache.org/cassandra/4.1.3/apache-cassandra-4.1.3-bin.tar.gz
+$STD tar -xvzf apache-cassandra-4.1.3-bin.tar.gz
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/cassandra.service
+[Unit]
+Description=Cassandra
+After=network.target
+
+[Service]
+PIDFile=/tmp/cassandra.pid
+ExecStart=/opt/cassandra/bin/cassandra -p /tmp/cassandra.pid -R
+StandardOutput=append:/tmp/cassandra.log
+StandardError=append:/tmp/cassandra-error.log
+LimitNOFILE=100000
+LimitMEMLOCK=infinity
+LimitNPROC=32768
+LimitAS=infinity
+
+[Install]
+WantedBy=default.target
+EOF
+systemctl enable -q --now cassandra.service
+msg_ok "Created Service"
+
+motd_ssh
+customize
