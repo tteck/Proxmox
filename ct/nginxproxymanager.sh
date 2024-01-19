6f4501c7a31f12804dd5ae35ebd4cf33c8931beb
tteckster <tteckster@gmail.com>
2024-01-19 00:16:02 -0500

    Update nginxproxymanager.sh
    tweak

--- ct/nginxproxymanager.sh
+++ ct/nginxproxymanager.sh
@@ -187,4 +187,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
          ${BL}http://${IP}:81${CL} 
-         Reboot LXC if no GUI\n"
+         ${RD}Reboot LXC if no GUI${CL}\n"
