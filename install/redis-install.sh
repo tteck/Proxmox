dedef2c6b82f98902f15bd2aaf8b394a38694cb6
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-01-20 21:03:34 +0530

    feat: address review comments

--- install/redis-install.sh
+++ install/redis-install.sh
@@ -19,11 +19,12 @@ $STD apt-get install -y sudo
 $STD apt-get install -y mc
 $STD apt-get install -y apt-transport-https
 $STD apt-get install -y gnupg
+$STD apt-get install -y lsb-release
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Redis"
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(grep '^VERSION_CODENAME=' /etc/os-release | cut -d'=' -f2) main" | sudo tee /etc/apt/sources.list.d/redis.list
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 $STD apt-get update
 $STD apt-get install -y redis
 sed -i 's/^bind .*/bind 0.0.0.0/' /etc/redis/redis.conf
