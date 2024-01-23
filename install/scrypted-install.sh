f227dc86843be23f95fe1ee3fd7e7372f367c381
tteckster <tteckster@gmail.com>
2024-01-23 05:22:04 -0500

    Update scrypted-install.sh
    tweak

--- install/scrypted-install.sh
+++ install/scrypted-install.sh
@@ -71,10 +71,10 @@ curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dea
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 msg_ok "Set up Node.js Repository"
 
-msg_info "Installing Node.js/npm"
+msg_info "Installing Node.js"
 $STD apt-get update
 $STD apt-get install -y nodejs
-msg_ok "Installed Node.js/npm"
+msg_ok "Installed Node.js"
 
 msg_info "Updating Python3"
 $STD apt-get install -y \
