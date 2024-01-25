adaf2107a8fe2287613da0b8c4c5a9a7c8becca2
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-01-25 23:30:25 +0530

    feat: add script for mongodb

--- install/mongodb-install.sh
+++ install/mongodb-install.sh
@@ -22,7 +22,9 @@ $STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing MongoDB"
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+#wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc |  gpg --dearmor | sudo tee /usr/share/keyrings/mongodb.gpg > /dev/null
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/debian bookworm/mongodb-org/5.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
 sudo apt install -y mongodb-org
 systemctl enable -q --now mongod.service
 msg_ok "Installed MongoDB"
