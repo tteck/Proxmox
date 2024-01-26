742410c651daa8150404548e4c2a2b14e79af879
Madhur Ahuja <ahuja.madhur@gmail.com>
2024-01-26 06:56:25 +0530

    feat: add script for mongodb

--- install/mongodb-install.sh
+++ install/mongodb-install.sh
@@ -22,13 +22,11 @@ $STD apt-get install -y gnupg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing MongoDB"
-curl -fsSL https://pgp.mongodb.com/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
-   --dearmor
-echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+wget -qO- https://pgp.mongodb.com/server-7.0.asc | gpg --dearmor >/usr/share/keyrings/mongodb-server-7.0.gpg
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" >/etc/apt/sources.list.d/mongodb-org-7.0.list
 $STD apt-get update
 $STD apt install -y mongodb-org
-sed -i 's/127\.0\.0\.1/0.0.0.0/g' /etc/mongod.conf
+sed -i 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf
 systemctl enable -q --now mongod.service
 msg_ok "Installed MongoDB"
 
