c99ce3252e0ab8e5eb14dc8b4990c9959cee06aa
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-02-04 22:20:05 +0100

    Add memos script

---
+++ install/memos-install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y ca-certificates
+$STD apt-get install -y mc
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Docker"
+get_latest_release() {
+  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+DOCKER_LATEST_VERSION=$(get_latest_release "moby/moby")
+
+msg_info "Installing Docker $DOCKER_LATEST_VERSION"
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+mkdir -p $(dirname $DOCKER_CONFIG_PATH)
+echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
+$STD sh <(curl -sSL https://get.docker.com)
+msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
+
+msg_info "Installing Memos"
+mkdir /memos/
+docker run -d \
+  --init \
+  --name memos \
+  --publish 80:5230 \
+  --volume /memos/:/var/opt/memos \
+  --restart unless-stopped \
+  neosmemo/memos:stable
+msg_ok "Installed Memos"
+
+read -r -p "Would you like to add Watchtower? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+docker run -d \
+  --name watchtower \
+  --restart unless-stopped \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
