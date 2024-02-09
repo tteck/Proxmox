be47eb1039b7dda6bfeaa83e91789830abfd9982
tteckster <tteckster@gmail.com>
2024-02-09 10:23:52 -0500

    Update tdarr-install.sh
    add handbrake

--- install/tdarr-install.sh
+++ install/tdarr-install.sh
@@ -17,6 +17,7 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
+$STD apt-get install -y handbrake-cli
 msg_ok "Installed Dependencies"
 
 if [[ "$CTTYPE" == "0" ]]; then
