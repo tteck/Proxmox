1ee317d403e0bcbd68ed51161bbef01b91cc1384
[ nad1r ] <nad1r@mail.com>
2024-02-10 03:33:47 +0100

    Update omv-install.sh

--- install/omv-install.sh
+++ install/omv-install.sh
@@ -23,25 +23,33 @@ msg_ok "Installed Dependencies"
 msg_info "Installing OpenMediaVault (Patience)"
 wget -qO- https://packages.openmediavault.org/public/archive.key | gpg --dearmor >"/etc/apt/trusted.gpg.d/openmediavault-archive-keyring.gpg"
 
-cat <<EOF >/etc/apt/sources.list.d/openmediavault.list
-deb https://packages.openmediavault.org/public sandworm main
-# deb https://downloads.sourceforge.net/project/openmediavault/packages sandworm main
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm main
 ## Uncomment the following line to add software from the proposed repository.
-# deb https://packages.openmediavault.org/public sandworm-proposed main
-# deb https://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm-proposed main
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm-proposed main
 ## This software is not part of OpenMediaVault, but is offered by third-party
 ## developers as a service to OpenMediaVault users.
-# deb https://packages.openmediavault.org/public sandworm partner
-# deb https://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public sandworm partner
+# deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages sandworm partner
 EOF
 
 export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 export APT_LISTCHANGES_FRONTEND=none
-$STD apt-get update
-apt-get -y --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault &>/dev/null
+apt-get install --yes gnupg
+wget --quiet --output-document=- https://packages.openmediavault.org/public/archive.key | gpg --dearmor --yes --output "/usr/share/keyrings/openmediavault-archive-keyring.gpg"
+apt-get update
+apt-get --yes --auto-remove --show-upgraded --allow-downgrades --allow-change-held-packages --no-install-recommends --option DPkg::Options::="--force-confdef" --option DPkg::Options::="--force-confold" install openmediavault-keyring openmediavault
+
+# Populate the database.
 omv-confdbadm populate
-msg_ok "Installed OpenMediaVault"
+
+# Display the login information.
+omv-salt deploy run hosts
+cat /etc/issue
+
 
 motd_ssh
 customize
