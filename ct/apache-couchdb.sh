cffb28d18edaa263c05182be88b11417df43fd49
Jan <jan.jambor@xwr.ch>
2024-02-11 18:49:30 +0100

    add Apache CouchDB draft

---
+++ ct/apache-couchdb.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster), JamborJan
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+
+╔═══╗             ╔╗          ╔═══╗            ╔╗  ╔═══╗╔══╗ 
+║╔═╗║             ║║          ║╔═╗║            ║║  ╚╗╔╗║║╔╗║ 
+║║ ║║╔══╗╔══╗ ╔══╗║╚═╗╔══╗    ║║ ╚╝╔══╗╔╗╔╗╔══╗║╚═╗ ║║║║║╚╝╚╗
+║╚═╝║║╔╗║╚ ╗║ ║╔═╝║╔╗║║╔╗║    ║║ ╔╗║╔╗║║║║║║╔═╝║╔╗║ ║║║║║╔═╗║
+║╔═╗║║╚╝║║╚╝╚╗║╚═╗║║║║║║═╣    ║╚═╝║║╚╝║║╚╝║║╚═╗║║║║╔╝╚╝║║╚═╝║
+╚╝ ╚╝║╔═╝╚═══╝╚══╝╚╝╚╝╚══╝    ╚═══╝╚══╝╚══╝╚══╝╚╝╚╝╚═══╝╚═══╝
+     ║║                                                      
+     ╚╝                                                      
+                               
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Apache-Cassandra"
+var_disk="10"
+var_cpu="2"
+var_ram="4096"
+var_os="debian"
+var_version="12"
+VERBOSE="yes"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -f /etc/systemd/system/couchdb.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_error "There is currently no update path available."
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
---
+++ install/apache-couchdb-install.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster), JamborJan
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
+$STD apt-get install -y apt-transport-https
+$STD apt-get install -y gpg
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Apache CouchDB"
+wget -qO- https://downloads.apache.org/couchdb/KEYS | gpg --dearmor >/etc/apt/trusted.gpg.d/couchdb.gpg
+echo "deb https://apache.jfrog.io/artifactory/couchdb-deb/ ${VERSION_CODENAME} main" >/etc/apt/sources.list.d/couchdb.sources.list
+$STD apt-get update
+$STD apt-get install -y couchdb
+# Change the bind address to make CouchDB accessible from other machines
+sed -i 's/;bind_address = 127.0.0.1/bind_address = 0.0.0.0/g' /opt/couchdb/etc/local.ini
+# Add an admin user (replace 'myadmin' and 'mypassword' with your desired admin username and password)
+echo "[admins]" >> /opt/couchdb/etc/local.ini
+echo "myadmin = mypassword" >> /opt/couchdb/etc/local.ini
+msg_ok "Installed Apache CouchDB"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get autoremove
+$STD apt-get autoclean
+msg_ok "Cleaned"
