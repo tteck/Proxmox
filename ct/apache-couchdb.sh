7a6286bfcec2cda876c52eb3a8815b6e905a4769
Jan <jan.jambor@xwr.ch>
2024-02-11 22:54:04 +0100

    fix spacing

--- ct/apache-couchdb.sh
+++ ct/apache-couchdb.sh
@@ -67,4 +67,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-${BL}http://${IP}:5984/_utils/${CL} \n"
+         ${BL}http://${IP}:5984/_utils/${CL} \n"
