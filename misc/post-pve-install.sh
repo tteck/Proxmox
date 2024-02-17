86da4aaabb15956aa0cbc10c53d80cb18c4024b4
tteckster <tteckster@gmail.com>
2024-02-17 15:58:34 -0500

    Update post-pve-install.sh
    tweak

--- misc/post-pve-install.sh
+++ misc/post-pve-install.sh
@@ -99,7 +99,7 @@ EOF
   esac
 
   if [[ "${VERSION}" == "bookworm" ]]; then
-    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "CEPH PACKAGE REPOSITORIES" --menu "The 'Ceph Package Repositories' provides access to both the 'no-subscription' and 'enterprise' repositories.\n \nCorrect 'ceph package sources?" 14 58 2 \
+    CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "CEPH PACKAGE REPOSITORIES" --menu "The 'Ceph Package Repositories' provides access to both the 'no-subscription' and 'enterprise' repositories (initially disabled).\n \nCorrect 'ceph package sources?" 14 58 2 \
       "yes" " " \
       "no" " " 3>&2 2>&1 1>&3)
     case $CHOICE in
