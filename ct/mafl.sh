0f91caa8b5d191adad1ddada77836c0d94fad4aa
tteckster <tteckster@gmail.com>
2024-02-26 06:07:28 -0500

    Update mafl.sh

--- ct/mafl.sh
+++ ct/mafl.sh
@@ -13,14 +13,15 @@ function header_info {
   / /|_/ / __ `/ /_/ /
  / /  / / /_/ / __/ /
 /_/  /_/\__,_/_/ /_/
+
 EOF
 }
 header_info
 echo -e "Loading..."
 APP="Mafl"
-var_disk="2"
-var_cpu="1"
-var_ram="512"
+var_disk="6"
+var_cpu="2"
+var_ram="2048"
 var_os="debian"
 var_version="12"
 variables
@@ -54,45 +55,18 @@ function default_settings() {
 function update_script() {
   header_info
   if [[ ! -d /opt/mafl ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-  msg_info "Stopping ${APP}"
-  systemctl stop mafl
-  msg_ok "Stopped ${APP}"
-
-  msg_info "Backing up config.yml"
-  cd ~
-  cp -R /opt/mafl/data/config.yml config.yml
-  cp -R /opt/mafl/public/icons icons
-  cp -R /opt/mafl/public/favicons favicons
-  msg_ok "Backed up config.yml and icons, favicons directory"
-
-  msg_info "Updating ${APP}"
-  RELEASE=$(curl -s https://api.github.com/repos/hywax/mafl/releases/latest | grep zipball_url | cut -d '"' -f 4)
-  rm -rf /opt/mafl/*
-  cd /opt/mafl
-  wget -q RELEASE -O mafl.zip
-  unzip mafl.zip &>/dev/null
-  msg_ok "Updated ${APP}"
-  msg_info "Restoring config.yml"
-  cd ~
-  cp -R config.yml /opt/mafl/data
-  cp -R icons /opt/mafl/public/icons
-  cp -R favicons /opt/mafl/public/favicons
-  msg_ok "Restored config.yml and icons, favicons directory"
-
-  msg_info "Cleaning"
-  rm -rf config.yml icons favicons /opt/mafl/mafl.zip
-  msg_ok "Cleaned"
-
-  msg_info "Starting ${APP}"
-  systemctl start mafl
-  msg_ok "Started ${APP}"
-  msg_ok "Updated Successfully"
+  msg_error "There is currently no update path available."
   exit
 }
 
 start
 build_container
 description
+
+msg_info "Setting Container to Normal Resources"
+pct set $CTID -memory 1024
+pct set $CTID -cores 1
+msg_ok "Set Container to Normal Resources"
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
