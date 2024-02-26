58f69adbd8e4f3b0f64b8e4a77d4483d109fe5e4
tteckster <tteckster@gmail.com>
2024-02-26 15:15:17 -0500

    Update mariadb-install.sh
    - remove Adminer
    - fixes https://github.com/tteck/Proxmox/issues/2565

--- install/mariadb-install.sh
+++ install/mariadb-install.sh
@@ -27,15 +27,6 @@ sed -i 's/^# *\(port *=.*\)/\1/' /etc/mysql/my.cnf
 sed -i 's/^bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 msg_ok "Installed MariaDB"
 
-read -r -p "Would you like to add Adminer? <y/N> " prompt
-if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
-  msg_info "Installing Adminer"
-  $STD apt install -y adminer
-  $STD a2enconf adminer
-  systemctl reload apache2
-  msg_ok "Installed Adminer"
-fi
-
 motd_ssh
 customize
 
