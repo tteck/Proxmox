b58ae2b8924b554a9d4fcd404f3dede9c6ed3ba8
tteckster <tteckster@gmail.com>
2024-03-08 15:22:55 -0500

    Update mqtt-install.sh
    go back to basic install

--- install/mqtt-install.sh
+++ install/mqtt-install.sh
@@ -21,15 +21,6 @@ $STD apt-get install -y gpg
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Mosquitto MQTT Broker"
-if [ "$PCT_OSTYPE" == "debian" ]; then
-  VERSION="$(awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release)"
- # wget -qO- http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key >/etc/apt/trusted.gpg.d/mosquitto-repo.asc
- # wget -qO /etc/apt/sources.list.d/mosquitto-${VERSION}.list http://repo.mosquitto.org/debian/mosquitto-${VERSION}.list
-  gpg --keyserver keyserver.ubuntu.com --recv-keys 61611AE430993623 >/dev/null 2>&1
-  gpg --export 61611AE430993623 | tee /etc/apt/trusted.gpg.d/mosquitto.gpg >/dev/null 2>&1
-  echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/mosquitto.gpg] https://repo.mosquitto.org/debian ${VERSION} main" >/etc/apt/sources.list.d/mosquitto-${VERSION}.list
-  $STD apt-get update
-fi
 $STD apt-get -y install mosquitto
 $STD apt-get -y install mosquitto-clients
 cat <<EOF >/etc/mosquitto/conf.d/default.conf
