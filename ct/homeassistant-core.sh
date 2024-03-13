89da288a7f01ffdc716b3308ce0400718ca773d5
tteckster <tteckster@gmail.com>
2024-03-12 20:33:47 -0400

    Update homeassistant-core.sh
    tweak

--- ct/homeassistant-core.sh
+++ ct/homeassistant-core.sh
@@ -147,14 +147,14 @@ WantedBy=default.target" >$service_path
   fi
 }
 
+start
+build_container
+description
+
 msg_info "Setting Container to Normal Resources"
 pct set $CTID -memory 1024
 pct set $CTID -cores 2
 msg_ok "Set Container to Normal Resources"
-
-start
-build_container
-description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
