59dfb89db7e7878f302163ae40341d0193520c7e
tteckster <tteckster@gmail.com>
2024-03-22 00:01:39 -0400

    Update hyperion-install.sh
    update install to utilize Debian 12

--- install/hyperion-install.sh
+++ install/hyperion-install.sh
@@ -20,13 +20,13 @@ $STD apt-get install -y mc
 $STD apt-get install -y lsb-release
 $STD apt-get install -y gpg
 $STD apt-get install -y apt-transport-https
-$STD apt-get install -y libpython3.9
+$STD apt-get install -y libpython3.11
 
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Hyperion"
-wget -qO- https://apt.hyperion-project.org/hyperion.pub.key | gpg --dearmor -o /usr/share/keyrings/hyperion.pub.gpg
-sh -c 'echo "deb [signed-by=/usr/share/keyrings/hyperion.pub.gpg] https://apt.hyperion-project.org/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/hyperion.list'
+wget -qO- https://releases.hyperion-project.org/hyperion.pub.key | gpg --dearmor -o /usr/share/keyrings/hyperion.pub.gpg
+echo "deb [signed-by=/usr/share/keyrings/hyperion.pub.gpg] https://apt.releases.hyperion-project.org/ $(lsb_release -cs) main" >/etc/apt/sources.list.d/hyperion.list
 $STD apt-get update
 $STD apt-get install -y hyperion
 $STD systemctl enable --now hyperion@root.service
