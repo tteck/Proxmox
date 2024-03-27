15f1340a78efcc8421b40af674d55ff6ae357781
Sander Schutten <schutten@hotmail.com>
2024-03-27 15:51:09 +0100

    Added docker

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -14,8 +14,41 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y curl sudo git mc
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
 msg_ok "Installed Dependencies"
+
+get_latest_release() {
+  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+
+DOCKER_LATEST_VERSION=$(get_latest_release "moby/moby")
+DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
+
+msg_info "Installing Docker $DOCKER_LATEST_VERSION"
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+mkdir -p $(dirname $DOCKER_CONFIG_PATH)
+echo -e '{\n  "log-driver": "journald"\n}' >/etc/docker/daemon.json
+$STD sh <(curl -sSL https://get.docker.com)
+msg_ok "Installed Docker $DOCKER_LATEST_VERSION"
+
+# read -r -p "Would you like to add Docker Compose? <y/N> " prompt
+# if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+#   msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+#   DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+#   mkdir -p $DOCKER_CONFIG/cli-plugins
+#   curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+#   chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+#   msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+# fi
+
+
+cd /opt/frigate
+wget -q https://raw.githubusercontent.com/blakeblackshear/frigate/dev/docker-compose.yml
+docker compose pull
+docker compose up -d
+
 
 motd_ssh
 customize
