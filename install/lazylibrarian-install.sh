5e3c600d3c765ae23d88428aaaaca0cf6f24db89
MountyMapleSyrup <159492850+MountyMapleSyrup@users.noreply.github.com>
2024-04-02 16:31:59 -0400

    Update lazylibrarian-install.sh
    Add dependencies that every script installs

--- install/lazylibrarian-install.sh
+++ install/lazylibrarian-install.sh
@@ -14,6 +14,9 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y mc
 $STD apt-get install -y git
 $STD apt-get install -y python3
 msg_ok "Installed Dependencies"
