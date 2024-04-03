97fbea35c991cc4a4d22ce045d1ef669f479ea3f
MountyMapleSyrup <159492850+MountyMapleSyrup@users.noreply.github.com>
2024-04-03 10:43:08 -0400

    Update Author for lazylibrarian-install.sh

--- install/lazylibrarian-install.sh
+++ install/lazylibrarian-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Copyright (c) 2021-2024 tteck
-# Author: MountyMapleSyrup (MountyMapleSyrup)
+# Author: tteck (tteckster)
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
