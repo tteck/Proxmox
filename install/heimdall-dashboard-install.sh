9c0f6b6a0c8fa1ae8f178633857790727feb4ddc
tteckster <tteckster@gmail.com>
2024-04-10 07:07:48 -0400

    Update and rename heimdalldashboard-install.sh to heimdall-dashboard-install.sh

--- install/heimdall-dashboard-install.sh
+++ install/heimdall-dashboard-install.sh
@@ -23,6 +23,7 @@ $STD apt-get install -y php8.2-{bz2,curl,sqlite3,zip,xml}
 msg_ok "Installed Dependencies"
 
 RELEASE=$(curl -sX GET "https://api.github.com/repos/linuxserver/Heimdall/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_info "Installing Heimdall Dashboard ${RELEASE}"
 wget -q https://github.com/linuxserver/Heimdall/archive/${RELEASE}.tar.gz
 tar xzf ${RELEASE}.tar.gz
