ba68b641de1f1c0e5a742537e7f85b40a1d3424a
tteckster <tteckster@gmail.com>
2024-04-09 20:18:26 -0400

    Update ombi-install.sh
    tweak

--- install/ombi-install.sh
+++ install/ombi-install.sh
@@ -22,9 +22,10 @@ msg_ok "Installed Dependencies"
 msg_info "Installing Ombi"
 RELEASE=$(curl -sL https://api.github.com/repos/Ombi-app/Ombi/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
 wget -q https://github.com/Ombi-app/Ombi/releases/download/${RELEASE}/linux-x64.tar.gz
-#echo "${RELEASE}" >/opt/${app}_version.txt
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 mkdir -p /opt/ombi
 tar -xzf linux-x64.tar.gz -C /opt/ombi
+rm -rf linux-x64.tar.gz
 msg_ok "Installed Ombi"
 
 msg_info "Creating Service"
