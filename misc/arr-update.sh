278b659671f599ad0965c57d15e3c3d2525fd1c5
Ryan Freeman <DrEVILish@users.noreply.github.com>
2024-04-12 23:33:45 +0100

    Create arr-update.sh

---
+++ misc/arr-update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+app=$1   
+installdir="/opt/$1"
+
+branch="master"
+dlbase="https://$app.servarr.com/v1/update/$branch/updatefile?os=linux&runtime=netcore"
+
+msg_info "Stopping $app"
+systemctl stop $app
+
+msg_info "Updating Dependencies"
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+msg_ok "Updated Dependencies"
+
+msg_info "Updating $app"
+wget -q "$DLURL"
+rm -rf "$installdir"
+tar -xzf ${app^}.*.tar.gz -C "/opt"
+msg_ok "Updated $app"
+rm -rf "${app^}.master.*.tar.gz"
+
+systemctl start $app
+msg_ok "Started $app"
