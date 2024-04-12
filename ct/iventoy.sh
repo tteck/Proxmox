5c9aa442babb89b8ae6bb32d98d012bab6b1b9ee
Ryan Freeman <DrEVILish@users.noreply.github.com>
2024-04-12 17:58:19 +0100

    Update iventoy.sh

--- ct/iventoy.sh
+++ ct/iventoy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/DrEVILish/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
