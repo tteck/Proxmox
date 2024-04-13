d50b02b9ee868ad6bcc6d8fe94ff9b8e94086408
Ryan Freeman <DrEVILish@users.noreply.github.com>
2024-04-13 01:05:28 +0100

    Update arr-update.sh

--- misc/arr-update.sh
+++ misc/arr-update.sh
@@ -5,14 +5,41 @@ source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
 
+if [[ "$1" == "" ]]; then
+  msg_error "App name missing"
+  exit 1
+fi
+
+color
+catch_errors
+
 APP=$1
 installdir="/opt/$1"
 
+#Radarr
+#Lidarr
+#Readarr
 branch="master"
+
+#Whisparr
+if [[ "$APP" == "Whisparr" ]]; then
+branch="nightly"
+fi
+
+#Prowlarr
+if [[ "$APP" == "Prowlarr" ]]; then
+branch="develop"
+fi
+
 dlbase="https://$APP.servarr.com/v1/update/$branch/updatefile?os=linux&runtime=netcore&arch=x64"
 
+#Sonarr
+if [[ "$APP" == "Sonarr" ]]; then
+dlbase="https://services.sonarr.tv/v1/download/main/latest?version=4&os=linux&arch=x64"
+fi
+
 msg_info "Stopping $APP"
-systemctl stop $APP
+#systemctl stop $APP
 msg_ok "Stopped $APP"
 
 msg_info "Updating Dependencies"
@@ -23,9 +50,11 @@ msg_ok "Updated Dependencies"
 msg_info "Updating $APP"
 wget -q --content-disposition "$dlbase"
 rm -rf "$installdir"
-tar -xzf ${APP^}.*.tar.gz -C "/opt"
-msg_ok "Updated $app"
-rm -rf "${APP^}.*.tar.gz"
+tar -xzf ${APP^}.*.tar.gz -C /opt
+chmod 775 "/opt/$APP"
+rm -rf ${APP^}.*.tar.gz
+msg_ok "Updated $APP"
 
-systemctl start $APP
+
+#systemctl start $APP
 msg_ok "Started $APP"
