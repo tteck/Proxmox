ceeea667f7f4a6bd311b1e6a94e2016c2daf15d1
Rémi Bédard-Couture <remi.bc@outlook.com>
2024-04-19 12:28:50 -0400

    Use tteck's official repo to download the function file

--- ct/calibre-web.sh
+++ ct/calibre-web.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/remz1337/Proxmox/remz/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # Co-Author: remz1337
