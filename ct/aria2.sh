80482414c4f8596b7c234f451f1fe4a70a84c714
tteckster <tteckster@gmail.com>
2024-04-21 10:34:30 -0400

    Update aria2.sh
    Correct port

--- ct/aria2.sh
+++ ct/aria2.sh
@@ -67,4 +67,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be accessible via the following IP.
-         ${BL}http://${IP}:6800${CL}"
+         ${BL}http://${IP}:6880${CL}"
