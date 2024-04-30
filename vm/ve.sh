4238d222d5d7d7e1b0a1b3aada5532b33c5efa22
Dharmin Patel <131821512+patel5d2@users.noreply.github.com>
2024-04-30 11:58:35 -0400

    my starting guide ve.sh

---
+++ vm/ve.sh
@@ -0,0 +1,15 @@
+pvesh get /nodes
+
+pvesh get /nodes/<node>/qemu
+
+pvesh create /nodes/<node>/qemu/<vmid>/status/start
+
+pvesh create /nodes/<node>/qemu/<vmid>/status/stop
+
+pvesh create /nodes/<node>/qemu/<vmid>/status/shutdown
+
+pvesh get /nodes/<node>/lxc
+
+pvesh create /nodes/<node>/lxc/<vmid>/status/start
+
+pvesh create /nodes/<node>/lxc/<vmid>/status/stop
