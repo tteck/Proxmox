6cb9e1db0aa317926e040ca53cb61adb821e0784
tteckster <tteckster@gmail.com>
2024-05-04 05:32:14 -0400

    Update flowiseai.sh
    fixes https://github.com/tteck/Proxmox/issues/2906

--- ct/flowiseai.sh
+++ ct/flowiseai.sh
@@ -20,8 +20,8 @@ header_info
 echo -e "Loading..."
 APP="FlowiseAI"
 var_disk="10"
-var_cpu="2"
-var_ram="2048"
+var_cpu="4"
+var_ram="4096"
 var_os="debian"
 var_version="12"
 variables
@@ -67,6 +67,10 @@ start
 build_container
 description
 
+msg_info "Setting Container to Normal Resources"
+pct set $CTID -memory 2048
+pct set $CTID -cores 2
+msg_ok "Set Container to Normal Resources"
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
          ${BL}http://${IP}:3000${CL} \n"
