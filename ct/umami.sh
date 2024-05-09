91985329f6ec95cc969f8af27ac319daf442d941
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-09 18:01:00 +0200

    New LXC: umami (#2946)

---
+++ ct/umami.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+                                   _ 
+  __  ______ ___  ____ _____ ___  (_)
+ / / / / __ `__ \/ __ `/ __ `__ \/ / 
+/ /_/ / / / / / / /_/ / / / / / / /  
+\__,_/_/ /_/ /_/\__,_/_/ /_/ /_/_/   
+                                     
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="umami"
+var_disk="8"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:3000${CL} \n"
