12bd10bef3a2682acc1868960ef219ad49b6cc9a
tteckster <tteckster@gmail.com>
2024-05-09 09:13:13 -0400

    Update wireguard.sh
    "There is currently no update path available."

--- ct/wireguard.sh
+++ ct/wireguard.sh
@@ -60,9 +60,7 @@ UPD=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "SUPPORT" --radio
   3>&1 1>&2 2>&3)
 header_info
 if [ "$UPD" == "1" ]; then
-msg_info "Updating ${APP} LXC"
-apt-get update &>/dev/null
-apt-get -y upgrade &>/dev/null
+msg_error "There is currently no update path available."
 exit
 fi
 if [ "$UPD" == "2" ]; then
