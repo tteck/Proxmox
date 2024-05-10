5f33a02064291c3f990e16705ac18b55b09337c5
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-10 16:14:23 +0200

    Adjust installation script to use 'bun' instead of 'npm' for installing Umami and building the project

--- install/umami-install.sh
+++ install/umami-install.sh
@@ -27,6 +27,7 @@ mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
 curl -fsSL https://bun.sh/install | bash
+source /root/.bashrc
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
