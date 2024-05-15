fb349849004ece255032beafa5c7b5e25230ade2
Bram Suurd <78373894+BramSuurdje@users.noreply.github.com>
2024-05-15 20:45:45 +0200

    Add iVentoy installation scripts

---
+++ ct/iventoy.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    _ _    __           __             
+   (_) |  / /__  ____  / /_____  __  __
+  / /| | / / _ \/ __ \/ __/ __ \/ / / /
+ / / | |/ /  __/ / / / /_/ /_/ / /_/ / 
+/_/  |___/\___/_/ /_/\__/\____/\__, /  
+                              /____/
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="iVentoy"
+var_disk="2"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="0"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:26000/ ${CL} \n"
