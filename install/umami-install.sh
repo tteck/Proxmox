be5c7141d8b1c57d807c8db09e95b9b6e8b0180c
Bram Suurd <bram.suurd@student.nhlstenden.com>
2024-05-16 11:37:07 +0200

    chore: Update Umami installation back to production. remove testing

--- install/umami-install.sh
+++ install/umami-install.sh
@@ -26,13 +26,12 @@ msg_info "Setting up Node.js Repository"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-curl -fsSL https://bun.sh/install | bash
-source /root/.bashrc
 msg_ok "Set up Node.js Repository"
 
 msg_info "Installing Node.js"
 $STD apt-get update
 $STD apt-get install -y nodejs
+$STD npm install -g yarn
 msg_ok "Installed Node.js"
 
 msg_info "Setting up postgresql"
@@ -54,9 +53,9 @@ msg_ok "Set up postgresql"
 msg_info "Installing Umami (Patience)"
 git clone -q https://github.com/umami-software/umami.git /opt/umami
 cd /opt/umami
-$STD bun install
+$STD yarn install
 echo -e "DATABASE_URL=postgresql://$DB_USER:$DB_PASS@localhost:5432/$DB_NAME" >>/opt/umami/.env
-$STD bun run build
+$STD yarn run build
 msg_ok "Installed Umami"
 
 msg_info "Creating Service"
@@ -69,7 +68,7 @@ Type=simple
 Restart=always
 User=root
 WorkingDirectory=/opt/umami
-ExecStart=/root/.bun/bin/bun run start
+ExecStart=/usr/bin/yarn run start
 
 [Install]
 WantedBy=multi-user.target
