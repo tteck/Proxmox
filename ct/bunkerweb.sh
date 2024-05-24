0e24893e52aef3b6988cfe413ea36edf8c2a9aed
tteckster <tteckster@gmail.com>
2024-05-23 21:38:21 -0400

    Create bunkerweb.sh

---
+++ ct/bunkerweb.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 tteck
+# Author: tteck (tteckster)
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    ____              __            _       __     __
+   / __ )__  ______  / /_____  ____| |     / /__  / /_
+  / __  / / / / __ \/ //_/ _ \/ ___/ | /| / / _ \/ __ \
+ / /_/ / /_/ / / / / ,< /  __/ /   | |/ |/ /  __/ /_/ /
+/_____/\__,_/_/ /_/_/|_|\___/_/    |__/|__/\___/_.___/
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="BunkerWeb"
+var_disk="4"
+var_cpu="1"
+var_ram="1024"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -d /etc/bunkerweb ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating $APP"
+apt-get update &>/dev/null
+apt-get -y upgrade &>/dev/null
+msg_ok "Updated $APP"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} setup should be reachable by going to the following URL.
+         ${BL}http://${IP}/setup${CL} \n"
