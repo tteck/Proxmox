258e7535e684145156449772cf25ab9046f09e48
barremian <machinetime77@gmail.com>
2024-05-28 20:54:07 +0200

    lxc-forgejo: Replace main branch with feature branch in build.func path

--- ct/forgejo.sh
+++ ct/forgejo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/barremian/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/barremian/Proxmox/feature/lxc-forgejo/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
