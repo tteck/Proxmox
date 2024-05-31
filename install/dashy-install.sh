9bbe81fe4279fd83a64597271e1c0769257e6225
tteckster <tteckster@gmail.com>
2024-05-30 21:05:15 -0400

    Update dashy-install.sh
    update node.js to 20.x

--- install/dashy-install.sh
+++ install/dashy-install.sh
@@ -19,13 +19,15 @@ $STD apt-get install -y sudo
 $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Node.js (Patience)"
-$STD apt-get install -y npm
-$STD npm cache clean -f
-$STD npm install -g n
-$STD n 16.20.1
-$STD npm install -g pnpm
-ln -sf /usr/local/bin/node /usr/bin/node
+msg_info "Setting up Node.js Repository"
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
+msg_ok "Set up Node.js Repository"
+
+msg_info "Installing Node.js"
+$STD apt-get update
+$STD apt-get install -y nodejs
 msg_ok "Installed Node.js"
 
 msg_info "Installing Yarn"
