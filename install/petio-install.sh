b03d0090b60bdddf0607330dd3176df34de0ee0b
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2024-06-06 14:57:58 +0300

    Update petio-install.sh
    Fix License

--- install/petio-install.sh
+++ install/petio-install.sh
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
-# https://github.com/oldhunterr/Proxmox/raw/main/LICENSE
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
 
 source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
 color
