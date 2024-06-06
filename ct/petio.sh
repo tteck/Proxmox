2a25d005b7c70eb70987fc39bec08719d47de8b9
Sayed Ali Alawi <75791634+oldhunterr@users.noreply.github.com>
2024-06-06 14:55:36 +0300

    Update petio.sh
    Fix Url, licence

--- ct/petio.sh
+++ ct/petio.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/oldhunterr/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
-# https://github.com/oldhunterr/Proxmox/raw/main/LICENSE
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
 
 function header_info {
 clear
