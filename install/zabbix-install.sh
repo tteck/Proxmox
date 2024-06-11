79b138cc6de0f102a9cf6d7998f96ad3c92aec84
tteckster <tteckster@gmail.com>
2024-06-11 14:19:41 -0400

    Update zabbix-install.sh
    tweak

--- install/zabbix-install.sh
+++ install/zabbix-install.sh
@@ -31,8 +31,7 @@ msg_info "Setting up PostgreSQL"
 $STD apt-get install -y postgresql
 DB_NAME=zabbixdb
 DB_USER=zabbix
-RAW_PASS="$(openssl rand -base64 18 | cut -c1-13)"
-DB_PASS=$(printf '%s\n' "$RAW_PASS" | sed 's/[&/\]/\\&/g')
+DB_PASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | cut -c1-13)
 $STD sudo -u postgres psql -c "CREATE ROLE $DB_USER WITH LOGIN PASSWORD '$DB_PASS';"
 $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8' TEMPLATE template0;"
 $STD sudo -u postgres psql -c "ALTER ROLE $DB_USER SET client_encoding TO 'utf8';"
