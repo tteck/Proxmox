cf9ea042b768d8698ba43cc856dcdc5878578d21
ulmentflam <johnwowen3@gmail.com>
2024-06-14 10:49:58 -0400

    Adding MC to the deps

--- install/ollama-install.sh
+++ install/ollama-install.sh
@@ -16,6 +16,7 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
+$STD apt-get install -y mc
 $STD apt-get install -y build-essential
 msg_ok "Installed Dependencies"
 
