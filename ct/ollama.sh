cf90fa55f5c33558ec8d0dff4d90f779672c0eb6
ulmentflam <johnwowen3@gmail.com>
2024-06-14 10:30:43 -0400

    Adding ollama install scripts

---
+++ ct/ollama.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 ulmentflam
+# Author: ulmentflam
+# License: MIT
+# https://github.com/ulmentflam/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+ ________  ___       ___       ________  _____ ______   ________
+|\   __  \|\  \     |\  \     |\   __  \|\   _ \  _   \|\   __  \
+\ \  \|\  \ \  \    \ \  \    \ \  \|\  \ \  \\\__\ \  \ \  \|\  \
+ \ \  \\\  \ \  \    \ \  \    \ \   __  \ \  \\|__| \  \ \   __  \
+  \ \  \\\  \ \  \____\ \  \____\ \  \ \  \ \  \    \ \  \ \  \ \  \
+   \ \_______\ \_______\ \_______\ \__\ \__\ \__\    \ \__\ \__\ \__\
+    \|_______|\|_______|\|_______|\|__|\|__|\|__|     \|__|\|__|\|__|
+
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="Ollama"
+var_disk="8"
+var_cpu="2"
+var_ram="2048"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+if [[ ! -f /etc/systemd/system/ollama.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+msg_info "Updating $APP LXC"
+apt-get update &>/dev/null
+curl -fsSL https://ollama.com/install.sh | sh
+msg_ok "Updated $APP LXC"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}:11434${CL} \n"
---
+++ install/ollama-install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 ulmentflam
+# Author: ulmentflam
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apt-get install -y curl
+$STD apt-get install -y sudo
+$STD apt-get install -y build-essential
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Ollama"
+$STD curl -fsSL https://ollama.com/install.sh | sh
+msg_ok "Installed Ollama"
+
+msg_info "Creating Service"
+service_path="/etc/systemd/system/ollama.service"
+echo "[Unit]
+Description=Ollama Service
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/ollama serve
+User=ollama
+Group=ollama
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target" >$service_path
+systemctl daemon-reload
+systemctl enable --now -q ollama
+msg_ok "Created Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
