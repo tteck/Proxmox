1439e21334864fbd5cf15dd8b61bc3994a1b489a
Sam Heinz <54530346+asylumexp@users.noreply.github.com>
2024-06-28 09:29:59 +1000

    Fix Scrutiny service failure
    It fails to start because it can't access an InfluxDB instance, so just added one.

--- install/scrutiny-install.sh
+++ install/scrutiny-install.sh
@@ -21,8 +21,23 @@ $STD apt-get install -y \
   curl \
   smartmontools  \
   make \
-  mc
+  mc \
+  lsb-base \
+  lsb-release \
+  gnupg2
 msg_ok "Installed Dependencies"
+
+msg_info "Setting up InfluxDB Repository"
+wget -q https://repos.influxdata.com/influxdata-archive_compat.key
+cat influxdata-archive_compat.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
+sh -c 'echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main" > /etc/apt/sources.list.d/influxdata.list'
+msg_ok "Set up InfluxDB Repository"
+
+msg_info "Installing InfluxDB"
+$STD apt-get update
+$STD apt-get install -y influxdb2
+$STD systemctl enable --now influxdb
+msg_ok "Installed InfluxDB"
 
 msg_info "Installing Scrutiny WebApp"
 mkdir -p /opt/scrutiny/{config,web,bin}
