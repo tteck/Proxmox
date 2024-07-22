dc1a6fce0a896d77e6c1951cef4dd2b8c73d414f
Miguel Muniz <miguel@muniz.org>
2024-07-22 13:11:06 -0700

    fixed where the script looks for gitea.service

--- .history/ct/gitea_20240722131033.sh
+++ .history/ct/gitea_20240722131033.sh
@@ -62,7 +62,7 @@ function default_settings() {
 
 function update_script() {
 header_info
-if [[  ! -f /etc/systemd/system/gitea.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+if [[  ! -f /etc/systemd/gitea.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 RELEASE=$(wget -q https://github.com/go-gitea/gitea/releases/latest -O - | grep "title>Release" | cut -d " " -f 4)
 VERSION=${RELEASE#v}
 msg_info "Updating gitea to ${RELEASE}"
