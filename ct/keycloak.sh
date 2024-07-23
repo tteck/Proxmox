8db7e57819ccbee1f8dd8bd8cf1c1010d71f7035
tteckster <tteckster@gmail.com>
2024-07-23 12:13:35 -0400

    Update keycloak.sh
    fix URL

--- ct/keycloak.sh
+++ ct/keycloak.sh
@@ -69,4 +69,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8080${CL} \n"
+         ${BL}http://${IP}:8080/admin${CL} \n"
