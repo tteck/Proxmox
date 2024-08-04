341e873ebac079c8c3a36ed93bbab1f3f6406fb6
tteckster <tteckster@gmail.com>
2024-08-04 17:06:19 -0400

    Update wastebin.sh
    Refactor

--- ct/wastebin.sh
+++ ct/wastebin.sh
@@ -23,8 +23,8 @@ header_info
 echo -e "Loading..."
 APP="Wastebin"
 var_disk="4"
-var_cpu="4"
-var_ram="2048"
+var_cpu="1"
+var_ram="1024"
 var_os="debian"
 var_version="12"
 variables
@@ -58,49 +58,37 @@ function default_settings() {
 function update_script() {
 header_info
 if [[ ! -d /opt/wastebin ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
-if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
-  read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
-  [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
-fi
-msg_info "Stopping Wastebin"
-systemctl stop wastebin
-msg_ok "Wastebin Stopped"
-
-msg_info "Updating Wastebin"
 RELEASE=$(curl -s https://api.github.com/repos/matze/wastebin/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-cd /opt
-if [ -d wastebin_bak ]; then
-  rm -rf wastebin_bak
+if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+  msg_info "Stopping Wastebin"
+  systemctl stop wastebin
+  msg_ok "Wastebin Stopped"
+
+  msg_info "Updating Wastebin"
+  wget -q https://github.com/matze/wastebin/releases/download/${RELEASE}/wastebin_${RELEASE}_x86_64-unknown-linux-musl.tar.zst
+  tar -xf wastebin_${RELEASE}_x86_64-unknown-linux-musl.tar.zst
+  cp -f wastebin /opt/wastebin/
+  chmod +x /opt/wastebin/wastebin
+  echo "${RELEASE}" >/opt/${APP}_version.txt
+  msg_ok "Updated Wastebin"
+
+  msg_info "Starting Wastebin"
+  systemctl start wastebin
+  msg_ok "Started Wastebin"
+
+  msg_info "Cleaning Up"
+  rm -rf wastebin_${RELEASE}_x86_64-unknown-linux-musl.tar.zst
+  msg_ok "Cleaned"
+  msg_ok "Updated Successfully"
+else
+  msg_ok "No update required. ${APP} is already at ${RELEASE}"
 fi
-mv wastebin wastebin_bak
-wget -q "https://github.com/matze/wastebin/archive/refs/tags/${RELEASE}.zip"
-unzip -q ${RELEASE}.zip
-mv wastebin-${RELEASE} /opt/wastebin
-cd /opt/wastebin
-cargo update -q 
-cargo build -q --release
-msg_ok "Updated Wastebin"
-
-msg_info "Starting Wastebin"
-systemctl start wastebin
-msg_ok "Started Wastebin"
-
-msg_info "Cleaning Up"
-cd /opt
-rm -R ${RELEASE}.zip 
-rm -R wastebin_bak 
-msg_ok "Cleaned"
-msg_ok "Updated Successfully"
 exit
 }
 
 start
 build_container
 description
-
-msg_info "Setting Container to Normal Resources"
-pct set $CTID -cores 2
-msg_ok "Set Container to Normal Resources"
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} Setup should be reachable by going to the following URL.
