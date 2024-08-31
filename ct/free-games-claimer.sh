486538c9adee9cce7253d66feb50526591324694
Matan Sarsha <smatan84@gmail.com>
2024-08-31 11:25:49 +0300

    feat(scripts): add free games claimer script

--- ct/free-games-claimer.sh
+++ ct/free-games-claimer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/msarsha/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/msarsha/Proxmox/feat/free-games-claimer/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
