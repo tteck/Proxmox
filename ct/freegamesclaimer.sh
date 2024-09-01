b439f4535ed514c64c53667a2cc64e38cf6e2823
Matan Sarsha <smatan84@gmail.com>
2024-09-01 21:29:10 +0300

    feat(scripts): setup instructions

--- ct/freegamesclaimer.sh
+++ ct/freegamesclaimer.sh
@@ -77,9 +77,9 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} has been setup to claim GOG and Epic games. \n
-Make sure to run an initial login for the cookies to be set.  \n
+Make sure to run an initial login for the cookies to be set.
 
-To run initial setup run: \n
-1. cd /opt/freegamesclaimer \n
-2. node epic-games \n
+For initial setup run: \n
+1. cd /opt/freegamesclaimer
+2. node epic-games
 3. node gog \n"
