20f8f649030c525ef9cef5804c61b99648e8aa18
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-09-16 12:41:53 +0200

    check for file, not for directory

--- ct/homebox.sh
+++ ct/homebox.sh
@@ -54,7 +54,7 @@ function default_settings() {
 }
 function update_script() {
 header_info
-if [[ ! -d /opt/homebox ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+if [[ ! -f /opt/homebox ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
 if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
   read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
   [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
