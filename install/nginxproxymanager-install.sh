97d48dd4b75a8af25d9d9eb498cd014f2054e2df
tteckster <tteckster@gmail.com>
2024-10-04 18:51:35 -0400

    Update nginxproxymanager-install.sh
    fixes https://github.com/tteck/Proxmox/issues/3841

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -37,9 +37,8 @@ $STD apt-get install -y \
   python3-pip \
   python3-venv \
   python3-cffi \
-  python3-certbot \
   python3-certbot-dns-cloudflare
-$STD pip3 install certbot-dns-multi
+$STD pip3 install certbot certbot-dns-multi
 $STD python3 -m venv /opt/certbot/
 rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 msg_ok "Installed Python Dependencies"
