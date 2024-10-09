6c51087d97a2cfb227e3671f11b97b742129aa7e
tteckster <tteckster@gmail.com>
2024-10-09 06:00:45 -0400

    Update mysql-install.sh
    tweak

--- install/mysql-install.sh
+++ install/mysql-install.sh
@@ -29,7 +29,7 @@ msg_ok "Installed Dependencies"
 msg_info "Installing MySQL"
 curl -fsSL https://repo.mysql.com/RPM-GPG-KEY-mysql-2023 | gpg --dearmor  -o /usr/share/keyrings/mysql.gpg
 echo "deb [signed-by=/usr/share/keyrings/mysql.gpg] http://repo.mysql.com/apt/debian $(lsb_release -sc) mysql-8.0" >/etc/apt/sources.list.d/mysql.list
-$STD sudo apt update
+$STD apt-get update
 export DEBIAN_FRONTEND=noninteractive
 $STD apt-get install -y \
   mysql-common \
@@ -39,7 +39,7 @@ msg_ok "Installed MySQL"
 
 msg_info "Configure MySQL Server"
 ADMIN_PASS="$(openssl rand -base64 18 | cut -c1-13)"
-$STD sudo mysql -uroot -p"$ADMIN_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$ADMIN_PASS'; FLUSH PRIVILEGES;"
+$STD mysql -uroot -p"$ADMIN_PASS" -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$ADMIN_PASS'; FLUSH PRIVILEGES;"
 echo "" >>~/mysql.creds
 echo -e "MySQL Root Password: $ADMIN_PASS" >>~/mysql.creds
 msg_ok "MySQL Server configured"
@@ -58,19 +58,19 @@ if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
     php-curl 
 	
 	wget -q "https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
-	sudo mkdir -p /var/www/html/phpMyAdmin
-	$STD sudo tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
-	sudo cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
+	mkdir -p /var/www/html/phpMyAdmin
+	$STD tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
+	cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
 	SECRET=$(openssl rand -base64 32)
-	sudo sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
-	sudo chmod 660 /var/www/html/phpMyAdmin/config.inc.php
-	sudo chown -R www-data:www-data /var/www/html/phpMyAdmin
-	sudo systemctl restart apache2
+	sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
+	chmod 660 /var/www/html/phpMyAdmin/config.inc.php
+	chown -R www-data:www-data /var/www/html/phpMyAdmin
+	systemctl restart apache2
   msg_ok "Added phpMyAdmin"
 fi
 
 msg_info "Start Service"
-sudo systemctl enable -q --now mysql
+systemctl enable -q --now mysql
 msg_ok "Service started"
 
 motd_ssh
