79e0c29e33d8ba7433c2c613b8348e280c257af0
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-10-11 11:01:11 +0200

    Optional Installing of phpMyAdmin for MariaDB (#3885)

--- install/mariadb-install.sh
+++ install/mariadb-install.sh
@@ -25,6 +25,31 @@ sed -i 's/^# *\(port *=.*\)/\1/' /etc/mysql/my.cnf
 sed -i 's/^bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 msg_ok "Installed MariaDB"
 
+read -r -p "Would you like to add PhpMyAdmin? <y/N> " prompt
+if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
+  msg_info "Installing phpMyAdmin"
+  $STD apt-get install -y \
+    apache2 \
+    php \
+    php-mysqli \
+    php-mbstring \
+    php-zip \
+    php-gd \
+    php-json \
+    php-curl 
+	
+	wget -q "https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
+	mkdir -p /var/www/html/phpMyAdmin
+	tar xf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
+	cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
+	SECRET=$(openssl rand -base64 24)
+	sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
+	chmod 660 /var/www/html/phpMyAdmin/config.inc.php
+	chown -R www-data:www-data /var/www/html/phpMyAdmin
+	systemctl restart apache2
+  msg_ok "Installed phpMyAdmin"
+fi
+
 motd_ssh
 customize
 
