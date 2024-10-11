af5218d0a16016236ba871db2bf7bb8c918ed5c4
tteckster <tteckster@gmail.com>
2024-10-11 04:51:43 -0400

    Update mysql-install.sh
    tweak

--- install/mysql-install.sh
+++ install/mysql-install.sh
@@ -57,7 +57,7 @@ if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
 	
 	wget -q "https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz"
 	mkdir -p /var/www/html/phpMyAdmin
-	$STD tar xvf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
+	tar xf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
 	cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
 	SECRET=$(openssl rand -base64 24)
 	sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
