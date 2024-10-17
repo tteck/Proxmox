22e7ff1f175f9f7c6bfa1ea0221d4ec0e04d2afa
tteckster <tteckster@gmail.com>
2024-10-17 05:40:01 -0400

    Update jellyseerr.sh
    tweak

--- ct/jellyseerr.sh
+++ ct/jellyseerr.sh
@@ -68,7 +68,7 @@ else
 fi
 msg_info "Updating $APP"
 cd /opt/jellyseerr
-output=$(git pull --no-rebase &>/dev/null)
+output=$(git pull --no-rebase)
 if echo "$output" | grep -q "Already up to date."
 then
   msg_ok "$APP is already up to date."
