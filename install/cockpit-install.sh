ec24441a2a4f1334abc1a79101e016bcd4c866bc
tteckster <tteckster@gmail.com>
2024-10-19 11:02:23 -0400

    Update cockpit-install.sh
    test

--- install/cockpit-install.sh
+++ install/cockpit-install.sh
@@ -25,7 +25,8 @@ msg_info "Installing Cockpit"
 source /etc/os-release
 echo "deb http://deb.debian.org/debian ${VERSION_CODENAME}-backports main" >/etc/apt/sources.list.d/backports.list
 $STD apt-get update
-$STD apt-get install -t ${VERSION_CODENAME}-backports cockpit --no-install-recommends -y
+#$STD apt-get install -t ${VERSION_CODENAME}-backports cockpit --no-install-recommends -y
+$STD apt-get install -t ${VERSION_CODENAME}-backports cockpit -y
 sed -i "s/root//g" /etc/cockpit/disallowed-users
 msg_ok "Installed Cockpit"
 
