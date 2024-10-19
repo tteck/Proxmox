e995fb4f30614112a67ff5c805e2996cd4d8a7b4
tteckster <tteckster@gmail.com>
2024-10-19 13:49:36 -0400

    Update tianji-install.sh
    tweak

--- install/tianji-install.sh
+++ install/tianji-install.sh
@@ -31,7 +31,7 @@ $STD apt-get install -y \
   mc
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Node.js, pnpm & pm2"
+msg_info "Installing Node.js"
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
@@ -39,7 +39,7 @@ $STD apt-get update
 $STD apt-get install -y nodejs
 $STD npm install -g pnpm@9.7.1
 export NODE_OPTIONS="--max_old_space_size=4096"
-msg_ok "Installed Node.js, pnpm & pm2"
+msg_ok "Installed Node.js"
 
 msg_info "Setting up PostgreSQL"
 DB_NAME=tianji_db
