f3408ed4f3be741003cfcb8c48a27060e8245114
tteckster <tteckster@gmail.com>
2024-10-19 10:36:23 -0400

    Update cockpit.sh
    increase resources

--- ct/cockpit.sh
+++ ct/cockpit.sh
@@ -20,9 +20,9 @@ EOF
 header_info
 echo -e "Loading..."
 APP="Cockpit"
-var_disk="2"
-var_cpu="1"
-var_ram="512"
+var_disk="4"
+var_cpu="2"
+var_ram="1024"
 var_os="debian"
 var_version="12"
 variables
