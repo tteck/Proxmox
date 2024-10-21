f75fbc8d500571542773786ce394b0bf152f8f83
tteckster <tteckster@gmail.com>
2024-10-20 22:02:26 -0400

    Update flaresolverr.sh
    "SET RESOURCES" whiptail

--- ct/flaresolverr.sh
+++ ct/flaresolverr.sh
@@ -56,6 +56,7 @@ function default_settings() {
 function update_script() {
   header_info
   if [[ ! -f /etc/systemd/system/flaresolverr.service ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+  whiptail --backtitle "Proxmox VE Helper Scripts" --msgbox --title "SET RESOURCES" "Please set the resources in your ${APP} LXC to ${var_cpu}vCPU and ${var_ram}RAM for the build process before continuing" 10 75
   RELEASE=$(wget -q https://github.com/FlareSolverr/FlareSolverr/releases/latest -O - | grep "title>Release" | cut -d " " -f 4)
   if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]] || [[ ! -f /opt/${APP}_version.txt ]]; then
     msg_info "Updating $APP LXC"
