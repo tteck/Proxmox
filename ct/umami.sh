09721e7caea1c05b106920479232dcb37ac5cd5b
tteckster <tteckster@gmail.com>
2024-10-20 22:13:27 -0400

    Update umami.sh
    "SET RESOURCES" whiptail

--- ct/umami.sh
+++ ct/umami.sh
@@ -55,6 +55,7 @@ function default_settings() {
 function update_script() {
 header_info
 if [[ ! -d /opt/umami ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+whiptail --backtitle "Proxmox VE Helper Scripts" --msgbox --title "SET RESOURCES" "Please set the resources in your ${APP} LXC to ${var_cpu}vCPU and ${var_ram}RAM for the build process before continuing" 10 75
 if (( $(df /boot | awk 'NR==2{gsub("%","",$5); print $5}') > 80 )); then
   read -r -p "Warning: Storage is dangerously low, continue anyway? <y/N> " prompt
   [[ ${prompt,,} =~ ^(y|yes)$ ]] || exit
