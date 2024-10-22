34917b15f5bbc21b6461c61c1581e48d792e361a
tteckster <tteckster@gmail.com>
2024-10-21 21:20:52 -0400

    Update peanut-install.sh
    pin version v2.6.1 (test)

--- install/peanut-install.sh
+++ install/peanut-install.sh
@@ -35,7 +35,7 @@ msg_ok "Installed NUT"
 
 msg_info "Installing Peanut"
 RELEASE=$(curl -sL https://api.github.com/repos/Brandawg93/PeaNUT/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
-wget -qO peanut.tar.gz https://api.github.com/repos/Brandawg93/PeaNUT/tarball/${RELEASE}
+wget -qO peanut.tar.gz https://api.github.com/repos/Brandawg93/PeaNUT/tarball/v2.6.1
 mkdir -p /opt/peanut
 tar -xzf peanut.tar.gz -C /opt/peanut --strip-components 1
 rm peanut.tar.gz
