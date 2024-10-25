468a5d367ded4cf453a1507452e112ac3e234e2a
tteckster <tteckster@gmail.com>
2024-10-25 05:16:08 -0400

    Update zoraxy-install.sh
    - breaking change
    - switch to binary install

--- install/zoraxy-install.sh
+++ install/zoraxy-install.sh
@@ -17,23 +17,16 @@ msg_info "Installing Dependencies"
 $STD apt-get install -y curl
 $STD apt-get install -y sudo
 $STD apt-get install -y mc
-$STD apt-get install -y git
 msg_ok "Installed Dependencies"
 
-msg_info "Installing Golang"
-set +o pipefail
-RELEASE=$(curl -s https://go.dev/dl/ | grep -o "go.*\linux-amd64.tar.gz" | head -n 1)
-wget -q https://golang.org/dl/$RELEASE
-$STD tar -xzf $RELEASE -C /usr/local
-$STD ln -s /usr/local/go/bin/go /usr/local/bin/go
-set -o pipefail
-msg_ok "Installed Golang"
-
 msg_info "Installing Zoraxy (Patience)"
-$STD git clone https://github.com/tobychui/zoraxy /opt/zoraxy
-cd /opt/zoraxy/src
-$STD go mod tidy
-$STD go build
+RELEASE=$(curl -s https://api.github.com/repos/tobychui/zoraxy/releases/latest  | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+wget -q "https://github.com/tobychui/zoraxy/releases/download/${RELEASE}/zoraxy_linux_amd64"
+mkdir -p /opt/zoraxy
+mv zoraxy_linux_amd64 /opt/zoraxy/zoraxy
+chmod +x /opt/zoraxy/zoraxy
+ln -s /opt/zoraxy/zoraxy /usr/local/bin/zoraxy
+echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed Zoraxy"
 
 msg_info "Creating Service"
@@ -43,8 +36,8 @@ Description=General purpose request proxy and forwarding tool
 After=syslog.target network-online.target
 
 [Service]
-ExecStart=/opt/zoraxy/src/./zoraxy
-WorkingDirectory=/opt/zoraxy/src/
+ExecStart=/opt/zoraxy/./zoraxy
+WorkingDirectory=/opt/zoraxy/
 Restart=always
 
 [Install]
@@ -57,7 +50,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf $RELEASE
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
