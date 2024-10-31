38d3f2c868bb426895aeb59ea41e89acd465b06a
tteckster <tteckster@gmail.com>
2024-10-31 02:18:28 -0400

    Update nginxproxymanager-install.sh
    tweak

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -37,8 +37,9 @@ $STD apt-get install -y \
   python3-pip \
   python3-venv \
   python3-cffi \
+  python3-certbot \
   python3-certbot-dns-cloudflare
-$STD pip3 install certbot certbot-dns-multi
+$STD pip3 install certbot-dns-multi
 $STD python3 -m venv /opt/certbot/
 rm -rf /usr/lib/python3.*/EXTERNALLY-MANAGED
 msg_ok "Installed Python Dependencies"
